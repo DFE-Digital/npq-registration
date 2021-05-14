625d89a13218593e628a850c4c29183b5dd0693f
Phil Lee <asmega@users.noreply.github.com>
2021-05-14 11:33:38 +0100

    remove bootstrapped homepage

--- spec/features/view_pages_spec.rb
+++ spec/features/view_pages_spec.rb
@@ -1,9 +1,4 @@
 require "rails_helper"
 
 RSpec.feature "View pages", type: :feature do
-  scenario "Navigate to home" do
-    visit "/pages/home"
-
-    expect(page).to have_text("Lorem")
-  end
 end
