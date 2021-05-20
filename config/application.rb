294dff4c08253047fb51ce367b3b9a904af948ae
Phil Lee <asmega@users.noreply.github.com>
2021-05-20 15:50:33 +0100

    remove unused rails engines

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
 
-require "rails"
+# require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -8,12 +8,12 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 # require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
