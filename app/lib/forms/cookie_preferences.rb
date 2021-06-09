565c73f83aedfeda3224fc9a0e87fd7881f0b103
Phil Lee <asmega@users.noreply.github.com>
2021-06-09 16:45:08 +0100

    add validations to cookie preferences

--- app/lib/forms/cookie_preferences.rb
+++ app/lib/forms/cookie_preferences.rb
@@ -1,7 +1,12 @@
 module Forms
   class CookiePreferences
+    VALID_CONSENT_OPTIONS = %w[accept reject].freeze
+
     include ActiveModel::Model
 
     attr_accessor :consent, :return_path
+
+    validates :consent, inclusion: { in: VALID_CONSENT_OPTIONS }
+    validates :return_path, format: { with: /\A\// }
   end
 end
--- spec/controllers/cookie_preferences_controller_spec.rb
+++ spec/controllers/cookie_preferences_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe CookiePreferencesController do
   describe "#create" do
     it "rejects cookies" do
       expect(cookies["consented-to-cookies"]).to be_blank
-      post :create, params: { cookie_preferences: { consent: "reject" } }
+      post :create, params: { cookie_preferences: { consent: "reject", return_path: "/" } }
       expect(cookies["consented-to-cookies"]).to eql("reject")
     end
 
     it "accepts cookies" do
       expect(cookies["consented-to-cookies"]).to be_blank
-      post :create, params: { cookie_preferences: { consent: "accept" } }
+      post :create, params: { cookie_preferences: { consent: "accept", return_path: "/" } }
       expect(cookies["consented-to-cookies"]).to eql("accept")
     end
   end
