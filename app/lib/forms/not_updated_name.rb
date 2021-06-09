c82235900efa4b56950473ecd5e7ca94318ea4cf
Phil Lee <asmega@users.noreply.github.com>
2021-06-09 17:22:01 +0100

    add validations to NotUpdatedName form

--- app/lib/forms/not_updated_name.rb
+++ app/lib/forms/not_updated_name.rb
@@ -1,8 +1,12 @@
 module Forms
   class NotUpdatedName < Base
+    VALID_NAME_NOT_UPDATED_ACTION_OPTIONS = %w[change_dqt_name use_old_name].freeze
+
     attr_accessor :name_not_updated_action
 
-    validates :name_not_updated_action, presence: true
+    validates :name_not_updated_action,
+              presence: true,
+              inclusion: { in: VALID_NAME_NOT_UPDATED_ACTION_OPTIONS }
 
     def self.permitted_params
       [:name_not_updated_action]
