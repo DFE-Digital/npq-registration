892c31211bfcbe963658515c2a0b45b381cc222e
James Rose <james@jbpr.net>
2021-06-23 10:26:01 +0100

    Store and communicate active alert to ECF

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_21_162804) do
+ActiveRecord::Schema.define(version: 2021_06_23_090146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2021_06_21_162804) do
     t.datetime "otp_expires_at"
     t.date "date_of_birth"
     t.boolean "trn_verified", default: false, null: false
+    t.boolean "active_alert", default: false
     t.index ["ecf_id"], name: "index_users_on_ecf_id", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
