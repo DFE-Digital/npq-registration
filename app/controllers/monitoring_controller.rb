48cb399866534a38523f5d9f21be07d3e557c029
Phil Lee <asmega@users.noreply.github.com>
2021-06-29 16:40:42 +0100

    expose git sha and docker image id to healthcheck

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -2,7 +2,7 @@ class MonitoringController < ActionController::Base
   def healthcheck
     respond_to do |format|
       format.json do
-        render json: { status: "OK" }
+        render json: { status: "OK", git_commit_sha: ENV["GIT_COMMIT_SHA"], docker_image_id: ENV["DOCKER_IMAGE_ID"] }
       end
     end
   end
