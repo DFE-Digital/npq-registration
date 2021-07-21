c63d4f0670ca3ded04cd254348b1f9bdf06b111f
Phil Lee <asmega@users.noreply.github.com>
2021-07-21 13:37:50 +0100

    add nino to filter_parameter_logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[password passw secret token _key crypt salt certificate otp ssn national_insurance_number]
+Rails.application.config.filter_parameters += %i[
+  password
+  passw
+  secret
+  token
+  _key
+  crypt
+  salt
+  certificate
+  otp
+  ssn
+  national_insurance_number
+  nino
+]
