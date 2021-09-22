87eab7a4e21a303ff1e5403dfb3008956a11ffb0
Phil Lee <asmega@users.noreply.github.com>
2021-09-22 16:03:25 +0100

    add convenience method on Application
    - use on the command line as quick way to check funding eligibility

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,8 @@ class Application < ApplicationRecord
   belongs_to :course
   belongs_to :lead_provider
   belongs_to :school, foreign_key: "school_urn", primary_key: "urn", optional: true
+
+  def calculate_funding_eligbility
+    Services::FundingEligibility.new(course: course, institution: school, headteacher_status: headteacher_status).call
+  end
 end
