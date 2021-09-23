9531234cce863d116bb6bdb459e119a75a7412dc
Phil Lee <asmega@users.noreply.github.com>
2021-09-23 10:40:09 +0100

    add banner when unauthorized to admin area

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,6 +7,7 @@ private
 
   def require_admin
     unless current_user.admin?
+      flash[:negative] = { title: "Unauthorized", text: "Sign in with you admininstrator account" }
       redirect_to sign_in_path
     end
   end
