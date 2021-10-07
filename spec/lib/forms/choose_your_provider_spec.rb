7b6938f2c99a1592506eb5cdb55c1282410699e4
Phil Lee <asmega@users.noreply.github.com>
2021-10-07 10:25:22 +0100

    fix back link on provider page
    - when npqh and eligible for funding

--- spec/lib/forms/choose_your_provider_spec.rb
+++ spec/lib/forms/choose_your_provider_spec.rb
@@ -14,4 +14,36 @@ RSpec.describe Forms::ChooseYourProvider, type: :model do
       expect(subject.errors[:lead_provider_id]).to be_blank
     end
   end
+
+  describe "#previous_step" do
+    let(:current_step) { "choose_your_provider" }
+    let(:request) { nil }
+    let(:course) { Course.find_by(name: "NPQ for Headship (NPQH)") }
+    let(:school) { create(:school) }
+    let(:store) do
+      {
+        "course_id" => course.id,
+        "institution_identifier" => "School-#{school.urn}",
+      }
+    end
+    let(:wizard) do
+      RegistrationWizard.new(
+        current_step: current_step,
+        store: store,
+        request: request,
+      )
+    end
+    let(:mock_funding_service) { instance_double(Services::FundingEligibility, call: true) }
+
+    context "when npqh and eligible for funding" do
+      before do
+        subject.wizard = wizard
+        allow(Services::FundingEligibility).to receive(:new).and_return(mock_funding_service)
+      end
+
+      it "returns :possible_funding" do
+        expect(subject.previous_step).to eql(:possible_funding)
+      end
+    end
+  end
 end
