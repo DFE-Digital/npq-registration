b71781d76c893fd334f5b406eb2853f35266144b
BroiSatse <sklajn@gmail.com>
2022-01-13 09:47:18 +0000

    Skipping find_school step after dqt_mismatch when not working in school

--- spec/lib/forms/dqt_mismatch_spec.rb
+++ spec/lib/forms/dqt_mismatch_spec.rb
@@ -2,18 +2,28 @@ require "rails_helper"
 
 RSpec.describe Forms::DqtMismatch do
   let(:request) { nil }
-  let(:store) { { "teacher_catchment" => "another" } }
+  let(:store) { { "teacher_catchment" => "england", "works_in_school" => "yes" } }
   let(:wizard) { RegistrationWizard.new(store: store, request: request, current_step: :dqt_mismatch) }
 
-  before do
-    subject.wizard = wizard
-  end
+  subject(:step) { described_class.new.tap { |s| s.wizard = wizard } }
 
   describe "#next_step" do
+    subject(:next_step) { step.next_step }
+
+    context "when both in catchment and works in school" do
+      it { is_expected.to be :find_school }
+    end
+
     context "when international teacher" do
-      it "return :choose_your_npq" do
-        expect(subject.next_step).to eql(:choose_your_npq)
-      end
+      let(:store) { super().merge("teacher_catchment" => "other") }
+
+      it { is_expected.to be :choose_your_npq }
+    end
+
+    context "when not working in school" do
+      let(:store) { super().merge("works_in_school" => "no") }
+
+      it { is_expected.to be :choose_your_npq }
     end
   end
 end
