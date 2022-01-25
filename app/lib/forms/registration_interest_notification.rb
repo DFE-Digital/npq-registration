2aa6a96ed8bbb1d0736ad27e93285a0810186ce3
Steven <steven.woods@digital.education.gov.uk>
2022-01-25 22:04:09 +0000

    Add registration interest form and model

---
+++ app/lib/forms/registration_interest_notification.rb
@@ -0,0 +1,30 @@
+module Forms
+  class RegistrationInterestNotification
+    include ActiveModel::Model
+
+    VALID_NOTIFICATION_OPTIONS = %w[yes no].freeze
+
+    attr_accessor :email, :notification_option
+
+    validates :notification_option, inclusion: { in: VALID_NOTIFICATION_OPTIONS }
+    validates :email,
+              presence: true,
+              length: { maximum: 128 },
+              unless: :selected_no?
+    validate :can_register_interest?
+
+    def current_term(term = "Autumn 2022")
+      term
+    end
+
+    def selected_no?
+      notification_option == "no"
+    end
+
+    def can_register_interest?
+      if RegistrationInterest.find_by(email: email).present?
+        errors.add(:email, :taken)
+      end
+    end
+  end
+end
