ccac3ec997422c610e9d2f928660bda2341fcd72
Naomi Lockhart <naomi@lockyy.com>
2022-04-29 17:41:24 +0100

    Add PrivateChildcareProvider model
    This represents the private childcare providers that are reported by the Ofsted registers.

---
+++ app/models/private_childcare_provider.rb
@@ -0,0 +1,28 @@
+class PrivateChildcareProvider < ApplicationRecord
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name,
+                  against: [:name],
+                  using: {
+                    tsearch: {
+                      prefix: true,
+                      dictionary: "english",
+                    },
+                  }
+
+  def address
+    [address_1, address_2, address_3, town, postcode, region].reject(&:blank?)
+  end
+
+  def address_string
+    address.join(", ")
+  end
+
+  def in_england?
+    true # Needs filling in
+  end
+
+  def identifier
+    "PrivateChildcareProvider-#{urn}"
+  end
+end
