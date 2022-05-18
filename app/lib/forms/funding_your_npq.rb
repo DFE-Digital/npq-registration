775ede05bbff6afab4f3002852dca937305e8285
Leonidas Apostolidis <leoapost@users.noreply.github.com>
2022-05-18 13:48:43 +0100

    Update the funding your npq copy

--- app/lib/forms/funding_your_npq.rb
+++ app/lib/forms/funding_your_npq.rb
@@ -26,11 +26,11 @@ module Forms
 
     def options
       [
-        (option("school", "My school or college is covering the cost", link_errors: true) if works_in_school?),
+        (option("school", "My workplace is covering the cost", link_errors: true) if works_in_school?),
         (option("trust", "My trust is paying") if works_in_school? && inside_catchment?),
         (option("employer", "My employer is paying") if !inside_catchment? || !works_in_school?),
         option("self", "I am paying"),
-        option("another", "My NPQ is being paid in another way", "For example, I am sharing the costs with my school or college"),
+        option("another", "My NPQ is being paid in another way", "For example, I am sharing the costs with my workplace"),
       ].compact.freeze
     end
 
