267584b718a6a599d94657b48ee1692df9192852
Leonidas Apostolidis <leoapost@users.noreply.github.com>
2022-05-18 16:16:37 +0100

    Update the funding your npq copy (#399)

--- spec/features/happy_journeys_spec.rb
+++ spec/features/happy_journeys_spec.rb
@@ -121,7 +121,7 @@ RSpec.feature "Happy journeys", type: :feature do
 
     expect(page).to be_axe_clean
     expect(page).to have_text("Funding")
-    page.choose "My school or college is covering the cost", visible: :all
+    page.choose "My workplace is covering the cost", visible: :all
     page.click_button("Continue")
 
     expect(page).to be_axe_clean
@@ -146,7 +146,7 @@ RSpec.feature "Happy journeys", type: :feature do
     expect(check_answers_page.summary_list["Course"].value).to eql("NPQ for Senior Leadership (NPQSL)")
     expect(check_answers_page.summary_list.key?("Have you been a headteacher for two years or more?")).to be_falsey
     expect(check_answers_page.summary_list["School or college"].value).to eql("open manchester school")
-    expect(check_answers_page.summary_list["How is your NPQ being paid for?"].value).to eql("My school or college is covering the cost")
+    expect(check_answers_page.summary_list["How is your NPQ being paid for?"].value).to eql("My workplace is covering the cost")
 
     allow(ApplicationSubmissionJob).to receive(:perform_later).with(anything)
 
