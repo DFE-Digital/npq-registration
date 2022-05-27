7ce90c53d915d54f73e19f0af574b3ff0e30598b
Naomi Lockhart <naomi@lockyy.com>
2022-05-27 15:19:00 +0100

    Simplify Forms::RegistrationInterestNotification
    There are no longer any questions attached to this so the code can be simplified.

--- spec/lib/forms/registration_interest_notification_spec.rb
+++ spec/lib/forms/registration_interest_notification_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Forms::RegistrationInterestNotification, type: :model do
   end
 
   describe "validations" do
-    it { is_expected.to validate_inclusion_of(:notification_option).in_array(Forms::RegistrationInterestNotification::VALID_NOTIFICATION_OPTIONS) }
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_length_of(:email).is_at_most(128) }
 
@@ -17,15 +16,6 @@ RSpec.describe Forms::RegistrationInterestNotification, type: :model do
           subject.email = existing_registration.email
           subject.valid?
           expect(subject.errors[:email]).to include("You have already registered to be notified")
-        end
-      end
-    end
-
-    describe "#selected_no?" do
-      context "opting not to register" do
-        it "does not provide an email error when selecting not to register interest" do
-          subject.notification_option = "no"
-          expect(subject.valid?).to be_truthy
         end
       end
     end
