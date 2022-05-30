b8c5a23f3d673f99c4f036375f1f33472b4f2c05
Naomi Lockhart <naomi@lockyy.com>
2022-05-30 17:19:29 +0100

    Skip early years flow if works in school

--- app/lib/forms/qualified_teacher_check.rb
+++ app/lib/forms/qualified_teacher_check.rb
@@ -70,8 +70,12 @@ module Forms
 
         if changing_answer?
           :check_answers
-        elsif wizard.query_store.inside_catchment? && wizard.query_store.works_in_school?
-          :find_school
+        elsif wizard.query_store.works_in_school?
+          if wizard.query_store.inside_catchment?
+            :find_school
+          else
+            :choose_your_npq
+          end
         else
           :work_in_childcare
         end
