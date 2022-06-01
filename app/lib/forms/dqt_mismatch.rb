15db8f4518bc20950e9ae55c210c83fdc268d5c5
Naomi Lockhart <naomi@lockyy.com>
2022-06-01 11:37:17 +0100

    Update DQT mismatch path next step bug
    It needs to match how qualified_teacher_check works

--- app/lib/forms/dqt_mismatch.rb
+++ app/lib/forms/dqt_mismatch.rb
@@ -7,8 +7,12 @@ module Forms
     def next_step
       if changing_answer?
         :check_answers
-      elsif wizard.query_store.inside_catchment? && wizard.query_store.works_in_school?
-        :find_school
+      elsif wizard.query_store.works_in_school?
+        if wizard.query_store.inside_catchment?
+          :find_school
+        else
+          :choose_your_npq
+        end
       else
         :work_in_childcare
       end
