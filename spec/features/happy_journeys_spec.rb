eec70a697dde6ccfad6283324f9261a6f996fbf9
Naomi Lockhart <naomi@lockyy.com>
2022-06-20 13:45:04 +0100

    Add testing requirement for all journey specs
    This makes sure that no matter what, all of the end to end jourrney specs check for the application that data that is persisted at the end, we can’t forget to check it this way.

--- spec/features/happy_journeys_spec.rb
+++ spec/features/happy_journeys_spec.rb
@@ -6,15 +6,21 @@ RSpec.feature "Happy journeys", type: :feature do
   end
 
   def latest_application_user
-    latest_application.user
+    latest_application&.user
   end
 
   def retrieve_latest_application_user_data
-    latest_application_user.as_json(except: %i[id created_at updated_at])
+    latest_application_user&.as_json(except: %i[id created_at updated_at])
   end
 
   def retrieve_latest_application_data
-    latest_application.as_json(except: %i[id created_at updated_at user_id])
+    latest_application&.as_json(except: %i[id created_at updated_at user_id])
+  end
+
+  before do
+    # Make sure all the tests are checking this data
+    expect(self).to receive(:retrieve_latest_application_user_data).and_call_original
+    expect(self).to receive(:retrieve_latest_application_data).and_call_original
   end
 
   scenario "registration journey via using old name and not headship" do
@@ -2043,6 +2049,20 @@ RSpec.feature "Happy journeys", type: :feature do
 
     expect(page).to be_axe_clean
 
+    expect(retrieve_latest_application_user_data).to eq(
+      "active_alert" => false,
+      "admin" => false,
+      "date_of_birth" => "1980-12-13",
+      "ecf_id" => nil,
+      "email" => "user@example.com",
+      "full_name" => "John Doe",
+      "national_insurance_number" => nil,
+      "otp_expires_at" => nil,
+      "otp_hash" => nil,
+      "trn" => "1234567",
+      "trn_auto_verified" => true,
+      "trn_verified" => true,
+    )
     expect(retrieve_latest_application_data).to eq(
       "cohort" => 2022,
       "course_id" => Course.find_by_code(code: :NPQEYL).id,
