39a969078d349872f85ac04f21a0cb156cd7da74
Naomi Lockhart <naomi@lockyy.com>
2022-06-20 16:37:07 +0100

    Remove Application.calculate_funding_eligibility
    It was not used anywhere and was adding needless complexity.

--- app/models/application.rb
+++ app/models/application.rb
@@ -13,42 +13,11 @@ class Application < ApplicationRecord
     private_nursery: "private_nursery",
   }
 
-  def calculate_funding_eligibility
-    Services::FundingEligibility.new(
-      course: course,
-      institution: institution,
-      inside_catchment: inside_catchment?,
-      new_headteacher: new_headteacher?,
-      trn: user.trn,
-    ).funded?
-  end
-
-  def institution
-    return private_childcare_provider if private_childcare_provider_institution?
-    return school if school_institution?
-
-    nil
-  end
-
   def inside_catchment?
     %w[england].include?(teacher_catchment)
   end
 
   def new_headteacher?
     %w[yes_when_course_starts yes_in_first_five_years yes_in_first_two_years].include?(headteacher_status)
-  end
-
-private
-
-  def private_childcare_provider_institution?
-    works_in_nursery? && private_nursery?
-  end
-
-  def school_institution?
-    works_in_public_childcare_provider? || works_in_school?
-  end
-
-  def works_in_public_childcare_provider?
-    works_in_nursery? && (local_authority_maintained_nursery? || preschool_class_as_part_of_school?)
   end
 end
