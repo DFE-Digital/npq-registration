53a55a20d58d61c971e94317a94e1d573c6b5103
Naomi Lockhart <naomi@lockyy.com>
2022-06-21 17:29:50 +0100

    Update how RegistrationInterest emails are validated

--- app/models/registration_interest.rb
+++ app/models/registration_interest.rb
@@ -3,13 +3,13 @@ class RegistrationInterest < ApplicationRecord
             presence: true,
             length: { maximum: 128 },
             uniqueness: { case_sensitive: false },
-            email: true
+            notify_email: true
 
   scope :not_yet_notified, -> { where(notified: false) }
   scope :random_sample, ->(count) { order("RANDOM()").first(count) }
 
   # We did not originally valid email format so we need to check this before sending
   def valid_email?
-    EmailValidator.valid?(email)
+    NotifyEmailValidator.valid?(value)
   end
 end
