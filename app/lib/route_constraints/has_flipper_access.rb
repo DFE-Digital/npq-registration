67cddd41bff028088a9d1d526941d1255af490a2
Naomi Lockhart <naomi@lockyy.com>
2022-10-20 12:35:11 +0100

    Add Flipper for managing feature flags
    - Adds Flipper
    - Move registration closing into Flipper interface

---
+++ app/lib/route_constraints/has_flipper_access.rb
@@ -0,0 +1,11 @@
+module RouteConstraints
+  class HasFlipperAccess
+    def self.matches?(request)
+      current_user_id = request.session['user_id']
+
+      current_user = User.find_by(id: current_user_id)
+
+      current_user.present? && current_user.respond_to?(:flipper_access?) && current_user.flipper_access?
+    end
+  end
+end
--- app/lib/services/feature.rb
+++ app/lib/services/feature.rb
@@ -1,25 +1,14 @@
 module Services
   class Feature
-    REGISTRATION_CLOSE_DATE = Time.zone.parse("12 March 2022 00:00")
-    REGISTRATION_OPEN_DATE = Time.zone.parse("6 June 2022 12:00")
-    REGISTRATION_CLOSED = (REGISTRATION_CLOSE_DATE..REGISTRATION_OPEN_DATE)
+    REGISTRATION_CLOSED_KEY = "Registration closed"
+
+    FEATURE_FLAG_KEYS = [
+      REGISTRATION_CLOSED_KEY
+    ]
 
     class << self
       def registration_closed?
-        features_enabled? && (registration_closed_due_to_timing? || registration_closed_via_env_variable?)
-      end
-
-      # We only enable these feature in prod.
-      def features_enabled?
-        ENV["SERVICE_ENV"] == "production"
-      end
-
-      def registration_closed_due_to_timing?
-        REGISTRATION_CLOSED.cover?(Time.zone.now)
-      end
-
-      def registration_closed_via_env_variable?
-        ENV.fetch("REGISTRATION_CLOSED", "false") == "true"
+        Flipper.enabled?(REGISTRATION_CLOSED_KEY)
       end
     end
   end
