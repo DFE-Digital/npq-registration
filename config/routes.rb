c7ecf65363b7d4bbd555ec1950914cad123da0da
Naomi Lockhart <naomi@lockyy.com>
2022-11-03 11:57:21 +0000

    Add interface for viewing and toggling current user feature flags

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get "/healthcheck", to: "monitoring#healthcheck", format: :json
 
+  resource :feature_flags, only: [:show]
+
   resources :schools, only: [:index]
   resources :institutions, only: [:index]
   resources :private_childcare_providers, only: [:index]
