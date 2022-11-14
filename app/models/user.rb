ab24d62338f205458a05becdb2c55d7a11922ecf
Naomi Lockhart <naomi@lockyy.com>
2022-11-14 12:50:43 +0000

    Don’t attempt to set NINO from GAI
    They don’t send us national insurance numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,6 @@ class User < ApplicationRecord
       date_of_birth: provider_data.info.date_of_birth,
       trn:,
       trn_verified: trn.present?,
-      national_insurance_number: provider_data.info.nino,
       full_name: provider_data.info.full_name,
       raw_tra_provider_data: provider_data,
     )
@@ -48,7 +47,6 @@ class User < ApplicationRecord
       date_of_birth: provider_data.info.date_of_birth,
       trn:,
       trn_verified: trn.present?,
-      national_insurance_number: provider_data.info.nino,
       full_name: provider_data.info.full_name,
       raw_tra_provider_data: provider_data,
     )
