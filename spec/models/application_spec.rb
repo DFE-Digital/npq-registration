846c2d77a3d6c2123e388daab1b491a68dcd72ef
Peter Yates <peter.yates@graphia.co.uk>
2022-11-18 16:07:21 +0000

    Add an unsynced user list to the admin section
    Unsynced users are users who have completed their application but we
    failed to link their record with an ECF user. This usually happens
    because they've used more than one email address and we already have
    their `ecf_id` on record.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,4 +1,11 @@
 require "rails_helper"
 
 RSpec.describe Application do
+  describe "scopes" do
+    describe ".unsynced" do
+      it "returns records where ecf_id is null" do
+        expect(described_class.unsynced.to_sql).to match(%("ecf_id" IS NULL))
+      end
+    end
+  end
 end
