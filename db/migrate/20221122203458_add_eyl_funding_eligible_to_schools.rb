f97b5f9e217f2cf5c819dc8bde252b8d4190f1eb
Leonidas Apostolidis <leoapost@gmail.com>
2022-11-30 16:19:25 +0000

    Fix the eyl_funding_eligible migration class name

--- db/migrate/20221122203458_add_eyl_funding_eligible_to_schools.rb
+++ db/migrate/20221122203458_add_eyl_funding_eligible_to_schools.rb
@@ -1,4 +1,4 @@
-class AddEylEligibleToSchools < ActiveRecord::Migration[6.1]
+class AddEylFundingEligibleToSchools < ActiveRecord::Migration[6.1]
   def change
     add_column :schools, :eyl_funding_eligible, :boolean, default: false
   end
