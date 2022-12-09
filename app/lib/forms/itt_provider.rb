90627d253d067207b38c90cff0441889d6df7efd
Sam Johnson <sh.johnson@me.com>
2022-12-09 19:37:49 +0000

    Add in approved itt providers form and view plus the csv for latest data

---
+++ app/lib/forms/itt_provider.rb
@@ -0,0 +1,35 @@
+module Forms
+  class ITTProvider < Base
+    QUESTION_NAME = :itt_provider
+
+    attr_accessor QUESTION_NAME
+
+    validates QUESTION_NAME, presence: true
+
+    def self.permitted_params
+      [QUESTION_NAME]
+    end
+
+    def question
+      Forms::QuestionTypes::TextField.new(name: QUESTION_NAME)
+    end
+
+    def next_step
+      if approved_itt_provider # check if its the NPQLTD course
+        :choose_your_npq
+      else
+
+      end
+    end
+
+    def previous_step
+      :employment_type
+    end
+
+    private
+
+    def approved_itt_provider
+      # look up the provider infomration
+    end
+  end
+end
