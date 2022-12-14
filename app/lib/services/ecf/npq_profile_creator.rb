165e0199b5ea550dad18c987c4d4997a363727fc
Naomi Lockhart <naomi@lockyy.com>
2022-12-14 16:08:11 +0000

    Create EcfSyncRequestLog when syncing apps to ecf

--- app/lib/services/ecf/npq_profile_creator.rb
+++ app/lib/services/ecf/npq_profile_creator.rb
@@ -38,10 +38,28 @@ module Services
           },
         )
 
+        # JsonApiClient::Resource uses errors for flow control, so failed saves
+        # will divert to the rescue block below
+        # I'd prefer to use the return value of save, but that's not possible
         profile.save
+
         application.update!(
           ecf_id: profile.id,
           teacher_catchment_synced_to_ecf: true,
+        )
+
+        EcfSyncRequestLog.create(
+          sync_type: :application_creation,
+          syncable: application,
+          status: :success,
+        )
+      rescue StandardError => e
+        EcfSyncRequestLog.create(
+          sync_type: :application_creation,
+          syncable: application,
+          status: :failed,
+          error_messages: ["#{e.class} - #{e.message}"],
+          response_body: e.env["response_body"],
         )
       end
 
--- spec/lib/services/ecf/npq_profile_creator_spec.rb
+++ spec/lib/services/ecf/npq_profile_creator_spec.rb
@@ -117,17 +117,127 @@ RSpec.describe Services::Ecf::NpqProfileCreator do
           },
         )
         .to_return(
-          status: 200,
-          body: response_body,
+          status: response_code,
+          body: response_body.to_json,
           headers: {
             "Content-Type" => "application/vnd.api+json",
           },
         )
     end
 
-    it "sets application.ecf_id with returned guid" do
-      subject.call
-      expect(application.ecf_id).to eql("789")
+    context "when authorized" do
+      let(:response_code) { 200 }
+
+      let(:response_body) do
+        {
+          data: {
+            type: "npq_profiles",
+            id: "789",
+          },
+        }
+      end
+
+      it "sets application.ecf_id with returned guid" do
+        expect {
+          subject.call
+        }.to change(application, :ecf_id).to("789")
+      end
+
+      it "creates a EcfSyncRequestLog with status :success" do
+        expect {
+          subject.call
+        }.to change(EcfSyncRequestLog, :count).by(1)
+
+        expect(
+          EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+        ).to match(
+          "syncable" => application,
+          "status" => "success",
+          "error_messages" => [],
+          "response_body" => nil,
+          "sync_type" => "application_creation",
+        )
+      end
+
+      context "when save fails" do
+        let(:error_array) do
+          [
+            {
+              "title": "Participant identity blank",
+              "detail": "must exist",
+            },
+            {
+              "title": "NPQ lead provider blank",
+              "detail": "must exist",
+            },
+            {
+              "title": "NPQ course blank",
+              "detail": "must exist",
+            },
+          ]
+        end
+        let(:response_body) do
+          {
+            "errors": error_array,
+          }
+        end
+
+        let(:response_code) { 400 }
+
+        it "does not set application.ecf_id " do
+          expect {
+            subject.call
+          }.to_not change(application, :ecf_id)
+        end
+
+        it "creates a EcfSyncRequestLog with status :failed" do
+          expect {
+            subject.call
+          }.to change(EcfSyncRequestLog, :count).by(1)
+
+          expect(
+            EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+          ).to match(
+            "syncable" => application,
+            "status" => "failed",
+            "error_messages" => ["JsonApiClient::Errors::ClientError - Participant identity blank; NPQ lead provider blank; NPQ course blank"],
+            "response_body" => response_body,
+            "sync_type" => "application_creation",
+          )
+        end
+      end
+    end
+
+    context "when unauthorized" do
+      let(:response_code) { 401 }
+
+      let(:response_body) do
+        {
+          "error" => "HTTP Token: Access denied",
+        }
+      end
+
+      it "does not set application.ecf_id " do
+        expect {
+          subject.call
+        }.to_not change(application, :ecf_id)
+      end
+
+      it "creates a EcfSyncRequestLog with status :failed" do
+        expect {
+          subject.call
+        }.to change(EcfSyncRequestLog, :count).by(1)
+
+        expect(
+          EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+        ).to match(
+          "syncable" => application,
+          "status" => "failed",
+          "error_messages" => ["JsonApiClient::Errors::NotAuthorized - JsonApiClient::Errors::NotAuthorized"],
+          "response_body" => response_body,
+          "sync_type" => "application_creation",
+        )
+      end
     end
   end
 end
