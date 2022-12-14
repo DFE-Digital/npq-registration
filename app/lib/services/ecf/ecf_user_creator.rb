0dda480fe715714c2addcfc878272f805203bfcd
Naomi Lockhart <naomi@lockyy.com>
2022-12-14 16:08:03 +0000

    Create EcfSyncRequestLog when syncing user to ecf

--- app/lib/services/ecf/ecf_user_creator.rb
+++ app/lib/services/ecf/ecf_user_creator.rb
@@ -9,8 +9,26 @@ module Services
 
       def call
         remote = EcfApi::User.new(email: user.email, full_name: user.full_name)
+
+        # JsonApiClient::Resource uses errors for flow control, so failed saves
+        # will divert to the rescue block below
+        # I'd prefer to use the return value of save, but that's not possible
         remote.save
         user.update!(ecf_id: remote.id)
+
+        EcfSyncRequestLog.create(
+          sync_type: :user_creation,
+          syncable: user,
+          status: :success,
+        )
+      rescue StandardError => e
+        EcfSyncRequestLog.create(
+          sync_type: :user_creation,
+          syncable: user,
+          status: :failed,
+          error_messages: ["#{e.class} - #{e.message}"],
+          response_body: e.env["response_body"],
+        )
       end
     end
   end
--- spec/lib/services/ecf/ecf_user_creator_spec.rb
+++ spec/lib/services/ecf/ecf_user_creator_spec.rb
@@ -18,15 +18,6 @@ RSpec.describe Services::Ecf::EcfUserCreator do
       }.to_json
     end
 
-    let(:response_body) do
-      {
-        data: {
-          type: "user",
-          id: "123",
-        },
-      }.to_json
-    end
-
     before do
       stub_request(:post, "https://ecf-app.gov.uk/api/v1/users")
         .with(
@@ -38,18 +29,121 @@ RSpec.describe Services::Ecf::EcfUserCreator do
           },
         )
         .to_return(
-          status: 201,
-          body: response_body,
+          status: response_code,
+          body: response_body.to_json,
           headers: {
             "Content-Type" => "application/vnd.api+json",
           },
         )
     end
 
-    it "sets user.ecf_id with returned guid" do
-      subject.call
-      user.reload
-      expect(user.ecf_id).to eql("123")
+    context "when authorized" do
+      let(:response_code) { 201 }
+      let(:response_body) do
+        {
+          data: {
+            type: "user",
+            id: "123",
+          },
+        }
+      end
+
+      it "sets user.ecf_id with returned guid" do
+        expect {
+          subject.call
+        }.to change(user, :ecf_id).to("123")
+      end
+
+      it "creates a EcfSyncRequestLog with status :success" do
+        expect {
+          subject.call
+        }.to change(EcfSyncRequestLog, :count).by(1)
+
+        expect(
+          EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+        ).to match(
+          "syncable" => user,
+          "status" => "success",
+          "error_messages" => [],
+          "response_body" => nil,
+          "sync_type" => "user_creation",
+        )
+      end
+
+      context "when save fails" do
+        let(:error_array) do
+          [
+            {
+              "title" => "Invalid action",
+              "detail" => "Validation failed: Email Enter an email address in the correct format, like name@example.com",
+            },
+          ]
+        end
+
+        let(:response_body) do
+          {
+            "errors": error_array,
+          }
+        end
+
+        let(:response_code) { 400 }
+
+        it "does not set application.ecf_id " do
+          expect {
+            subject.call
+          }.to_not change(user, :ecf_id)
+        end
+
+        it "creates a EcfSyncRequestLog with status :failed" do
+          expect {
+            subject.call
+          }.to change(EcfSyncRequestLog, :count).by(1)
+
+          expect(
+            EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+          ).to match(
+            "syncable" => user,
+            "status" => "failed",
+            "error_messages" => [
+              "JsonApiClient::Errors::ClientError - Invalid action",
+            ],
+            "response_body" => response_body,
+            "sync_type" => "user_creation",
+          )
+        end
+      end
+    end
+
+    context "when unauthorized" do
+      let(:response_code) { 401 }
+
+      let(:response_body) do
+        {
+          "error" => "HTTP Token: Access denied",
+        }
+      end
+
+      it "does not set application.ecf_id " do
+        expect {
+          subject.call
+        }.to_not change(user, :ecf_id)
+      end
+
+      it "creates a EcfSyncRequestLog with status :failed" do
+        expect {
+          subject.call
+        }.to change(EcfSyncRequestLog, :count).by(1)
+
+        expect(
+          EcfSyncRequestLog.last.slice(:syncable, :status, :error_messages, :response_body, :sync_type),
+        ).to match(
+          "syncable" => user,
+          "status" => "failed",
+          "error_messages" => ["JsonApiClient::Errors::NotAuthorized - JsonApiClient::Errors::NotAuthorized"],
+          "response_body" => response_body,
+          "sync_type" => "user_creation",
+        )
+      end
     end
   end
 end
