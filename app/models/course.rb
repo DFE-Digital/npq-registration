d1848a585d4db1fd95ddf986595ef3e220ccd381
Naomi Lockhart <naomi@lockyy.com>
2023-02-20 14:17:46 +0000

    Remove COURSE_ECF_ID_TO_IDENTIFIER_MAPPING

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,17 +1,4 @@
 class Course < ApplicationRecord
-  COURSE_ECF_ID_TO_IDENTIFIER_MAPPING = {
-    "15c52ed8-06b5-426e-81a2-c2664978a0dc" => "npq-leading-teaching",
-    "7d47a0a6-fa74-4587-92cc-cd1e4548a2e5" => "npq-leading-behaviour-culture",
-    "29fee78b-30ce-4b93-ba21-80be2fde286f" => "npq-leading-teaching-development",
-    "a42736ad-3d0b-401d-aebe-354ef4c193ec" => "npq-senior-leadership",
-    "0f7d6578-a12c-4498-92a0-2ee0f18e0768" => "npq-headship",
-    "aef853f2-9b48-4b6a-9d2a-91b295f5ca9a" => "npq-executive-leadership",
-    "7fbefdd4-dd2d-4a4f-8995-d59e525124b7" => "npq-additional-support-offer",
-    "0222d1a8-a8e1-42e3-a040-2c585f6c194a" => "npq-early-headship-coaching-offer",
-    "66dff4af-a518-498f-9042-36a41f9e8aa7" => "npq-early-years-leadership",
-    "829fcd45-e39d-49a9-b309-26d26debfa90" => "npq-leading-literacy",
-  }.with_indifferent_access.freeze
-
   scope :aso, -> { where(identifier: "npq-additional-support-offer") }
   scope :ehco, -> { where(identifier: "npq-early-headship-coaching-offer") }
   scope :npqeyl, -> { where(identifier: "npq-early-years-leadership") }
--- db/migrate/20230220114340_add_identifier_to_courses.rb
+++ db/migrate/20230220114340_add_identifier_to_courses.rb
@@ -2,8 +2,6 @@ class AddIdentifierToCourses < ActiveRecord::Migration[6.1]
   def change
     add_column :courses, :identifier, :string
 
-    Course::COURSE_ECF_ID_TO_IDENTIFIER_MAPPING.each do |ecf_id, identifier|
-      Course.find_by!(ecf_id:).update(identifier:)
-    end
+    Services::Courses::DefinitionLoader.call
   end
 end
