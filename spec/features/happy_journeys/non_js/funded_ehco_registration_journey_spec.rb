b399600aa47a372b97aa59c3f944eaae20d20e84
Naomi Lockhart <naomi@lockyy.com>
2023-02-20 14:49:21 +0000

    Correct display text for EHCO
    c88

--- spec/features/happy_journeys/non_js/funded_ehco_registration_journey_spec.rb
+++ spec/features/happy_journeys/non_js/funded_ehco_registration_journey_spec.rb
@@ -142,7 +142,7 @@ RSpec.feature "Happy journeys", type: :feature do
 
     navigate_to_page(path: "/account", submit_form: false, axe_check: false) do
       expect(page).to have_text("Teach First")
-      expect(page).to have_text("Early Headship Coaching Offer")
+      expect(page).to have_text("Early headship coaching offer")
     end
 
     visit "/registration/check-answers"
