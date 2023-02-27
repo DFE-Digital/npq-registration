dfc64f07783210f9e91c29d49bfae278cc95e05d
Naomi Lockhart <naomi@lockyy.com>
2023-02-27 14:03:45 +0000

    Build URL in previous funding abstraction

--- spec/support/helpers/api_mocking.rb
+++ spec/support/helpers/api_mocking.rb
@@ -1,10 +1,19 @@
 def mock_previous_funding_api_request(course_identifier:, trn:, response:)
-  stub_request(:get, "https://ecf-app.gov.uk/api/v1/npq-funding/#{trn}?npq_course_identifier=#{course_identifier}")
+  base_url = "https://ecf-app.gov.uk/api/v1/npq-funding/#{trn}"
+
+  query_params = {
+    npq_course_identifier: course_identifier,
+  }
+
+  query_string = CGI.unescape(query_params.to_query)
+  url = "#{base_url}?#{query_string}"
+
+  stub_request(:get, url)
     .with(
       headers: {
         "Authorization" => "Bearer ECFAPPBEARERTOKEN",
       },
-      )
+    )
     .to_return(
       status: 200,
       body: response,
