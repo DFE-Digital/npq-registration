0b04bd92a5c4178d83717867b2862dcd7b0a40c8
Naomi Lockhart <naomi@lockyy.com>
2023-03-03 15:27:17 +0000

    Update context API stubs to support GAI_ID

--- spec/support/helpers/api_mocking.rb
+++ spec/support/helpers/api_mocking.rb
@@ -1,11 +1,11 @@
-def mock_previous_funding_api_request(course_identifier:, trn: nil, get_an_identity_id: nil, response:)
+def mock_previous_funding_api_request(course_identifier:, response:, trn: nil, get_an_identity_id: nil)
   base_url = "https://ecf-app.gov.uk/api/v1/npq/previous_funding"
 
   query_params = {
     npq_course_identifier: course_identifier,
-    trn:,
-    get_an_identity_id:
-  }.compact
+  }
+  query_params[:trn] = trn if trn.present?
+  query_params[:get_an_identity_id] = get_an_identity_id if get_an_identity_id.present?
 
   query_string = CGI.unescape(query_params.to_query)
   url = "#{base_url}?#{query_string}"
