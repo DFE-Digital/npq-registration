c51b82dabc52adb7ff3492597d8d67b7b435218a
Naomi Lockhart <naomi@lockyy.com>
2023-04-12 12:08:14 +0100

    Drop default for applications.cohort
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_31_120924) do
+ActiveRecord::Schema.define(version: 2023_04_12_110012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2023_03_31_120924) do
     t.boolean "works_in_nursery"
     t.boolean "works_in_childcare"
     t.text "kind_of_nursery"
-    t.integer "cohort", default: 2021
+    t.integer "cohort"
     t.boolean "targeted_delivery_funding_eligibility", default: false
     t.string "funding_eligiblity_status_code"
     t.jsonb "raw_application_data", default: {}
