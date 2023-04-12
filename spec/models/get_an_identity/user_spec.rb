b106956411431fcc613956fcaf6ae8717e27cfa0
Naomi Lockhart <naomi@lockyy.com>
2023-04-12 10:13:55 +0100

    Remove extraneous test

--- spec/models/get_an_identity/user_spec.rb
+++ spec/models/get_an_identity/user_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe GetAnIdentity::User do
     let(:access_token) { SecureRandom.uuid }
     let(:stubbed_url) { "https://example.com" }
 
-    let(:uid) { "123456" }
+    let(:uid) { SecureRandom.uuid }
 
     before do
       allow(ENV).to receive(:fetch).with("TRA_OIDC_DOMAIN").and_return(stubbed_url)
@@ -27,10 +27,9 @@ RSpec.describe GetAnIdentity::User do
     end
 
     context "when the response is successful" do
-      let(:response_uid) { uid }
       let(:response_body) do
         {
-          "userId" => response_uid,
+          "userId" => "654321",
           "email" => "mail@example.com",
           "firstName" => "Jane",
           "lastName" => "Doe",
@@ -44,12 +43,12 @@ RSpec.describe GetAnIdentity::User do
       let(:response_status_code) { 200 }
 
       let(:loaded_user) do
-        described_class.find("123456")
+        described_class.find(uid)
       end
 
       it "returns a user that responds to the correct attributes" do
         expect(loaded_user.id).to eq uid
-        expect(loaded_user.uid).to eq response_uid
+        expect(loaded_user.uid).to eq "654321"
         expect(loaded_user.email).to eq "mail@example.com"
         expect(loaded_user.full_name).to eq "Jane Doe"
         expect(loaded_user.date_of_birth).to eq Date.new(1990, 1, 1)
@@ -57,22 +56,6 @@ RSpec.describe GetAnIdentity::User do
         expect(loaded_user.mobile_number).to eq ""
         expect(loaded_user.merged_user_ids).to eq []
         expect(loaded_user.raw).to eq response_body
-      end
-
-      context "when uid from response does not match the requested uid" do
-        let(:response_uid) { "654321" }
-
-        it "returns a user that responds to the correct attributes" do
-          expect(loaded_user.id).to eq uid
-          expect(loaded_user.uid).to eq response_uid
-          expect(loaded_user.email).to eq "mail@example.com"
-          expect(loaded_user.full_name).to eq "Jane Doe"
-          expect(loaded_user.date_of_birth).to eq Date.new(1990, 1, 1)
-          expect(loaded_user.trn).to eq "1234567"
-          expect(loaded_user.mobile_number).to eq ""
-          expect(loaded_user.merged_user_ids).to eq []
-          expect(loaded_user.raw).to eq response_body
-        end
       end
     end
 
