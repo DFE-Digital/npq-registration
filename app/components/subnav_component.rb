170a89893008b11caced8df027208b7c08057300
Sam Johnson <sh.johnson@me.com>
2023-05-26 16:35:34 +0100

    fix component issues

--- app/components/subnav_component.rb
+++ app/components/subnav_component.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class SubnavComponent < BaseComponent
-  include ViewComponent::SlotableV2
+  include ViewComponent::Slotable
 
-  renders_many :nav_items, "NavItemComponent"
+  renders_many :nav_items, 'NavItemComponent'
 
   class NavItemComponent < BaseComponent
     attr_reader :path
