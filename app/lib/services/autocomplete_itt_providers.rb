45e2086f6484c1c1a3f4d18eb94289cb20fd7cc8
Stanislaw Klajn <sklajn@gmail.com>
2023-06-21 01:43:23 +0100

    ITT picker aliases

--- app/lib/services/autocomplete_itt_providers.rb
+++ app/lib/services/autocomplete_itt_providers.rb
@@ -5,6 +5,12 @@ class Services::AutocompleteIttProviders
     end
   end
 
+  def self.as_autocomplete_options
+    IttProvider.find_each.map do |provider|
+      [provider.legal_name, provider.legal_name, { 'data-additional-synonyms': [provider.operating_name].to_json }]
+    end
+  end
+
   def self.names
     @names ||= IttProvider.all.pluck(:legal_name)
   end
--- app/webpacker/packs/itt-provider-picker.js
+++ app/webpacker/packs/itt-provider-picker.js
@@ -4,8 +4,30 @@ import openregisterLocationPicker from 'govuk-country-and-territory-autocomplete
 const $allIttProvidersAutoCompleteElements = document.querySelectorAll('[data-module="app-itt-provider-autocomplete"]')
 
 nodeListForEach($allIttProvidersAutoCompleteElements, (component) => {
+  const options = component.querySelectorAll('select option');
+  const optionsMap = []
+  nodeListForEach(options, (option) => {
+    optionsMap.push({ search: option.text.toLowerCase(), text: option.text, value: option.value })
+
+    if (option.dataset.additionalSynonyms) {
+      JSON.parse(option.dataset.additionalSynonyms).forEach(alias => {
+        optionsMap.push({search: alias.toLowerCase(), text: alias, value: option.value})
+      })
+    }
+  })
+  console.log(window.opts = optionsMap)
+
   openregisterLocationPicker({
     selectElement: component.querySelector('select'),
-    url: ''
+    url: '',
+    source (query, callback) {
+      const distinct = {}
+
+      const filtered = optionsMap.filter(option => option.search.includes(query.toLowerCase()))
+      filtered.forEach(option => {
+        distinct[option.value] = distinct[option.value] || { path: option.text !== option.value && option.text , name: option.value }
+      })
+      callback(Object.values(distinct))
+    }
   })
 })
