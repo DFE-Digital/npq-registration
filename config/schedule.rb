4132379959268ae40a8a15a7e233c73d595ac5cc
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-07-03 15:37:52 +0100

    added display functionality for provider status

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "config", "environment"))
 
-every 3.hours do
+every 1.day, at: "00:00" do
   runner "ApplicationSynchronizationJob.perform_now", environment: Rails.env
 end
