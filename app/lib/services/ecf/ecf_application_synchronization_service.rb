4322a05dda94970ff5d217f5cc08d2f3b49bfad3
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-07-03 12:11:33 +0100

    removed the payload data code for ecf endpoint

--- app/lib/services/ecf/ecf_application_synchronization_service.rb
+++ app/lib/services/ecf/ecf_application_synchronization_service.rb
@@ -3,9 +3,7 @@ module Services
     class EcfApplicationSynchronizationService
       def call
         uri = build_uri
-        required_application_ids = fetch_required_application_ids
-        params = build_request_params(required_application_ids)
-        request = build_http_get_request(uri, params)
+        request = build_http_get_request(uri)
         response = send_http_request(uri, request)
         handle_response(response)
       rescue StandardError => e
@@ -18,18 +16,9 @@ module Services
         URI.parse("#{ENV['ECF_APP_BASE_URL']}/api/v1/npq/application_synchronizations#index")
       end
 
-      def fetch_required_application_ids
-        Application.where(lead_provider_approval_status: nil).limit(1000).pluck(:ecf_id)
-      end
-
-      def build_request_params(application_ids)
-        { ids: application_ids.join(",") }
-      end
-
-      def build_http_get_request(uri, params)
+      def build_http_get_request(uri)
         request = Net::HTTP::Get.new(uri)
         request["Authorization"] = "Bearer #{ENV['ECF_APP_BEARER_TOKEN']}"
-        request.body = URI.encode_www_form(params)
         request
       end
 
