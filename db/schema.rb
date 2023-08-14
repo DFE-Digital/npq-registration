58f1abe42d1cbd94297ea8e3e46b487278a4bc9e
javier-npq <javier.fernandez@education.gov.uk>
2023-08-14 23:17:41 +0100

    Added updated schools list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_20_141121) do
+ActiveRecord::Schema.define(version: 2023_08_02_120824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
