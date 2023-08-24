b9da3871a497180d540110621733023b18c8befd
Pablo Manrubia <pmanrubia@gmail.com>
2023-08-24 08:27:03 +0200

    Sync get_identity_id to ECF

--- app/lib/services/importers/populate_get_identity_id.rb
+++ app/lib/services/importers/populate_get_identity_id.rb
@@ -6,6 +6,13 @@ module Services
           user = User.find(row.fetch(:user_id))
           user.update!(uid: row.fetch(:id))
           Rails.logger.info("User #{user.id} has been updated with get_identity_id #{row.fetch(:id)}")
+
+          if user.synced_to_ecf?
+            user.ecf_user.update!(get_identity_id: row.fetch(:id))
+            Rails.logger.info("ECF user #{user.id} has been updated with get_identity_id #{row.fetch(:id)}")
+          else
+            Rails.logger.error("User #{user.id} get_identity_id has not been synced to ECF")
+          end
         end
       end
     end
--- spec/lib/services/importers/populate_get_identity_id_spec.rb
+++ spec/lib/services/importers/populate_get_identity_id_spec.rb
@@ -35,4 +35,26 @@ RSpec.describe Services::Importers::PopulateGetIdentityId do
 
     expect(logger).to have_received(:info).with("User #{user.id} has been updated with get_identity_id 123")
   end
+
+  describe "Syncing to ECF" do
+    it "Updates the get_identity_id for the ECF user" do
+      user = create(:user, ecf_id: SecureRandom.uuid)
+      ecf_user = instance_spy(EcfApi::Npq::User)
+
+      allow_any_instance_of(User).to receive(:ecf_user).and_return(ecf_user)
+      expect(ecf_user).to receive(:update!).with(get_identity_id: "123")
+
+      described_class.new.import([{ id: "123", user_id: user.id }])
+    end
+
+    it "Logs an error if user is not synched with ECF" do
+      user = create(:user, ecf_id: nil)
+      logger = instance_spy(Logger)
+      allow(Rails).to receive(:logger).and_return(logger)
+
+      described_class.new.import([{ id: "123", user_id: user.id }])
+
+      expect(logger).to have_received(:error).with("User #{user.id} get_identity_id has not been synced to ECF")
+    end
+  end
 end
