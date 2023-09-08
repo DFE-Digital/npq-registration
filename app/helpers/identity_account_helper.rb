6c0cefc58e3ec976d45cdafc6ac59f0c97be3860
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-09-08 15:54:19 +0100

    updated link_to_identity_account

--- app/helpers/identity_account_helper.rb
+++ app/helpers/identity_account_helper.rb
@@ -7,11 +7,7 @@ module IdentityAccountHelper
     ENV.fetch("TRA_OIDC_CLIENT_ID")
   end
 
-  def redirect_uri
-    JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"].last
-  end
-
-  def link_to_identity_account
+  def link_to_identity_account(redirect_uri)
     "#{tra_oidc_domain}account?client_id=#{tra_oidc_client_id}&redirect_uri=#{redirect_uri}&sign_out_uri=#{redirect_uri}"
   end
 end
