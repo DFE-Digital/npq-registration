39a454ff889ed1c948d65a6cd1347c6898ca41da
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-09-08 15:54:19 +0100

    updated link_to_identity_account

--- spec/support/helpers/journey_helper.rb
+++ spec/support/helpers/journey_helper.rb
@@ -35,12 +35,9 @@ module Helpers
         .to_return(status: 200, body: participant_validator_response(**response), headers: {})
     end
 
-    def stub_env_variables_for_gai(stubbed_url: "https://example.com", stubbed_client_id: "register-for-npq", stubbed_redirect_uri: "https://example.com/")
+    def stub_env_variables_for_gai(stubbed_url: "https://example.com", stubbed_client_id: "register-for-npq")
       stub_const("ENV", ENV.to_hash.merge("TRA_OIDC_DOMAIN" => stubbed_url))
       stub_const("ENV", ENV.to_hash.merge("TRA_OIDC_CLIENT_ID" => stubbed_client_id))
-      vcap_application = JSON.parse(ENV["VCAP_APPLICATION"])
-      vcap_application["application_uris"] << stubbed_redirect_uri
-      stub_const("ENV", ENV.to_hash.merge("VCAP_APPLICATION" => vcap_application.to_json))
     end
   end
 end
