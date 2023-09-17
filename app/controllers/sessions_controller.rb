ca00c059bbf509a403e89438b95a83a069839f0e
Pablo Manrubia <pmanrubia@gmail.com>
2023-09-17 09:27:42 +0200

    Use URI API to build redirect API

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,27 @@
 class SessionsController < ApplicationController
   def destroy
     sign_out_all_scopes
-    reset_session
-    session.clear
-    # redirect_to root_path
 
-    root_path = "http://localhost:3000/"
-    redirect_to "https://preprod.teaching-identity.education.gov.uk/connect/signout?client_id=register-for-npq=&post_logout_redirect_uri=#{CGI.escape(root_path)}"
+    root_uri = URI::HTTP.build({
+      scheme: request.protocol.chomp("://"), # "http://".chomp("://") -> "http"
+      host: request.host,
+      port: request.port,
+      path: "/"
+    })
+
+    tra_domain_uri = URI.parse(ENV["TRA_OIDC_DOMAIN"])
+
+    logout_uri = URI::Generic.build({
+      scheme: tra_domain_uri.scheme,
+      host: tra_domain_uri.host,
+      port: tra_domain_uri.port, # This will be nil if not provided, and the build method will ignore it.
+      path: "/connect/signout",
+      query: URI.encode_www_form({
+        "client_id" => "register-for-npq",
+        "post_logout_redirect_uri" => root_uri.to_s
+      })
+    })
+
+    redirect_to logout_uri.to_s
   end
 end
