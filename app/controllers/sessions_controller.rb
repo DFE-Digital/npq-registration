1d9d2517290b08dba06f6bfaba7a9ab47ec4bb38
Pablo Manrubia <pmanrubia@gmail.com>
2023-09-17 09:31:08 +0200

    DRY-up SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,26 +2,32 @@ class SessionsController < ApplicationController
   def destroy
     sign_out_all_scopes
 
-    root_uri = URI::HTTP.build({
-      scheme: request.protocol.chomp("://"), # "http://".chomp("://") -> "http"
-      host: request.host,
-      port: request.port,
-      path: "/"
-    })
+    redirect_to build_sign_out_uri
+  end
 
+private
+
+  def build_sign_out_uri
     tra_domain_uri = URI.parse(ENV["TRA_OIDC_DOMAIN"])
 
-    logout_uri = URI::Generic.build({
+    URI::Generic.build({
       scheme: tra_domain_uri.scheme,
       host: tra_domain_uri.host,
-      port: tra_domain_uri.port, # This will be nil if not provided, and the build method will ignore it.
+      port: tra_domain_uri.port,
       path: "/connect/signout",
       query: URI.encode_www_form({
         "client_id" => "register-for-npq",
-        "post_logout_redirect_uri" => root_uri.to_s
-      })
-    })
+        "post_logout_redirect_uri" => root_uri.to_s,
+      }),
+    }).to_s
+  end
 
-    redirect_to logout_uri.to_s
+  def root_uri
+    URI::HTTP.build({
+      scheme: request.protocol.chomp("://"),
+      host: request.host,
+      port: request.port,
+      path: "/",
+    })
   end
 end
