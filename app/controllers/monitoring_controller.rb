5e9d56a33d340c6f611cef495e6640fa97f5e9f4
Peter Yates <peter.yates@graphia.co.uk>
2023-09-21 16:36:53 +0100

    Just render JSON regardless

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -1,9 +1,5 @@
 class MonitoringController < ActionController::Base
   def healthcheck
-    respond_to do |format|
-      format.json do
-        render json: { status: "OK", git_commit_sha: ENV["GIT_COMMIT_SHA"], docker_image_id: ENV["DOCKER_IMAGE_ID"] }
-      end
-    end
+    render json: { status: "OK", git_commit_sha: ENV["GIT_COMMIT_SHA"], docker_image_id: ENV["DOCKER_IMAGE_ID"] }
   end
 end
