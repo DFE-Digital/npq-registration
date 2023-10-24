72f961c7e3b50389b6cc8d7f116538a9dfa9fc82
Pablo Manrubia <pmanrubia@gmail.com>
2023-10-24 22:01:05 +0100

    Remove Job for testing from wrong folder

--- spec/jobs/cron_test_job.rb
+++
@@ -1,8 +0,0 @@
-class CronTestJop < CronJob
-  # set the cron expression to run every 10 minutes
-  self.cron_expression = '*/10 * * * *'
-
-  def perform
-    Sentry.capture_message("Cron job is working")
-  end
-end
