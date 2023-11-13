581b7877c1e81b91df4fa101d4374e65c9edc454
Pablo Manrubia <pmanrubia@gmail.com>
2023-11-13 22:01:32 +0000

    Add monitor to sweep stale sessions

--- app/jobs/crons/sweep_stale_sessions_job.rb
+++ app/jobs/crons/sweep_stale_sessions_job.rb
@@ -1,6 +1,10 @@
 class Crons::SweepStaleSessionsJob < CronJob
+  include Sentry::Cron::MonitorCheckIns
+
   # run at 3:30 AM every day
   self.cron_expression = "30 3 * * *"
+
+  sentry_monitor_check_ins slug: "sweep-stale-sessions"
 
   def perform
     SweepStaleSessionsJob.perform_later
