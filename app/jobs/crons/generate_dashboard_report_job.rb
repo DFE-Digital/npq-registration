09117cc61c760b423c5abcc7ebe24c6470ec57fc
Pablo Manrubia <pmanrubia@gmail.com>
2023-11-13 21:57:51 +0000

    Add monitor for generate dashboard report

--- app/jobs/crons/generate_dashboard_report_job.rb
+++ app/jobs/crons/generate_dashboard_report_job.rb
@@ -1,6 +1,10 @@
 class Crons::GenerateDashboardReportJob < CronJob
+  include Sentry::Cron::MonitorCheckIns
+
   # Run every hour
   self.cron_expression = "0 * * * *"
+
+  sentry_monitor_check_ins slug: "generate-dashboard"
 
   def perform
     DashboardReportJob.perform_later
