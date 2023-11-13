0b9515f6979089ebdaa7b49747d4d6b89c4a52a4
Pablo Manrubia <pmanrubia@gmail.com>
2023-11-13 22:03:34 +0000

    Add monitor to update application statuses

--- app/jobs/crons/update_applications_statuses_job.rb
+++ app/jobs/crons/update_applications_statuses_job.rb
@@ -1,6 +1,10 @@
 class Crons::UpdateApplicationsStatusesJob < CronJob
+  include Sentry::Cron::MonitorCheckIns
+
   # run every two hours
   self.cron_expression = "0 */2 * * *"
+
+  sentry_monitor_check_ins slug: "update-application-statuses"
 
   def perform
     ApplicationSynchronizationJob.perform_later
