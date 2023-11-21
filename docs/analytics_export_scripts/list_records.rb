174045251fc338a225192d51869d8bed73693377
Slawosz Slawinski <slawosz@gmail.com>
2023-11-21 19:02:34 +0000

    Add documentation

---
+++ docs/analytics_export_scripts/list_records.rb
@@ -0,0 +1,14 @@
+require "google/cloud/bigquery"
+def list_records(project_id, dataset_id, table_id)
+  bigquery = Google::Cloud::Bigquery.new(project: project_id)
+  dataset = bigquery.dataset(dataset_id)
+  table = dataset.table(table_id)
+
+  query = "SELECT * FROM `#{dataset_id}.#{table_id}` LIMIT 10"
+  rows = bigquery.query(query)
+
+  rows.each do |row|
+    puts row
+  end
+end
+list_records "ecf-bq", "npq_registration", "cron_test_applications"
