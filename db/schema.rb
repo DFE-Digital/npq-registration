40c9bd28065743663ef5050b6176907fd64ceaf5
Javier Fernandez <javier.fernandez@education.gov.uk>
2023-12-08 16:22:19 +0500

    enabled pg_trm extanstion

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_08_083903) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_08_112007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "citext"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "applications", force: :cascade do |t|
@@ -56,8 +57,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_083903) do
     t.bigint "private_childcare_provider_id"
     t.index ["course_id"], name: "index_applications_on_course_id"
     t.index ["lead_provider_id"], name: "index_applications_on_lead_provider_id"
-    t.index ["school_id"], name: "index_applications_on_school_id"
     t.index ["private_childcare_provider_id"], name: "index_applications_on_private_childcare_provider_id"
+    t.index ["school_id"], name: "index_applications_on_school_id"
     t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
@@ -287,9 +288,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_08_083903) do
   end
 
   add_foreign_key "applications", "courses"
-  add_foreign_key "applications", "private_childcare_providers"
-  add_foreign_key "applications", "users"
   add_foreign_key "applications", "lead_providers"
+  add_foreign_key "applications", "private_childcare_providers"
   add_foreign_key "applications", "schools"
   add_foreign_key "applications", "users"
 end
