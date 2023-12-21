dd8d1c23ac9409815daad983c82d96899e40cfd6
Pablo Manrubia <pmanrubia@gmail.com>
2023-12-21 15:13:44 +0100

    Add validation for clawed_back status

--- spec/models/statement_item_spec.rb
+++ spec/models/statement_item_spec.rb
@@ -5,5 +5,31 @@ RSpec.describe StatementItem, type: :model do
     it { is_expected.to validate_presence_of(:statement_id) }
     it { is_expected.to validate_presence_of(:declaration_id) }
     it { is_expected.to validate_inclusion_of(:state).in_array(StatementItem::STATES) }
+
+    describe "clawed_back_by validation" do
+      context "when state is clawed_back" do
+        it "is invalid without a clawed_back_by" do
+          statement_item = described_class.new(state: "clawed_back", clawed_back_by: nil)
+
+          expect(statement_item).not_to be_valid
+          expect(statement_item.errors[:clawed_back_by]).to include("can't be blank")
+        end
+
+        it "is valid with a clawed_back_by" do
+          clawed_back_by_item = described_class.create!(state: "eligible")
+          statement_item = described_class.new(state: "clawed_back", clawed_back_by: clawed_back_by_item)
+
+          expect(statement_item).to be_valid
+        end
+      end
+
+      context "when state is not clawed_back" do
+        it "is valid without a clawed_back_by" do
+          statement_item = described_class.new(state: "eligible", clawed_back_by: nil)
+
+          expect(statement_item).to be_valid
+        end
+      end
+    end
   end
 end
