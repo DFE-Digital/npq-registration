136edfca0ea23bb52ad8950bdcf13d0fdfedf6d2
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-03 08:59:07 +0100

    Add state validations for Declaration

--- spec/models/declaration_spec.rb
+++ spec/models/declaration_spec.rb
@@ -2,11 +2,27 @@ require "rails_helper"
 
 RSpec.describe Declaration, type: :model do
   describe "validations" do
-    it { is_expected.to validate_presence_of(:application_id) }
-    it { is_expected.to validate_presence_of(:course_id) }
-    it { is_expected.to validate_presence_of(:lead_provider_id) }
-    it { is_expected.to validate_presence_of(:user_id) }
-    it { is_expected.to validate_inclusion_of(:state).in_array(Declaration::STATES) }
     it { is_expected.to belong_to(:application).required }
+
+    describe "state validations" do
+      let(:valid_states) { %w[submitted eligible ineligible payable voided paid awaiting_clawback clawed_back] }
+
+      it "is valid with a state included in STATES" do
+        valid_states.each do |state|
+          declaration = described_class.new(state:)
+          expect(declaration).to be_valid
+        end
+      end
+
+      it "is not valid with a state not included in STATES" do
+        declaration = described_class.new(state: "invalid_state")
+        expect(declaration).not_to be_valid
+      end
+
+      it "is not valid with a nil state" do
+        declaration = described_class.new(state: nil)
+        expect(declaration).not_to be_valid
+      end
+    end
   end
 end
