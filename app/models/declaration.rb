d6c7ee0089a7ce2092a459592d8b793ee808b3db
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-03 07:17:33 +0100

    Remove duplicated STATE definition

--- app/models/declaration.rb
+++ app/models/declaration.rb
@@ -16,13 +16,9 @@
 #   to prevent them from being made too early/late
 # * A declaration can change into many ‘states’ refer to here https://docs.google.com/presentation/d/1UMFxAZTs8r0_K7VlEZSxx2MYzB_9yG36gQrPgLy5R_Q/edit?usp=sharing
 class Declaration < ApplicationRecord
-  STATES = %w[submitted eligible payable paid voided ineligible awaiting_clawback clawed_back].freeze
-
   belongs_to :application
 
   validates :application_id, presence: true
-
-  validates :state, inclusion: { in: STATES }
 
   # validates :declaration_date - this depends on the schedule
   # theoretically the supplier can make multiple declarations at once, that *is* valid
