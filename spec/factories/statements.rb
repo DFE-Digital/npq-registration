6f791c22260c6c65280cd9a7585c920f7df1511c
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-09 18:23:58 +0100

    Remote top range validation in Statement

--- spec/factories/statements.rb
+++ spec/factories/statements.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     month { Faker::Number.between(from: 1, to: 12) }
     year { Faker::Number.between(from: 2022, to: 2024) }
     deadline_date { Faker::Date.forward(days: 30) }
-    cohort { build :cohort }
-    lead_provider { build :lead_provider }
+    cohort { association :cohort }
+    lead_provider { association :lead_provider }
     marked_as_paid_at { nil } # You can customize this attribute as needed
     reconcile_amount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
     created_at { Faker::Time.between(from: 2.years.ago, to: Time.zone.now) }
