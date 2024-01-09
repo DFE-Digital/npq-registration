c589b31f7c2e0fdca32536aad0cff38fa8c2ff3f
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-09 18:17:42 +0100

    Fix rubocop issues

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe Schedule, type: :model do
   it { is_expected.to belong_to(:cohort) }
   it { is_expected.to have_many(:applications) }
 
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:declaration_starts_on) }
-    it { should validate_presence_of(:schedule_applies_from) }
-    it { should validate_presence_of(:schedule_applies_to) }
-    it { should validate_presence_of(:declaration_types) }
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:declaration_starts_on) }
+    it { is_expected.to validate_presence_of(:schedule_applies_from) }
+    it { is_expected.to validate_presence_of(:schedule_applies_to) }
+    it { is_expected.to validate_presence_of(:declaration_types) }
   end
 end
-
