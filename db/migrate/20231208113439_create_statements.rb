9c5563422533843ef1e9f7a0894c707fb3a16a94
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-10 06:02:02 +0100

    Update `nil` attributes in Statements

--- db/migrate/20231208113439_create_statements.rb
+++ db/migrate/20231208113439_create_statements.rb
@@ -1,9 +1,10 @@
 class CreateStatements < ActiveRecord::Migration[7.0]
   def change
     create_table :statements do |t|
-      t.integer :month
-      t.integer :year
+      t.integer :month, null: false
+      t.integer :year, null: false
       t.date :deadline_date
+      t.date :payment_date
       t.references :cohort, null: false, foreign_key: true
       t.references :lead_provider, null: false, foreign_key: true
       t.datetime :marked_as_paid_at
