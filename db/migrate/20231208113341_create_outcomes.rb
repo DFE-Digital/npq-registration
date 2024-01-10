5d824d2253f2171b3f33fd7809933febdb1521b1
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-10 06:01:31 +0100

    Update `nil` attributes in Outcomes

--- db/migrate/20231208113341_create_outcomes.rb
+++ db/migrate/20231208113341_create_outcomes.rb
@@ -1,8 +1,8 @@
 class CreateOutcomes < ActiveRecord::Migration[7.0]
   def change
     create_table :outcomes do |t|
-      t.string :state
-      t.date :completion_date
+      t.string :state, null: false
+      t.date :completion_date, null: false
       t.references :declaration, null: false, foreign_key: true
 
       t.timestamps
