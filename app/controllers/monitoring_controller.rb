6a7c195f63adc01bec0bcbdc9776e94d06591c32
Peter Yates <peter.yates@graphia.co.uk>
2024-01-19 11:47:53 +0000

    Add database info to the healthcheck
    Also now it will return :internal_server_error when the database isn't
    connected or is (for some reason) unpopulated.

--- app/controllers/monitoring_controller.rb
+++ app/controllers/monitoring_controller.rb
@@ -1,5 +1,40 @@
 class MonitoringController < ActionController::Base
   def healthcheck
-    render json: { status: "OK", git_commit_sha: ENV["COMMIT_SHA"], docker_image_id: ENV["DOCKER_IMAGE_ID"] }
+    render status:, json: {
+      git_commit_sha: ENV["COMMIT_SHA"],
+      database: {
+        connected: database_connected?,
+        populated: database_populated?,
+        migration_version:,
+      },
+    }
+  end
+
+private
+
+  def status
+    if database_connected? && database_populated?
+      :ok
+    else
+      :internal_server_error
+    end
+  end
+
+  def database_connected?
+    ApplicationRecord.connection.select_value("SELECT 1") == 1
+  rescue StandardError
+    false
+  end
+
+  def database_populated?
+    [
+      ApplicationRecord.connection.select_value("select count(*) from courses;"),
+    ].all?(&:positive?)
+  rescue StandardError
+    false
+  end
+
+  def migration_version
+    ApplicationRecord.connection.migration_context.current_version
   end
 end
