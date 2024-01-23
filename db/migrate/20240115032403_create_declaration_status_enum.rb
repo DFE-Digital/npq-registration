3e662970a5e5f79b8d6fcbefa1f322c456d73338
Pablo Manrubia <pmanrubia@gmail.com>
2024-01-23 08:47:53 +0100

    use rails support to create enum types

--- db/migrate/20240115032403_create_declaration_status_enum.rb
+++ db/migrate/20240115032403_create_declaration_status_enum.rb
@@ -1,13 +1,9 @@
 class CreateDeclarationStatusEnum < ActiveRecord::Migration[7.1]
   def up
-    execute <<-SQL
-      CREATE TYPE declaration_status_enum AS ENUM ('eligible', 'payable', 'paid', 'voided', 'ineligible', 'awaiting_clawback', 'clawed_back');
-    SQL
+    create_enum :declaration_status_enum, ['eligible', 'payable', 'paid', 'voided', 'ineligible', 'awaiting_clawback', 'clawed_back']
   end
 
   def down
-    execute <<-SQL
-      DROP TYPE IF EXISTS declaration_status_enum;
-    SQL
+    drop_enum :declaration_status_enum
   end
 end
