11a84ea67389b36e0678990e7d082e551b693e8f
Peter Yates <peter.yates@graphia.co.uk>
2024-01-30 09:35:31 +0000

    Get simple test working and the openapi.yaml built

--- app/controllers/api/v1/applications_controller.rb
+++ app/controllers/api/v1/applications_controller.rb
@@ -1,10 +1,13 @@
 module Api
   module V1
     class ApplicationsController < BaseController
-      def index = head(:method_not_allowed)
-      def show = head(:method_not_allowed)
-      def accept = head(:method_not_allowed)
-      def reject = head(:method_not_allowed)
+      def index
+        render status: :ok, json: { hello: "world" }.to_json
+      end
+
+      # def show = head(:method_not_allowed)
+      # def accept = head(:method_not_allowed)
+      # def reject = head(:method_not_allowed)
     end
   end
 end
