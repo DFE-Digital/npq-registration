c045d65b3ac324558eed8c23311f1548849334d7
Slawosz Slawinski <slawosz@gmail.com>
2024-02-06 09:42:00 +0000

    Add features specs for course start date

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -6,8 +6,10 @@ class Admin::SettingsController < SuperAdminController
   def update
     @setting = Setting.find(params[:id])
     if @setting.update(params[:setting].permit(:course_start_date))
+      flash[:success] = "Settings updated"
       redirect_to admin_settings_path
     else
+      flash[:errork] = "Settings can not be updated"
       render :index
     end
   end
