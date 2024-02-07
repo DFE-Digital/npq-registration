59b1c5a6e8d9b56815624bbe6bfd473853c73c8e
Peter Yates <peter.yates@graphia.co.uk>
2024-02-07 15:09:40 +0000

    Make admins controller target admins

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -2,20 +2,20 @@ class Admin::AdminsController < SuperAdminController
   include Pagy::Backend
 
   def index
-    @pagy, @users = pagy(scope)
+    @pagy, @admins = pagy(Admin.all)
   end
 
   def new
-    @user = User.new
+    @admin = Admin.new
   end
 
   def create
-    @user = User.find_or_initialize_by(email: user_params[:email])
-    @user.assign_attributes(user_params)
-    @user.admin = true
+    @admin = Admin.find_or_initialize_by(email: user_params[:email])
+    @admin.assign_attributes(user_params)
+    @admin.admin = true
 
-    if @user.save
-      flash[:success] = "Admin permissions granted to #{@user.email}"
+    if @admin.save
+      flash[:success] = "Admin permissions granted to #{@admin.email}"
       redirect_to admin_admins_path
     else
       render :new
@@ -23,26 +23,22 @@ class Admin::AdminsController < SuperAdminController
   end
 
   def destroy
-    @user = User.find(params[:id])
+    @admin = Admin.find(params[:id])
 
-    if @user == current_user || @user.super_admin?
+    if @admin == current_admin || @admin.super_admin?
       flash[:error] = "You cannot remove admin permissions from yourself or another super admin."
       redirect_back fallback_location: admin_admins_path
-    elsif @user.update(admin: false)
+    elsif @admin.update(admin: false) # we should just delete the record here instead
       redirect_to admin_admins_path
     else
-      flash[:error] = "Failed to remove admin permissions from #{@user.email}, please contact technical support if this problem persists."
+      flash[:error] = "Failed to remove admin permissions from #{@admin.email}, please contact technical support if this problem persists."
       redirect_back fallback_location: admin_admins_path
     end
   end
 
 private
 
-  def scope
-    User.admins
-  end
-
   def user_params
-    params.require(:user).permit(:full_name, :email)
+    params.require(:admin).permit(:full_name, :email)
   end
 end
