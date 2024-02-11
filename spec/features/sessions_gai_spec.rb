ffa2b1c6025517ddf60ec1ca6665b747ae84ecff
Peter Yates <peter.yates@graphia.co.uk>
2024-02-11 15:47:03 +0000

    Fix GAI session tests
    Here the GAI session spec was logging in a user using the magic link,
    which is only used by admins. We're only checking that the links appear
    when a user is logged in, so it's more straightforward to stub
    `User.find_by`, which means current_user will return a truthy value and
    the logout/GAI info will be displayed.
    
    Not perfect but better.

--- spec/features/sessions_gai_spec.rb
+++ spec/features/sessions_gai_spec.rb
@@ -5,22 +5,12 @@ RSpec.feature "Sessions: integration with GAI", type: :feature do
 
   include_context "Stub Get An Identity Omniauth Responses"
 
-  # this is puzzling, we're testing GAI but logging in with a OTP
-  xscenario "GAI header links are only visible for logged-in users" do
-    User.create!(email: "user@example.com")
+  before do
+    allow(User).to receive(:find_by).and_return(FactoryBot.create(:user))
+  end
 
-    visit "/sign-in"
-    expect(page).not_to have_content("Sign out")
-    expect(page).not_to have_content("DfE Identity Account")
-
-    page.fill_in "What’s your email address?", with: " User@example.com "
-    page.click_button "Sign in"
-
-    code = ActionMailer::Base.deliveries.last[:personalisation].unparsed_value[:code]
-
-    expect(page).to have_content("Check your email")
-    page.fill_in "Enter your code", with: code
-    page.click_button "Sign in"
+  scenario "GAI header links are only visible for logged-in users" do
+    visit "/"
 
     expect(page).to have_link("Sign out", href: /\/sign-out/)
     expect(page).to have_link("DfE Identity account", href: /\/account\?client_id=register-for-npq&redirect_uri=[^&]+&sign_out_uri=[^&]+/)
