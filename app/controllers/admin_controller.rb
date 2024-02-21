b1d06b2af67d707c8f6df1e75639eef8d3b2cae6
Peter Yates <peter.yates@graphia.co.uk>
2024-02-21 15:36:23 +0000

    Create admin template and use for new admin console
    This currently matches the default but drops the phase banner.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,5 @@
 class AdminController < ApplicationController
+  layout "application"
   before_action :require_admin
 
   def show
--- app/controllers/npq_separation/admin_controller.rb
+++ app/controllers/npq_separation/admin_controller.rb
@@ -1,4 +1,5 @@
 class NpqSeparation::AdminController < ApplicationController
+  layout "admin"
   before_action :require_admin
 
   def show; end
