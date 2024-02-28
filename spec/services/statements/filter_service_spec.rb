c4107e6c7183c386e6a23acfee693528bedbba46
Pablo Manrubia <pmanrubia@gmail.com>
2024-02-28 12:06:54 +0100

    Update spec/services/statements/filter_service_spec.rb

--- spec/services/statements/filter_service_spec.rb
+++ spec/services/statements/filter_service_spec.rb
@@ -3,12 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe Statements::FilterService do
-  let(:params) { {} }
-  let(:service) { described_class.new(params) }
-
-  describe '#initialize' do
-    it 'initializes with default params' do
-      expect(service.instance_variable_get(:@params)).to eq(params)
-    end
   end
 end
