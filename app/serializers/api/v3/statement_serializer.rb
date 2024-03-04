68a2cd4eb050732ad4c2317ca0ba2ebdd8b14cac
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-04 10:44:31 +0100

    dry-up statements serializer

--- app/serializers/api/v3/statement_serializer.rb
+++ app/serializers/api/v3/statement_serializer.rb
@@ -5,13 +5,14 @@ module API
 
       field :month
       field :year
-      field(:type) { 'npq' }
       field :cohort_id, name: :cohort
-      field(:deadline_date, name: :cut_off_date) { |s, _| s.deadline_date.strftime("%Y-%m-%d") }
-      field(:payment_date) { |s, _| s.payment_date&.strftime("%Y-%m-%d") }
+      field :deadline_date, name: :cut_off_date, datetime_format: "%Y-%m-%d"
+      field :payment_date, datetime_format: "%Y-%m-%d"
+      field :created_at, datetime_format: ->(dt) { dt.iso8601 }
+      field :updated_at, datetime_format: ->(dt) { dt.iso8601 }
+
       field(:paid) { |s, _| s.payment_date.present? }
-      field(:created_at) { |s, _| s.created_at.iso8601 }
-      field(:updated_at) { |s, _| s.updated_at.iso8601 }
+      field(:type) { "npq" }
     end
   end
 end
