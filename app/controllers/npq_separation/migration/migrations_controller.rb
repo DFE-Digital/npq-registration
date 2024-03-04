5f4e15d717a2c67cbe430613eb33742dc0cff74f
Ross Oliver <ross.oliver36@gmail.com>
2024-03-04 15:31:54 +0000

    Add controller and views for migration
    Add the controller actions and views necessary for a user to be able to kick
    off, view the progress and outcome of a migration. Only super admins will be
    able to run migrations.
    
    Fixes a typo in the `sign_in.html.erb` error message.
    
    Adds a config option for `rspec` to easily run tests against an in-memory cache
    and `rack_test` Capybara driver.

--- app/controllers/npq_separation/migration/migrations_controller.rb
+++ app/controllers/npq_separation/migration/migrations_controller.rb
@@ -1,3 +1,25 @@
 class NpqSeparation::Migration::MigrationsController < ApplicationController
-  def index = head(:method_not_allowed)
+  before_action :require_super_admin
+
+  def index
+    @data_migrations = Migration::DataMigration.all
+    @in_progress_migration = @data_migrations.present? && !@data_migrations.all?(&:complete?)
+    @completed_migration = @data_migrations.present? && @data_migrations.all?(&:complete?)
+  end
+
+  def create
+    Migration::Migrator.prepare_for_migration
+    MigrationJob.perform_later
+
+    redirect_to npq_separation_migration_migrations_path
+  end
+
+private
+
+  def require_super_admin
+    unless current_admin&.super_admin?
+      flash[:negative] = { title: "Unauthorized", text: "Sign in with your admininstrator account" }
+      redirect_to sign_in_path
+    end
+  end
 end
