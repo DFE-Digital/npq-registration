a922f80c747288b05adfb39ed76891cb7bda9483
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-06 15:28:33 +0100

    Do not use collection serializer

--- app/serializers/api/v3/statements_serializer.rb
+++
@@ -1,7 +0,0 @@
-module API
-  module V3
-    class StatementsSerializer < Blueprinter::Base
-      association :statements, blueprint: StatementSerializer
-    end
-  end
-end
--- app/views/api/v3/statements.json.jbuilder
+++
@@ -1,3 +0,0 @@
-# app/views/api/v3/statements/index.json.jbuilder
-
-json.array! @statements, partial: 'api/v3/statements/statement', as: :statement
