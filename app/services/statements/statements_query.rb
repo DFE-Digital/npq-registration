34e63bed4bed9575e0e0f61f0a9e06008199589e
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-06 16:41:58 +0100

    Filter by updated_since

--- app/services/statements/statements_query.rb
+++ app/services/statements/statements_query.rb
@@ -3,12 +3,13 @@ module Statements
     def initialize(lead_provider:, params:)
       @lead_provider = lead_provider
       @cohorts = String(params[:cohort]).split(",")
-      @update_since = params[:updated_since]
+      @updated_since = params[:updated_since]
     end
 
     def statements
       statements = Statement.includes(:cohort).where(lead_provider:)
       statements = statements.where(cohorts: { start_year: cohorts }) if cohorts.any?
+      statements = statements.where('updated_at >= ?', @updated_since) if @updated_since.present?
 
       statements
     end
@@ -20,6 +21,6 @@ module Statements
     attr_reader \
       :lead_provider,
       :cohorts,
-      :update_since
+      :updated_since
   end
 end
--- spec/services/statements/statements_query_spec.rb
+++ spec/services/statements/statements_query_spec.rb
@@ -16,40 +16,56 @@ RSpec.describe Statements::StatementsQuery do
     end
 
     describe "filtering" do
-      let(:cohort_2023) { create(:cohort, start_year: 2023) }
-      let(:cohort_2024) { create(:cohort, start_year: 2024) }
-      let(:cohort_2025) { create(:cohort, start_year: 2025) }
+      describe "by updated since" do
+        let(:updated_since) { 1.day.ago }
+        let!(:statement1) { create(:statement, lead_provider: lead_provider, updated_at: 2.days.ago) }
+        let!(:statement2) { create(:statement, lead_provider: lead_provider, updated_at: Time.now) }
 
-      it "filters by cohort" do
-        _statement = create(:statement, lead_provider:, cohort: cohort_2023)
-        statement = create(:statement, lead_provider:, cohort: cohort_2024)
-        query = Statements::StatementsQuery.new(
-          lead_provider:,
-          params: { cohort: "2024" },
-        )
+        it "filters by updated since" do
+          query = Statements::StatementsQuery.new(
+            lead_provider: lead_provider,
+            params: { updated_since: updated_since },
+          )
 
-        expect(query.statements).to eq([statement])
+          expect(query.statements).to eq([statement2])
+        end
       end
+      describe "by cohort" do
+        let(:cohort_2023) { create(:cohort, start_year: 2023) }
+        let(:cohort_2024) { create(:cohort, start_year: 2024) }
+        let(:cohort_2025) { create(:cohort, start_year: 2025) }
 
-      it "filters by multiple cohorts" do
-        statement1 = create(:statement, lead_provider:, cohort: cohort_2023)
-        statement2 = create(:statement, lead_provider:, cohort: cohort_2024)
-        _statement = create(:statement, lead_provider:, cohort: cohort_2025)
-        query = Statements::StatementsQuery.new(
-          lead_provider:,
-          params: { cohort: "2023,2024" },
-        )
+        it "filters by cohort" do
+          _statement = create(:statement, lead_provider:, cohort: cohort_2023)
+          statement = create(:statement, lead_provider:, cohort: cohort_2024)
+          query = Statements::StatementsQuery.new(
+            lead_provider:,
+            params: { cohort: "2024" },
+          )
 
-        expect(query.statements).to match_array([statement1, statement2])
-      end
+          expect(query.statements).to eq([statement])
+        end
 
-      it "returns no statements if no cohorts are found" do
-        query = Statements::StatementsQuery.new(
-          lead_provider:,
-          params: { cohort: "9999" },
-        )
+        it "filters by multiple cohorts" do
+          statement1 = create(:statement, lead_provider:, cohort: cohort_2023)
+          statement2 = create(:statement, lead_provider:, cohort: cohort_2024)
+          _statement = create(:statement, lead_provider:, cohort: cohort_2025)
+          query = Statements::StatementsQuery.new(
+            lead_provider:,
+            params: { cohort: "2023,2024" },
+          )
 
-        expect(query.statements).to be_empty
+          expect(query.statements).to match_array([statement1, statement2])
+        end
+
+        it "returns no statements if no cohorts are found" do
+          query = Statements::StatementsQuery.new(
+            lead_provider:,
+            params: { cohort: "9999" },
+          )
+
+          expect(query.statements).to be_empty
+        end
       end
     end
   end
