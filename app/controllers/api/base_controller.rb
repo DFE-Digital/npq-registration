0881bf6f060f0553a706ceb252fbbacc8236f192
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-03-07 10:34:24 +0000

    [CPDLP-2866] TS1 - Statements endpoint - API base and error handling

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,25 @@
 module API
-  class BaseController < ApplicationController
+  class BaseController < ActionController::API
     include API::TokenAuthenticatable
+
+    before_action :remove_charset
+    rescue_from ActionController::UnpermittedParameters, with: :unpermitted_parameter_response
+    rescue_from ActionController::BadRequest, with: :bad_request_response
+    rescue_from ArgumentError, with: :bad_request_response
+
+  private
+
+    def remove_charset
+      ActionDispatch::Response.default_charset = nil
+    end
+
+    def unpermitted_parameter_response(exception)
+      render json: { errors: API::Errors::Response.new(error: I18n.t(:unpermitted_parameters), params: exception.params).call }, status: :unprocessable_entity
+    end
+
+    def bad_request_response(exception)
+      Sentry.capture_exception(exception)
+      render json: { errors: API::Errors::Response.new(error: I18n.t(:bad_request), params: exception.message).call }, status: :bad_request
+    end
   end
 end
