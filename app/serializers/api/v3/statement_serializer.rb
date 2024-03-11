6c0613734ed4daac7c71b5e9bae36dfb026bf06a
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-11 09:27:22 +0100

    Use nested attributes for Serializer

--- app/serializers/api/v3/statement_serializer.rb
+++ app/serializers/api/v3/statement_serializer.rb
@@ -1,17 +1,25 @@
 module API
   module V3
-    class StatementSerializer < Blueprinter::Base
-      identifier :id
+    class AttributesSerializer < Blueprinter::Base
+      exclude :id
 
-      field :month
+      field(:month) { |s, _| Date::MONTHNAMES[s.month] }
       field :year
       field(:cohort) { |s, _| s.cohort.start_year }
       field :deadline_date, name: :cut_off_date, datetime_format: "%Y-%m-%d"
       field :payment_date, datetime_format: "%Y-%m-%d"
+      field(:paid) { |s, _| s.payment_date.present? }
       field :created_at
       field :updated_at
+    end
 
-      field(:paid) { |s, _| s.payment_date.present? }
+    class StatementSerializer < Blueprinter::Base
+      identifier :id
+      field(:type) { "statement" }
+
+      field :attributes do |statement, _options|
+        AttributesSerializer.render_as_hash(statement)
+      end
     end
   end
 end
--- spec/serializers/api/v3/statement_serializer_spec.rb
+++ spec/serializers/api/v3/statement_serializer_spec.rb
@@ -4,79 +4,84 @@ require "rails_helper"
 
 RSpec.describe API::V3::StatementSerializer, type: :serializer do
   let(:subject) { described_class }
-  let(:cohort) { create(:cohort) }
-  let(:statement) { create(:statement, cohort:) }
+  let(:cohort) { build(:cohort) }
+  let(:statement) { build(:statement, cohort:) }
 
-  it "serializes the `id`" do
-    response = subject.render_as_hash(statement)
+  describe "core attributes" do
+    subject(:response) { JSON.parse(described_class.render(statement)) }
 
-    expect(response[:id]).to eq(statement.id)
+    it "serializes the `id`" do
+      cohort.save!
+
+      expect(response["id"]).to eq(statement.id)
+    end
+
+    it "serializes the `type`" do
+      response = JSON.parse(described_class.render(statement))
+
+      expect(response["type"]).to eq("statement")
+    end
   end
 
-  it "serializes the `cohort start year`" do
-    cohort.start_year = 2025
-    response = subject.render_as_hash(statement)
+  describe "nested attributes" do
+    subject(:attributes) { JSON.parse(described_class.render(statement))["attributes"] }
 
-    expect(response[:cohort]).to eq(2025)
-  end
+    it "serializes the `cohort start year`" do
+      cohort.start_year = 2025
 
-  it "serializes the `month`" do
-    statement.month = 7
-    response = subject.render_as_hash(statement)
+      expect(attributes["cohort"]).to eq(2025)
+    end
 
-    expect(response[:month]).to eq(7)
-  end
+    it "serializes the `month`" do
+      statement.month = 7
 
-  it "serializes the `year`" do
-    statement.year = 2023
-    response = subject.render_as_hash(statement)
+      expect(attributes["month"]).to eq("July")
+    end
 
-    expect(response[:year]).to eq(2023)
-  end
+    it "serializes the `year`" do
+      statement.year = 2023
 
-  it "serializes the `deadline`" do
-    statement.deadline_date = Date.new(2023, 7, 1)
-    response = subject.render_as_hash(statement)
+      expect(attributes["year"]).to eq(2023)
+    end
 
-    expect(response[:cut_off_date]).to eq("2023-07-01")
-  end
+    it "serializes the `deadline`" do
+      statement.deadline_date = Date.new(2023, 7, 1)
 
-  it "serializes the `payment_date`" do
-    statement.payment_date = Date.new(2023, 7, 1)
-    response = subject.render_as_hash(statement)
+      expect(attributes["cut_off_date"]).to eq("2023-07-01")
+    end
 
-    expect(response[:payment_date]).to eq("2023-07-01")
-  end
-
-  describe "`paid` status" do
-    it "returns `true` when `payment_date` is not nil" do
+    it "serializes the `payment_date`" do
       statement.payment_date = Date.new(2023, 7, 1)
-      response = subject.render_as_hash(statement)
 
-      expect(response[:paid]).to eq(true)
+      expect(attributes["payment_date"]).to eq("2023-07-01")
     end
 
-    it "returns `false` when `payment_date` is nil" do
-      statement.payment_date = nil
-      response = subject.render_as_hash(statement)
+    describe "`paid` status" do
+      it "returns `true` when `payment_date` is not nil" do
+        statement.payment_date = Date.new(2023, 7, 1)
 
-      expect(response[:paid]).to eq(false)
-    end
-  end
+        expect(attributes["paid"]).to eq(true)
+      end
 
-  describe "timestamp serialization" do
-    it "serializes the `created_at`" do
-      statement.created_at = Time.utc(2023, 7, 1, 12, 0, 0)
-      response = subject.render_as_hash(statement)
+      it "returns `false` when `payment_date` is nil" do
+        statement.payment_date = nil
 
-      expect(response[:created_at]).to eq("2023-07-01T12:00:00Z")
+        expect(attributes["paid"]).to eq(false)
+      end
     end
 
-    it "serializes the `updated_at`" do
-      statement.updated_at = Time.utc(2023, 7, 2, 12, 0, 0)
-      response = subject.render_as_hash(statement)
+    describe "timestamp serialization" do
+      it "serializes the `created_at`" do
+        statement.created_at = Time.utc(2023, 7, 1, 12, 0, 0)
 
-      expect(response[:updated_at]).to eq("2023-07-02T12:00:00Z")
+        expect(attributes["created_at"]).to eq("2023-07-01T12:00:00Z")
+      end
+
+      it "serializes the `updated_at`" do
+        statement.updated_at = Time.utc(2023, 7, 2, 12, 0, 0)
+
+        expect(attributes["updated_at"]).to eq("2023-07-02T12:00:00Z")
+      end
     end
   end
 end
