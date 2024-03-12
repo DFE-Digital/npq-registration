7b0250ab2194689a903bb3e5a70c2c4ef6937e4f
Peter Yates <peter.yates@graphia.co.uk>
2024-03-12 17:54:06 +0000

    Make statement links text_colour and add action col
    The sea of blue links is overpowering. This change adds a 'Action'
    column with a 'View' link that is blue and makes the rest of the (meta)
    links black

--- app/components/npq_separation/admin/statements_table_component.rb
+++ app/components/npq_separation/admin/statements_table_component.rb
@@ -21,6 +21,7 @@ module NpqSeparation
           ("Lead provider" if show_lead_provider),
           "Cohort",
           "Status",
+          govuk_visually_hidden("Action"),
         ].compact
       end
 
@@ -31,28 +32,42 @@ module NpqSeparation
             lead_provider_link(statement.lead_provider),
             cohort_link(statement),
             statement_tag(statement),
+            view_link(statement),
           ].compact
         end
       end
 
       def id_link(statement)
-        govuk_link_to(statement.id.to_s, npq_separation_admin_finance_statement_path(statement), no_visited_state: true)
+        govuk_link_to(statement.id.to_s, npq_separation_admin_finance_statement_path(statement), **metadata_link_arguments)
+      end
+
+      def view_link(statement)
+        govuk_link_to(
+          "View",
+          npq_separation_admin_finance_statement_path(statement),
+          no_visited_state: true,
+          visually_hidden_suffix: "statement #{statement.id}",
+        )
       end
 
       def lead_provider_link(lead_provider)
         return unless show_lead_provider
 
-        govuk_link_to(lead_provider.name, npq_separation_admin_finance_lead_provider_path(lead_provider), no_visited_state: true)
+        govuk_link_to(lead_provider.name, npq_separation_admin_lead_provider_path(lead_provider), **metadata_link_arguments)
       end
 
       def cohort_link(statement)
         text = helpers.format_cohort(statement.cohort)
 
-        govuk_link_to(text, "#")
+        govuk_link_to(text, "#", **metadata_link_arguments)
       end
 
       def statement_tag(statement)
         govuk_tag(text: statement.state.capitalize)
+      end
+
+      def metadata_link_arguments
+        { text_colour: true, no_underline: true }
       end
     end
   end
