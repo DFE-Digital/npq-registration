8698a3694c3a471a8db04f094bd6002c5dd2d6cc
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-03-12 15:02:56 +0000

    [CPDLP-2859] TS1 - Statements endpoint - happy path (show/index)

--- app/controllers/api/v3/statements_controller.rb
+++ app/controllers/api/v3/statements_controller.rb
@@ -1,8 +1,25 @@
 module API
   module V3
     class StatementsController < BaseController
-      def show = head(:method_not_allowed)
-      def index = head(:ok)
+      def index
+        render json: StatementSerializer.render(statements_query.statements, root: "data")
+      end
+
+      def show
+        render json: StatementSerializer.render(statements_query.statement(id: statement_params[:id]), root: "data")
+      end
+
+    private
+
+      def statements_query
+        ::Statements::Query.new(
+          lead_provider: current_lead_provider,
+        )
+      end
+
+      def statement_params
+        params.permit(:id)
+      end
     end
   end
 end
--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -1,6 +1,4 @@
-require "oj"
-
 Blueprinter.configure do |config|
   config.generator = Oj
-  config.datetime_format = ->(datetime) { datetime.nil? ? datetime : datetime.rfc3339 }
+  config.datetime_format = ->(datetime) { datetime&.rfc3339 }
 end
