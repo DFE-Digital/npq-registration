95b436954a8fd93dbe9f52d12b705bb9350a2dec
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-19 07:42:26 +0100

    Add support for Syntax highlighting

--- app/views/api/guidance/test.md
+++ app/views/api/guidance/test.md
@@ -1,1 +1,13 @@
+#content
+
 This is a markdown page with .md extension
+
+```json
+{
+  "name": "test",
+  "title": "Test",
+  "description": "This is a test page",
+  "type": "markdown",
+  "content": "This is a markdown page with .md extension"
+}
+```
--- config/initializers/markdown.rb
+++ config/initializers/markdown.rb
@@ -1,7 +1,22 @@
+require "rouge/plugins/redcarpet"
+
 module TemplateHandlers
+  class HTMLWithCodeHighlighting < GovukMarkdown::Renderer
+    include Rouge::Plugins::Redcarpet
+
+    def initialize(govuk_options = {})
+      super(govuk_options, { with_toc_data: true, link_attributes: { class: "govuk-link" } })
+    end
+  end
+
   class Markdown
-    def self.call(_template, source)
-      GovukMarkdown.render(source).inspect.html_safe
+    def self.call(template, source)
+      new.call(template, source)
+    end
+
+    def call(_template, source)
+      markdown = Redcarpet::Markdown.new(HTMLWithCodeHighlighting, fenced_code_blocks: true)
+      markdown.render(source.to_s).inspect.html_safe
     end
   end
 end
