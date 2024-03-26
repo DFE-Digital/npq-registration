80c03884f82c6499468e9018b5018d8e45bc7a4d
Pablo Manrubia <pmanrubia@gmail.com>
2024-03-26 09:32:53 +0100

    Remove .md extension from tests

--- spec/features/guidance_spec.rb
+++ spec/features/guidance_spec.rb
@@ -1,39 +1,67 @@
 require "rails_helper"
 
 RSpec.feature "Guidance", type: :feature do
-  it "renders the index page" do
-    visit "/api/guidance"
+  describe "GET /api/guidance" do
+    it "renders the index page" do
+      visit "/api/guidance"
 
-    expect(page).to have_content("Guidance")
+      expect(page).to have_content("Guidance")
+    end
+
+    it "renders the call to action button" do
+      visit "/api/guidance"
+
+      expect(page).to have_link("Get started", href: "/api/guidance/get-started")
+    end
   end
 
-<<<<<<< HEAD
   describe "GET /api/guidance/get-started" do
     it "renders the .html page" do
       visit "/api/guidance/get-started"
-=======
-  describe "GET /api/guidance/get-started.md" do
-    it "renders the .html page" do
-      visit "/api/guidance/get-started.md"
->>>>>>> f8b4d6e1 (Add guidance show for get-started page)
 
       expect(page).not_to have_content("#Connect to the API")
       expect(page).to have_content("Connect to the API")
     end
+  end
 
-    it "renders the navigation menu" do
-<<<<<<< HEAD
-      visit "/api/guidance/get-started"
-=======
-      visit "/api/guidance/get-started.md"
->>>>>>> f8b4d6e1 (Add guidance show for get-started page)
+  describe "GET /api/guidance/what-you-can-do-in-the-api.md" do
+    it "renders the .html page" do
+      visit "/api/guidance/what-you-can-do-in-the-api"
 
-      expect(page).to have_link("Connect to the API", href: "#connect-to-the-api")
+      expect(page).not_to have_content("#What you can do in the API")
+      expect(page).to have_content("What you can do in the API")
+    end
+  end
+
+  describe "GET /api/guidance/definitions" do
+    it "renders the .html page" do
+      visit "/api/guidance/definitions"
+
+      expect(page).not_to have_content("#Definitions")
+      expect(page).to have_content("Definitions")
+    end
+  end
+
+  describe "GET /api/guidance/api-latest-version" do
+    it "renders the .html page" do
+      visit "/api/guidance/api-latest-version"
+
+      expect(page).not_to have_content("#API latest version")
+      expect(page).to have_content("API latest version")
+    end
+  end
+
+  describe "GET /api/guidance/test-environment" do
+    it "renders the .html page" do
+      visit "/api/guidance/test-environment"
+
+      expect(page).not_to have_content("#Test environment")
+      expect(page).to have_content("Test environment")
     end
   end
 
   it "renders a nested markdown page" do
-    # to be updated with a valid nested page
+    # to be updated with the first nested page
     visit "/api/guidance/nested/nested/test"
 
     expect(page).to have_content("This is a nested markdown page")
