8e64636a779cdc63b3cb7686bdb75b81f3b30bc7
Peter Yates <peter.yates@graphia.co.uk>
2024-04-18 15:08:58 +0100

    Add validation messages for Statement year

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -10,7 +10,12 @@ class Statement < ApplicationRecord
               only_integer: true,
               message: "Month must be a number between 1 and 12",
             }
-  validates :year, numericality: { in: 2020..2050, only_integer: true }
+  validates :year,
+            numericality: {
+              in: 2020..2050,
+              only_integer: true,
+              message: "Year must be a 4 digit number",
+            }
   validates :ecf_id, presence: true, uniqueness: { case_sensitive: false }
 
   validate :validate_max_statement_items_count
--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Statement, type: :model do
 
   describe "validations" do
     it { is_expected.to validate_numericality_of(:month).is_in(1..12).only_integer.with_message("Month must be a number between 1 and 12") }
-    it { is_expected.to validate_numericality_of(:year).only_integer.is_in(2020..2050) }
+    it { is_expected.to validate_numericality_of(:year).only_integer.is_in(2020..2050).with_message("Year must be a 4 digit number") }
     it { is_expected.to allow_value(%w[true false]).for(:output_fee) }
     it { is_expected.not_to allow_value(nil).for(:output_fee) }
     it { is_expected.to validate_presence_of(:ecf_id) }
