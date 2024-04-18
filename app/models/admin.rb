20887061196176b76cbde39813140d0790939d67
Peter Yates <peter.yates@graphia.co.uk>
2024-04-18 15:25:57 +0100

    Add validation message for Admin full_name

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,8 @@
 class Admin < ApplicationRecord
-  validates :full_name, presence: true, length: { maximum: 64 }
+  validates :full_name,
+            presence: { message: "Enter a full name" },
+            length: { maximum: 64, message: "Full name must be shorter than 64 characters" }
+
   validates :email, presence: true, length: { maximum: 64 }
 
   # Whether this user has admin access to the feature flagging interface
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe Admin, type: :model do
   describe "validation" do
     describe "full_name" do
-      it { is_expected.to validate_presence_of(:full_name) }
-      it { is_expected.to validate_length_of(:full_name).is_at_most(64) }
+      it { is_expected.to validate_presence_of(:full_name).with_message("Enter a full name") }
+      it { is_expected.to validate_length_of(:full_name).is_at_most(64).with_message("Full name must be shorter than 64 characters") }
     end
 
     describe "email" do
