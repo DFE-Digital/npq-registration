97d9104a99ed9aa6594ca6f9664f459a0ab7f3e7
Leandro Alemão <leandro.costantini@gmail.com>
2024-04-23 14:30:13 +0100

    [CPDLP-2961] Address PR comments

--- app/services/migration/migrators/application.rb
+++ app/services/migration/migrators/application.rb
@@ -1,5 +1,26 @@
 module Migration::Migrators
   class Application < Base
+    ATTRIBUTES_TO_COMPARE = %i[
+      headteacher_status
+      eligible_for_funding
+      funding_choice
+      teacher_catchment
+      works_in_school
+      employer_name
+      employment_role
+      works_in_nursery
+      works_in_childcare
+      kind_of_nursery
+      targeted_delivery_funding_eligibility
+      funding_eligiblity_status_code
+      employment_type
+      lead_mentor
+      primary_establishment
+      tsf_primary_eligibility
+      tsf_primary_plus_eligibility
+      lead_provider_approval_status
+    ].freeze
+
     def call
       migrate(ecf_npq_applications, :application) do |ecf_npq_application|
         application = ::Application.find_by!(ecf_id: ecf_npq_application.id)
@@ -11,35 +32,12 @@ module Migration::Migrators
   private
 
     def ecf_npq_applications
-      @ecf_npq_applications ||= Migration::Ecf::NpqApplication.joins(:participant_identity).all.limit(500)
-    end
-
-    def attributes_to_compare
-      %i[
-        headteacher_status
-        eligible_for_funding
-        funding_choice
-        teacher_catchment
-        works_in_school
-        employer_name
-        employment_role
-        works_in_nursery
-        works_in_childcare
-        kind_of_nursery
-        targeted_delivery_funding_eligibility
-        funding_eligiblity_status_code
-        employment_type
-        lead_mentor
-        primary_establishment
-        tsf_primary_eligibility
-        tsf_primary_plus_eligibility
-        lead_provider_approval_status
-      ]
+      @ecf_npq_applications ||= Migration::Ecf::NpqApplication.joins(:participant_identity).all
     end
 
     def compare_attributes_values!(ecf_npq_application, application)
-      return if ecf_npq_application.attributes.slice(*attributes_to_compare.map(&:to_s)) ==
-        application.attributes.slice(*attributes_to_compare.map(&:to_s))
+      return if ecf_npq_application.attributes.slice(*ATTRIBUTES_TO_COMPARE.map(&:to_s)) ==
+        application.attributes.slice(*ATTRIBUTES_TO_COMPARE.map(&:to_s))
 
       application.errors.add(:base, "There are some discrepancies in one or more attributes values")
       raise ActiveRecord::RecordInvalid, application
--- spec/services/migration/migrators/application_spec.rb
+++ spec/services/migration/migrators/application_spec.rb
@@ -10,18 +10,13 @@ RSpec.describe Migration::Migrators::Application do
     let(:ecf_npq_application2) { create(:ecf_migration_npq_application) }
 
     before do
-      create(:application, ecf_id: ecf_npq_application1.id)
-      create(:application, ecf_id: ecf_npq_application2.id)
+      create(:application, ecf_id: ecf_npq_application1.id, **ecf_npq_application1.slice(described_class::ATTRIBUTES_TO_COMPARE))
+      create(:application, ecf_id: ecf_npq_application2.id, **ecf_npq_application2.slice(described_class::ATTRIBUTES_TO_COMPARE))
 
       create(:data_migration, model: :application)
     end
 
     context "when all attributes values of interest matches" do
-      before do
-        allow_any_instance_of(Migration::Ecf::NpqApplication).to receive(:attributes).and_return({})
-        allow_any_instance_of(Application).to receive(:attributes).and_return({})
-      end
-
       it "migrates the applications" do
         subject
 
@@ -31,28 +26,26 @@ RSpec.describe Migration::Migrators::Application do
     end
 
     context "when any attributes values do not match" do
+      before do
+        ecf_npq_application1.update!(teacher_catchment: "any")
+      end
+
       it "increments the failure count" do
         subject
 
         expect(Migration::DataMigration.find_by(model: :application).processed_count).to eq(2)
-        expect(Migration::DataMigration.find_by(model: :application).failure_count).to eq(2)
+        expect(Migration::DataMigration.find_by(model: :application).failure_count).to eq(1)
       end
 
       it "calls FailureManager with correct params" do
         expect_any_instance_of(Migration::FailureManager).to receive(:record_failure).with(ecf_npq_application1, "Validation failed: There are some discrepancies in one or more attributes values").and_call_original
-        expect_any_instance_of(Migration::FailureManager).to receive(:record_failure).with(ecf_npq_application2, "Validation failed: There are some discrepancies in one or more attributes values").and_call_original
 
         subject
       end
     end
 
-    context "when a application is not correctly created" do
+    context "when an application exists in ECF but cannot be matched to an application in NPQ registration" do
       let!(:ecf_migration_npq_application) { create(:ecf_migration_npq_application) }
-
-      before do
-        allow_any_instance_of(Migration::Ecf::NpqApplication).to receive(:attributes).and_return({})
-        allow_any_instance_of(Application).to receive(:attributes).and_return({})
-      end
 
       it "increments the failure count" do
         subject
