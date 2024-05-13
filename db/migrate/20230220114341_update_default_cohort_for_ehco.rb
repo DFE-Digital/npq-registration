90e6d28b674291696075481629d8dffd133caa1a
Ross Oliver <ross.oliver36@gmail.com>
2024-05-13 11:51:10 +0100

    Remove default_cohort migration fix
    The `default_cohort` is no longer present in the model and the data has already
    been fixed; removing as this is causing issues in staging.

--- db/migrate/20230220114341_update_default_cohort_for_ehco.rb
+++
@@ -1,7 +0,0 @@
-class UpdateDefaultCohortForEhco < ActiveRecord::Migration[6.1]
-  def change
-    ehco_course = Course.where(identifier: "npq-early-headship-coaching-offer").first
-
-    ehco_course&.update!(default_cohort: 2022)
-  end
-end
