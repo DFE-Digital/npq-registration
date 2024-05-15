0fecc6098a4f287645b01975002e374e08183a4c
Slawosz Slawinski <slawosz@gmail.com>
2024-05-15 15:21:29 +0100

    Import all schools

--- db/seeds/base/add_schools.rb
+++ db/seeds/base/add_schools.rb
@@ -1,5 +1,5 @@
 CSV.read(Rails.root.join("db/seeds/schools.csv"), headers: true).tap do |data|
-  Rails.logger.info("Importing 1000 schools")
+  Rails.logger.info("Importing all schools")
 
-  School.insert_all(data.first(1000).map(&:to_h))
+  School.insert_all(data.map(&:to_h))
 end
