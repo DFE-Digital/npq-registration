b745f6f9b003e4e0808185b5880f80f759530e7b
Slawosz Slawinski <slawosz@gmail.com>
2024-05-15 17:02:22 +0100

    Import schools in batches

--- db/seeds/base/add_schools.rb
+++ db/seeds/base/add_schools.rb
@@ -1,5 +1,7 @@
 CSV.read(Rails.root.join("db/seeds/schools.csv"), headers: true).tap do |data|
   Rails.logger.info("Importing all schools")
 
-  School.insert_all(data.map(&:to_h))
+  data.each_slice(1000) do |schools|
+    School.insert_all(schools.map(&:to_h))
+  end
 end
