6f08969367c9b7c1026ea5a998a9430f6592036d
Peter Yates <peter.yates@graphia.co.uk>
2024-05-17 10:14:26 +0100

    Remove initialize_with from course factory
    We'll be using Course.find instead

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -3,9 +3,5 @@ FactoryBot.define do
     sequence(:name) { |n| "NPQ Course #{n}" }
     identifier { Course::IDENTIFIERS.sample }
     ecf_id { SecureRandom.uuid }
-
-    initialize_with do
-      Course.find_by(identifier:) || new(**attributes)
-    end
   end
 end
