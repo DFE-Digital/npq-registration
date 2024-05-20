6498dc609048fafec0bf542cb6cf0b644987ecef
Leandro Alemão <leandro.costantini@gmail.com>
2024-05-20 21:09:23 +0100

    [CPDLP-3049] Address PR comments

--- spec/requests/api/docs/v3/applications_spec.rb
+++ spec/requests/api/docs/v3/applications_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe "NPQ Applications endpoint", type: :request, openapi_spec: "v3/sw
   let(:application) { create(:application, lead_provider:) }
 
   it_behaves_like "an API index endpoint documentation",
-                  "api/v3/npq-applications",
+                  "/api/v3/npq-applications",
                   "NPQ Applications",
                   "NPQ applications",
                   "#/components/schemas/ListApplicationsFilter",
                   "#/components/schemas/ApplicationsResponse"
 
   it_behaves_like "an API show endpoint documentation",
-                  "api/v3/npq-applications/{id}",
+                  "/api/v3/npq-applications/{id}",
                   "NPQ Applications",
                   "NPQ application",
                   "#/components/schemas/ApplicationResponse" do
@@ -22,19 +22,19 @@ RSpec.describe "NPQ Applications endpoint", type: :request, openapi_spec: "v3/sw
   end
 
   it_behaves_like "an API accept application endpoint documentation",
-                  "api/v3/npq-applications/{id}/accept",
+                  "/api/v3/npq-applications/{id}/accept",
                   "#/components/schemas/ApplicationResponse" do
     let(:resource) { application }
   end
 
   it_behaves_like "an API reject application endpoint documentation",
-                  "api/v3/npq-applications/{id}/reject",
+                  "/api/v3/npq-applications/{id}/reject",
                   "#/components/schemas/ApplicationResponse" do
     let(:resource) { application }
   end
 
   it_behaves_like "an API change application funded place endpoint documentation",
-                  "api/v3/npq-applications/{id}/change-funded-place",
+                  "/api/v3/npq-applications/{id}/change-funded-place",
                   "#/components/schemas/ApplicationResponse" do
     let(:application) { create(:application, :eligible_for_funded_place, lead_provider:) }
     let(:resource) { application }
--- spec/swagger_schemas/requests/application_accept_request.rb
+++ spec/swagger_schemas/requests/application_accept_request.rb
@@ -3,7 +3,29 @@ APPLICATION_ACCEPT_REQUEST = {
   type: :object,
   properties: {
     data: {
-      "$ref": "#/components/schemas/ApplicationAcceptDataRequest",
+      description: "A NPQ application acceptance request data",
+      type: :object,
+      required: %i[type attributes],
+      properties: {
+        type: {
+          description: "The data typed",
+          type: :string,
+          example: "npq-application-accept",
+        },
+        attributes: {
+          description: "A NPQ application acceptance request attributes",
+          type: :object,
+          required: %i[funded_place],
+          properties: {
+            funded_place: {
+              description: "This field indicates whether the application is funded",
+              nullable: false,
+              type: :boolean,
+              example: true,
+            },
+          },
+        },
+      },
     },
   },
 }.freeze
