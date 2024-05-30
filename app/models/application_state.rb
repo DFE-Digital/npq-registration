711f63925496b0496b693b6bde4edeb7ae3aed74
Leandro Alemão <leandro.costantini@gmail.com>
2024-05-30 14:42:56 +0100

    [CPDLP-3079] Address PR comments

--- app/models/application_state.rb
+++ app/models/application_state.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationState < ApplicationRecord
-  belongs_to :application, touch: true
+  belongs_to :application
   belongs_to :lead_provider, optional: true
 
   enum state: {
