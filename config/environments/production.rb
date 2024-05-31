260be58725229479fa13e5cf71dba77e348493cb
Peter Yates <peter.yates@graphia.co.uk>
2024-05-31 14:19:20 +0100

    Enable semantic logging to STDOUT in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,20 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   # Logging
   config.log_level = :info
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    config.rails_semantic_logger.add_file_appender = false
+
+    $stdout.sync = true
+
+    config.semantic_logger.add_appender(io: $stdout, level: Rails.application.config.log_level, formatter: :json)
+  end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
