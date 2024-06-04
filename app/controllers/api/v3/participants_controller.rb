ff876f8c8ea2e8d382be8efd7fbd4cb0fcdb653a
Leandro Alemão <leandro.costantini@gmail.com>
2024-06-04 09:31:39 +0100

    [CPDLP-3081] Add NPQ single participant profiles endpoint

--- app/controllers/api/v3/participants_controller.rb
+++ app/controllers/api/v3/participants_controller.rb
@@ -8,7 +8,10 @@ module API
         render json: to_json(paginate(participants_query.participants))
       end
 
-      def show = head(:method_not_allowed)
+      def show
+        render json: to_json(participant)
+      end
+
       def change_schedule = head(:method_not_allowed)
       def defer = head(:method_not_allowed)
       def withdraw = head(:method_not_allowed)
@@ -32,11 +35,15 @@ module API
       end
 
       def participant_params
-        params.permit(:sort, filter: %i[updated_since training_status from_participant_id])
+        params.permit(:ecf_id, :sort, filter: %i[updated_since training_status from_participant_id])
       end
 
       def to_json(obj)
         ParticipantSerializer.render(obj, view: :v3, root: "data", lead_provider: current_lead_provider)
+      end
+
+      def participant
+        participants_query.participant(ecf_id: participant_params[:ecf_id])
       end
     end
   end
--- app/services/participants/query.rb
+++ app/services/participants/query.rb
@@ -18,6 +18,13 @@ module Participants
       scope.order(order_by)
     end
 
+    def participant(id: nil, ecf_id: nil)
+      return scope.find_by!(ecf_id:) if ecf_id.present?
+      return scope.find(id) if id.present?
+
+      fail(ArgumentError, "id or ecf_id needed")
+    end
+
   private
 
     def where_lead_provider_is(lead_provider)
