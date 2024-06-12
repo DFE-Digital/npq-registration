56d70a4de9de7576864187b569e11c6824b2ba54
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-06-12 16:00:42 +0100

    [CPDLP-3074] TS5 - swagger docs

--- spec/swagger_schemas/requests/application_accept_request.rb
+++ spec/swagger_schemas/requests/application_accept_request.rb
@@ -1,70 +1,87 @@
 APPLICATION_ACCEPT_REQUEST = {
-  description: "A NPQ application acceptance request",
-  type: :object,
-  properties: {
-    data: {
-      description: "A NPQ application acceptance request data",
-      type: :object,
-      required: %i[type attributes],
-      properties: {
-        type: {
-          description: "The data typed",
-          type: :string,
-          example: "npq-application-accept",
-        },
-        attributes: {
-          description: "A NPQ application acceptance request attributes",
-          type: :object,
-          properties: {
-            funded_place: {
-              description: "This field indicates whether the application is funded",
-              nullable: false,
-              type: :boolean,
-              required: false,
-              example: true,
+  v1: {
+    description: "A NPQ application acceptance request",
+    type: :object,
+    properties: {
+      data: {
+        description: "A NPQ application acceptance request data",
+        type: :object,
+        required: %i[type attributes],
+        properties: {
+          type: {
+            description: "The data typed",
+            type: :string,
+            example: "npq-application-accept",
+          },
+          attributes: {
+            description: "A NPQ application acceptance request attributes",
+            type: :object,
+            properties: {
+              funded_place: {
+                description: "This field indicates whether the application is funded",
+                nullable: false,
+                type: :boolean,
+                required: false,
+                example: true,
+              },
             },
           },
         },
       },
     },
   },
-}.freeze
-
-APPLICATION_ACCEPT_REQUEST_V3 = {
-  description: "A NPQ application acceptance request",
-  type: :object,
-  properties: {
-    data: {
-      description: "A NPQ application acceptance request data",
-      type: :object,
-      required: %i[type attributes],
-      properties: {
-        type: {
-          description: "The data typed",
-          type: :string,
-          example: "npq-application-accept",
-        },
-        attributes: {
-          description: "A NPQ application acceptance request attributes",
-          type: :object,
-          properties: {
-            funded_place: {
-              description: "This field indicates whether the application is funded",
-              nullable: false,
-              type: :boolean,
-              required: false,
-              example: true,
-            },
-            schedule_identifier: {
-              description: "The new schedule of the participant",
-              nullable: false,
-              type: :string,
-              required: false,
-              example: "npq-leadership-autumn",
+}.tap { |h|
+  h[:v2] = h[:v1]
+  h[:v3] = {
+    description: "A NPQ application acceptance request",
+    type: :object,
+    properties: {
+      data: {
+        description: "A NPQ application acceptance request data",
+        type: :object,
+        required: %i[type attributes],
+        properties: {
+          type: {
+            description: "The data typed",
+            type: :string,
+            example: "npq-application-accept",
+          },
+          attributes: {
+            description: "A NPQ application acceptance request attributes",
+            type: :object,
+            properties: {
+              funded_place: {
+                description: "This field indicates whether the application is funded",
+                nullable: false,
+                type: :boolean,
+                required: false,
+                example: true,
+              },
+              schedule_identifier: {
+                description: "The new schedule of the participant",
+                nullable: false,
+                type: :string,
+                example: "npq-leadership-spring",
+                required: false,
+                enum: %w[
+                  npq-aso-march
+                  npq-aso-june
+                  npq-aso-november
+                  npq-aso-december
+                  npq-ehco-march
+                  npq-ehco-june
+                  npq-ehco-november
+                  npq-ehco-december
+                  npq-leadership-autumn
+                  npq-leadership-spring
+                  npq-specialist-autumn
+                  npq-specialist-spring
+                ],
+              },
             },
           },
         },
       },
     },
-  },
+  }
 }.freeze
