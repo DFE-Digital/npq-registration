91000282779b62bacb8e7df677823ed88298148b
Leandro C <leandro.costantini@gmail.com>
2024-06-13 12:03:43 +0100

    [CPDLP-3082] Update swagger docs with participant profile endpoints (#1417)
    * [CPDLP-3080] Add NPQ participant profiles endpoint
    
    * [CPDLP-3080] Address PR comments
    
    * [CPDLP-3080] Address PR comments
    
    * [CPDLP-3080] Address PR comments
    
    * [CPDLP-3082] Update swagger docs with participant profile endpoints
    
    * [CPDLP-3082] Address PR comments

--- spec/models/course_group_spec.rb
+++ spec/models/course_group_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe CourseGroup, type: :model do
     it { is_expected.to validate_presence_of(:name).with_message("Enter a unique course group name") }
     it { is_expected.to validate_uniqueness_of(:name).with_message("Course name already exist, enter a unique name") }
   end
+
+  describe "associations" do
+    it { is_expected.to have_many(:courses) }
+  end
 end
