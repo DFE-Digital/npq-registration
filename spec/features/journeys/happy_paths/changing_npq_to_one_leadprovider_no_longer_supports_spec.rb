c4fe3115d918d11248fc06aaf58266375592e53f
Sławosz Sławiński <slawosz@gmail.com>
2024-06-20 12:06:54 +0100

    Update messages for eligiblity (#1440)
    * Update messages for eligiblity
    
    * Further copy changes

--- spec/features/journeys/happy_paths/changing_npq_to_one_leadprovider_no_longer_supports_spec.rb
+++ spec/features/journeys/happy_paths/changing_npq_to_one_leadprovider_no_longer_supports_spec.rb
@@ -117,7 +117,7 @@ RSpec.feature "Happy journeys", type: :feature do
 
     expect_page_to_have(path: "/registration/ineligible-for-funding/change", submit_form: false) do
       expect(page).to have_text("Funding")
-      expect(page).to have_text("Early years leadership NPQ because your workplace is not in the list of EY settings that are eligible for funding")
+      expect(page).to have_text("Early years leadership NPQ as your workplace is not in the list of EY settings that are eligible for funding")
       expect(page).to have_text("This means that you would need to pay for the course another way")
 
       page.click_link("Continue")
