ca95306d011b6b5470d8b5b1cf0d9969505ab30d
Pablo M <pablo@advera.co.uk>
2024-06-21 06:28:36 +0100

    Add outcomes table

---
+++ app/models/outcome.rb
@@ -0,0 +1,13 @@
+class Outcome < ApplicationRecord
+  belongs_to :declaration
+
+  validates :state, presence: true
+  validates :completion_date, presence: true
+  validate :completion_date_not_in_the_future
+
+private
+
+  def completion_date_not_in_the_future
+    errors.add(:completion_date, "must be in the future") if completion_date && completion_date > Time.zone.today
+  end
+end
