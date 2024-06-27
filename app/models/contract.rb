bdfa479813974b649dd0bb42ddd55c7a211a8b29
Leandro Alemão <leandro.costantini@gmail.com>
2024-06-27 19:07:13 +0100

    [CPDLP-3094] Rollback contracts stuff

--- app/models/contract.rb
+++
@@ -1,4 +0,0 @@
-class Contract < ApplicationRecord
-  belongs_to :statement
-  belongs_to :course
-end
--- db/seeds/base/add_contracts.rb
+++
@@ -1,9 +0,0 @@
-LeadProvider.find_each do |lead_provider|
-  Cohort.find_each do |cohort|
-    Course.find_each do |course|
-      Statement.where(lead_provider:, cohort:).find_each do |statement|
-        FactoryBot.create(:contract, course.identifier.underscore, statement:)
-      end
-    end
-  end
-end
