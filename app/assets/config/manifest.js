a00becff286e973befa8667acc764dfbdbebf178
Leonidas Apostolidis <leoapost@gmail.com>
2024-07-09 00:46:15 +0100

    Add and configure Sprockets

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/images
+//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/fonts
+//= link application.css
+//= link application.js
+//= link_tree ../builds
