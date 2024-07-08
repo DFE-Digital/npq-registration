d7a7c2ae76c115db8e66b8cef7c7bc242281558e
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-07-08 16:02:59 +0100

    [CPDLP-3103] set required to each individual attribute

--- spec/swagger_schemas/requests/participant_declaration_request.rb
+++ spec/swagger_schemas/requests/participant_declaration_request.rb
@@ -1,15 +1,18 @@
 PARTICIPANT_DECLARATION_REQUEST = {
   description: "A participant declaration data request",
   type: :object,
+  required: %w[type attributes],
   properties: {
     type: {
       type: :string,
+      required: true,
       enum: %w[
         participant-declaration
       ],
       example: "participant-declaration",
     },
     attributes: {
+      required: true,
       anyOf: [
         { "$ref": "#/components/schemas/ParticipantDeclarationStartedRequest" },
         { "$ref": "#/components/schemas/ParticipantDeclarationRetainedRequest" },
--- spec/swagger_schemas/requests/participant_declaration_started_request.rb
+++ spec/swagger_schemas/requests/participant_declaration_started_request.rb
@@ -7,11 +7,13 @@ PARTICIPANT_DECLARATION_STARTED_REQUEST = {
       description: "The unique id of the participant",
       type: :string,
       format: :uuid,
+      required: true,
       example: "db3a7848-7308-4879-942a-c4a70ced400a",
     },
     declaration_type: {
       description: "The event declaration type",
       type: :string,
+      required: true,
       enum: %w[
         started
       ],
@@ -20,12 +22,14 @@ PARTICIPANT_DECLARATION_STARTED_REQUEST = {
     declaration_date: {
       description: "The event declaration date",
       type: :string,
+      required: true,
       format: "date-time",
       example: "2021-05-31T02:21:32.000Z",
     },
     course_identifier: {
       description: "The type of course the participant is enrolled in",
       type: :string,
+      required: true,
       enum: %w[
         npq-leading-teaching
         npq-leading-behaviour-culture
