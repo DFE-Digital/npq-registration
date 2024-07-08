846677f138ec3ca900e73dad8169e875f593b169
Leonidas Apostolidis <leoapost@gmail.com>
2024-07-09 00:41:48 +0100

    Add and setup jsbundling-rails and webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const mode = process.env.NODE_ENV === 'development' ? 'development' : 'production';
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  mode,
+  optimization: {
+    moduleIds: 'deterministic',
+  },
+  entry: {
+    application: "./app/javascript/application.js"
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    chunkFormat: "module",
+    path: path.resolve(__dirname, 'app/assets/builds'),
+  },
+  plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1
+    })
+  ]
+}
