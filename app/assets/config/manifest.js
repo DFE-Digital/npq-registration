4bbc541bc38cbc7b6c2dfcd58b702c615f919be7
Leonidas Apostolidis <leoapost@gmail.com>
2024-07-15 18:16:19 +0100

    Fix swagger assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,4 +3,6 @@
 //= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/fonts
 //= link application.css
 //= link application.js
+//= link swagger-ui.css
+//= link swagger-ui.js
 //= link_tree ../builds
