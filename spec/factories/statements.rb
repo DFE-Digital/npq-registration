94a2a17ef5cee2b39c70ab0909d3086a91a930f6
Ross Oliver <ross.oliver36@gmail.com>
2024-07-23 16:09:31 +0100

    Use current cohort for statements
    Generally speaking the current cohort is the one we will most likely want for
    the majority of test cases.

--- spec/factories/statements.rb
+++ spec/factories/statements.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     year { Faker::Number.between(from: 2021, to: 2024) }
     deadline_date { Faker::Date.forward(days: 30) }
     payment_date { Faker::Date.forward(days: 30) }
-    cohort
+    cohort { create(:cohort, :current) }
     lead_provider
     reconcile_amount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
     state { "open" }
