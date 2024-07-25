7fe1dcb4858022ede6cc63cbbc0caa9bc6fd47dd
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-07-25 11:13:04 +0100

    [CPDLP-3282] Merge conflict fixes

--- db/seeds/base/add_declarations.rb
+++ db/seeds/base/add_declarations.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+helpers = Class.new { include ActiveSupport::Testing::TimeHelpers }.new
+
 LeadProvider.find_each do |lead_provider|
   quantity = { "review" => 4, "development" => 1 }.fetch(Rails.env, 0)
 
@@ -15,12 +17,14 @@ LeadProvider.find_each do |lead_provider|
       cohort: Cohort.all.sample,
     )
     %w[started].each do |declaration_type|
-      FactoryBot.create(
-        :declaration,
-        :submitted_or_eligible,
-        application: application1,
-        declaration_type:,
-      )
+      helpers.travel_to application1.schedule.applies_from do
+        FactoryBot.create(
+          :declaration,
+          :submitted_or_eligible,
+          application: application1,
+          declaration_type:,
+        )
+      end
     end
 
     # Application with a started and retained-1 declaration
@@ -34,12 +38,14 @@ LeadProvider.find_each do |lead_provider|
       cohort: Cohort.all.sample,
     )
     %w[started retained-1].each do |declaration_type|
-      FactoryBot.create(
-        :declaration,
-        :submitted_or_eligible,
-        application: application2,
-        declaration_type:,
-      )
+      helpers.travel_to application2.schedule.applies_from do
+        FactoryBot.create(
+          :declaration,
+          :submitted_or_eligible,
+          application: application2,
+          declaration_type:,
+        )
+      end
     end
 
     # Application with a started, retained-1 and retained-2 declaration
@@ -53,12 +59,14 @@ LeadProvider.find_each do |lead_provider|
       cohort: Cohort.all.sample,
     )
     %w[started retained-1 retained-2].each do |declaration_type|
-      FactoryBot.create(
-        :declaration,
-        :submitted_or_eligible,
-        application: application3,
-        declaration_type:,
-      )
+      helpers.travel_to application3.schedule.applies_from do
+        FactoryBot.create(
+          :declaration,
+          :submitted_or_eligible,
+          application: application3,
+          declaration_type:,
+        )
+      end
     end
 
     # Application with a started, retained-1, retained-2 and completed declaration
@@ -72,20 +80,24 @@ LeadProvider.find_each do |lead_provider|
       cohort: Cohort.all.sample,
     )
     %w[started retained-1 retained-2 completed].each do |declaration_type|
-      declaration = FactoryBot.create(
-        :declaration,
-        :submitted_or_eligible,
-        application: application4,
-        declaration_type:,
-      )
+      declaration = helpers.travel_to(application4.schedule.applies_from) do
+        FactoryBot.create(
+          :declaration,
+          :submitted_or_eligible,
+          application: application4,
+          declaration_type:,
+        )
+      end
 
       next unless declaration_type == "completed"
 
       ParticipantOutcomes::Create::STATES.reverse.each do |state|
-        FactoryBot.create(:participant_outcome,
-                          declaration:,
-                          state:,
-                          completion_date: declaration.declaration_date.to_s)
+        helpers.travel_to declaration.declaration_date do
+          FactoryBot.create(:participant_outcome,
+                            declaration:,
+                            state:,
+                            completion_date: declaration.declaration_date.to_s)
+        end
 
         break if Faker::Boolean.boolean(true_ratio: 0.3)
       end
