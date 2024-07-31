9f2467edec0fd8c38974488a7f001de62392d257
Ross Oliver <ross.oliver36@gmail.com>
2024-07-31 13:17:20 +0100

    Add missing data attribute to declaration request
    The request body should be nested in a `data` attribute.

--- spec/swagger_schemas/requests/participant_declaration_request.rb
+++ spec/swagger_schemas/requests/participant_declaration_request.rb
@@ -1,23 +1,30 @@
 PARTICIPANT_DECLARATION_REQUEST = {
   description: "A participant declaration data request",
   type: :object,
-  required: %w[type attributes],
+  required: %w[data],
   properties: {
-    type: {
-      type: :string,
-      required: true,
-      enum: %w[
-        participant-declaration
-      ],
-      example: "participant-declaration",
-    },
-    attributes: {
-      required: true,
-      anyOf: [
-        { "$ref": "#/components/schemas/ParticipantDeclarationStartedRequest" },
-        { "$ref": "#/components/schemas/ParticipantDeclarationRetainedRequest" },
-        { "$ref": "#/components/schemas/ParticipantDeclarationCompletedRequest" },
-      ],
+    data: {
+      description: "A participant declaration data request",
+      type: :object,
+      required: %w[type attributes],
+      properties: {
+        type: {
+          type: :string,
+          required: true,
+          enum: %w[
+            participant-declaration
+          ],
+          example: "participant-declaration",
+        },
+        attributes: {
+          required: true,
+          anyOf: [
+            { "$ref": "#/components/schemas/ParticipantDeclarationStartedRequest" },
+            { "$ref": "#/components/schemas/ParticipantDeclarationRetainedRequest" },
+            { "$ref": "#/components/schemas/ParticipantDeclarationCompletedRequest" },
+          ],
+        },
+      },
     },
   },
 }.freeze
