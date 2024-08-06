140b67ed165e1a331b0bcde05bad8d79c98a5d2f
ethax-ross <ross.oliver36@gmail.com>
2024-08-06 15:22:04 +0100

    Only return one declaration error at a time on void/create (#1563)
    Currently we can return multiple declaration errors when voiding a declaration.
    
    Only return a single declaration error at a time.
    
    We also want to omit the `course_identifier` error when there is a
    `participant_id` error.

--- spec/support/matchers/error_matcher.rb
+++ spec/support/matchers/error_matcher.rb
@@ -1,7 +1,10 @@
 RSpec::Matchers.define :have_error do |attribute, type, message, context|
   match do |actual|
     actual.invalid?(context) && actual.errors.any? do |error|
-      error.attribute == attribute && error.type == type && error.message == message
+      error_type_match = type.nil? || error.type == type
+      error_message_match = message.nil? || error.message == message
+
+      error.attribute == attribute && error_type_match && error_message_match
     end
   end
 end
