b22bdff2630caa858ac08dec1042fd96adeed4b6
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-08 16:17:52 +0100

    Fix for docker-compose

--- db/migrate/20230106114221_update_default_cohort_for_ehco.rb
+++ db/migrate/20230106114221_update_default_cohort_for_ehco.rb
@@ -1,6 +1,10 @@
 class UpdateDefaultCohortForEhco < ActiveRecord::Migration[6.1]
   def change
-    ehco_course = Course.where(identifier: "npq-early-headship-coaching-offer").first
+    ehco_course = if ActiveRecord::Base.connection.column_exists?(:courses, :identifier)
+                    Course.where(identifier: "npq-early-headship-coaching-offer").first
+                  else
+                    Course.where(name: "Early Headship Coaching Offer").first
+                  end
 
     ehco_course&.update!(default_cohort: 2022)
   end
