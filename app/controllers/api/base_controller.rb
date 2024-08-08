73fc8de4c787df00f8403d72957a945234a0e30e
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-08 17:51:52 +0100

    [CPDLP-3293] moved 'append_info_to_payload' to a concern

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -4,33 +4,12 @@ module API
 
     include API::TokenAuthenticatable
     include ActionController::MimeResponds
+    include API::LoggerPayload
 
     rescue_from ActionController::UnpermittedParameters, with: :unpermitted_parameter_response
     rescue_from ActionController::BadRequest, with: :bad_request_response
     rescue_from ArgumentError, with: :bad_request_response
     rescue_from API::Errors::FilterValidationError, with: :filter_validation_error_response
-
-    def append_info_to_payload(payload)
-      super
-      payload[:current_user_class] = current_lead_provider&.class&.name
-      payload[:current_user_id] = current_lead_provider&.id
-      payload[:current_user_name] = current_lead_provider&.name
-
-      payload[:query_params] = request.query_parameters.to_json
-      payload[:request_headers] = request.env.slice(
-        "HTTP_HOST",
-        "HTTP_ACCEPT",
-        "HTTP_VERSION",
-        "QUERY_STRING",
-        "HTTP_USER_AGENT",
-      ).transform_values(&:to_s).to_json
-      payload[:request_body] = request.raw_post
-
-      payload[:response_headers] = response.headers.transform_values(&:to_s).to_json
-      if response.status != 200
-        payload[:response_body] = response.body
-      end
-    end
 
   private
 
---
+++ spec/controllers/concerns/api/logger_payload_spec.rb
@@ -0,0 +1,113 @@
+require "rails_helper"
+
+class ControllerWithLoggerPayload
+  prepend API::LoggerPayload
+
+  def initialize(s = 200)
+    @response_status = s
+  end
+
+  def current_lead_provider
+    OpenStruct.new(
+      class: LeadProvider,
+      id: 9999,
+      name: "Best Provider",
+    )
+  end
+
+  def request
+    OpenStruct.new(
+      query_parameters: { page: 5 },
+      env: {
+        "HTTP_HOST" => "HOST_123",
+        "HTTP_ACCEPT" => "ACCEPT_123",
+        "HTTP_VERSION" => "VERSION_123",
+        "QUERY_STRING" => "STRING_123",
+        "HTTP_USER_AGENT" => "AGENT_123",
+        "SOMETHING_ELSE" => "XXX123",
+      },
+      raw_post: "RAW_POST",
+    )
+  end
+
+  def response
+    OpenStruct.new(
+      headers: {
+        "HEADER1" => "123",
+        "HEADER2" => "321",
+      },
+      status: @response_status,
+      body: "RAW_BODY",
+    )
+  end
+
+  def append_info_to_payload(payload)
+  end
+end
+
+RSpec.describe API::LoggerPayload do
+  subject { ControllerWithLoggerPayload.new(response_status) }
+  let(:payload) { Hash.new }
+
+  describe "#append_info_to_payload" do
+    context "when response status 200" do
+      let(:response_status) { 200 }
+
+      it "should append payload data" do
+        subject.append_info_to_payload(payload)
+
+        expect(payload[:current_user_class]).to eq("LeadProvider")
+        expect(payload[:current_user_id]).to eq(9999)
+        expect(payload[:current_user_name]).to eq("Best Provider")
+
+        expect(payload[:query_params]).to eq({page: 5}.to_json)
+
+        expect(payload[:request_headers]).to eq({
+          "HTTP_HOST" => "HOST_123",
+          "HTTP_ACCEPT" => "ACCEPT_123",
+          "HTTP_VERSION" => "VERSION_123",
+          "QUERY_STRING" => "STRING_123",
+          "HTTP_USER_AGENT" => "AGENT_123",
+        }.to_json)
+        expect(payload[:request_body]).to eq("RAW_POST")
+
+        expect(payload[:response_headers]).to eq({
+          "HEADER1" => "123",
+          "HEADER2" => "321",
+        }.to_json)
+
+        expect(payload[:response_body]).to be_nil
+      end
+    end
+
+    context "when response status 400" do
+      let(:response_status) { 400 }
+
+      it "should include response_body" do
+        subject.append_info_to_payload(payload)
+
+        expect(payload[:current_user_class]).to eq("LeadProvider")
+        expect(payload[:current_user_id]).to eq(9999)
+        expect(payload[:current_user_name]).to eq("Best Provider")
+
+        expect(payload[:query_params]).to eq({page: 5}.to_json)
+
+        expect(payload[:request_headers]).to eq({
+          "HTTP_HOST" => "HOST_123",
+          "HTTP_ACCEPT" => "ACCEPT_123",
+          "HTTP_VERSION" => "VERSION_123",
+          "QUERY_STRING" => "STRING_123",
+          "HTTP_USER_AGENT" => "AGENT_123",
+        }.to_json)
+        expect(payload[:request_body]).to eq("RAW_POST")
+
+        expect(payload[:response_headers]).to eq({
+          "HEADER1" => "123",
+          "HEADER2" => "321",
+        }.to_json)
+
+        expect(payload[:response_body]).to eq("RAW_BODY")
+      end
+    end
+  end
+end
