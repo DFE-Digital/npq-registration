ad07b8caa0a047bee4a7ade02f12afe50a61e3c2
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-09 16:10:23 +0100

    [CPDLP-3293] Make sure empty hash is not sent as '{}'

--- app/controllers/concerns/api/logger_payload.rb
+++ app/controllers/concerns/api/logger_payload.rb
@@ -2,12 +2,14 @@ module API
   module LoggerPayload
     def append_info_to_payload(payload)
       super
-      payload[:current_user_class] = current_lead_provider&.class&.name
-      payload[:current_user_id] = current_lead_provider&.id
-      payload[:current_user_name] = current_lead_provider&.name
+      params = {}
 
-      payload[:query_params] = request.query_parameters.to_json
-      payload[:request_headers] = request.env.slice(
+      params[:current_user_class] = current_lead_provider&.class&.name
+      params[:current_user_id] = current_lead_provider&.id
+      params[:current_user_name] = current_lead_provider&.name
+
+      params[:query_params] = request.query_parameters
+      params[:request_headers] = request.env.slice(
         "HTTP_VERSION",
         "HTTP_HOST",
         "HTTP_USER_AGENT",
@@ -16,13 +18,23 @@ module API
         "HTTP_CONNECTION",
         "HTTP_CACHE_CONTROL",
         "QUERY_STRING",
-      ).transform_values(&:to_s).to_json
-      payload[:request_body] = request.raw_post
+      ).transform_values(&:to_s)
+      params[:request_body] = request.raw_post
 
-      payload[:response_headers] = response.headers.transform_values(&:to_s).to_json
+      params[:response_headers] = response.headers.transform_values(&:to_s)
       if response.status != 200
-        payload[:response_body] = response.body
+        params[:response_body] = response.body
       end
+
+      params.transform_values! do |val|
+        if val.is_a?(Hash)
+          val = val.present? ? val.to_json : nil
+        end
+
+        val.presence
+      end
+
+      payload.merge!(params)
     end
   end
 end
