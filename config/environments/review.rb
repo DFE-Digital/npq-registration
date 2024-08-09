4629e760a2ff188995eb2e96d441545dbf7a586d
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-09 09:48:03 +0100

    [CPDLP-3293] Set log_level to :info

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -1,7 +1,6 @@
 require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
-  config.log_level = :info
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
 
   # Enable/disable aspects of the NPQ separation
