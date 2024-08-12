6d5a6ae35a0e622eb0095c27b9e7ea316cec8812
Toby Benedyk <tobybenedyk@gmail.com>
2024-08-12 12:17:59 +0100

    [CPDNPQ-2004] TRN import with missing leading zeros

--- app/services/importers/manual_validation.rb
+++ app/services/importers/manual_validation.rb
@@ -16,9 +16,10 @@ class Importers::ManualValidation
       Rails.logger.debug "no application found for #{row['application_ecf_id']}" if application.nil?
       next if application.nil?
 
-      Rails.logger.debug "updating trn for application: #{row['application_ecf_id']} with trn: #{row['validated_trn']}"
+      trn = add_leading_zero(row["validated_trn"])
+      Rails.logger.debug "updating trn for application: #{row['application_ecf_id']} with trn: #{trn}"
 
-      application.user.update!(trn: row["validated_trn"], trn_verified: true)
+      application.user.update!(trn:, trn_verified: true)
     end
   end
 
@@ -32,5 +33,9 @@ private
 
   def rows
     @rows ||= CSV.read(path_to_csv, headers: true)
+  end
+
+  def add_leading_zero(trn)
+    trn.rjust(7, "0")
   end
 end
