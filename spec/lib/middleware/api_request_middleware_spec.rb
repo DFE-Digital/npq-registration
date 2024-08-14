a9d870bb969f19285f3f8046c428e651e30c23ee
Leandro C. <leandro.costantini@gmail.com>
2024-08-14 10:55:32 +0100

    [CPDLP-3292] Stream API requests events to BQ (#1571)
    * [CPDLP-3292] Stream API requests events to BQ
    
    * [CPDLP-3292] Address PR comments

---
+++ spec/lib/middleware/api_request_middleware_spec.rb
@@ -0,0 +1,106 @@
+require "rails_helper"
+require "middleware/api_request_middleware"
+
+RSpec.describe Middleware::ApiRequestMiddleware, type: :request do
+  let(:status) { 200 }
+  let(:request) { Rack::MockRequest.new(subject) }
+  let(:headers) { { "HEADER" => "Yeah!" } }
+  let(:mock_response) { ["Hellowwworlds!"] }
+
+  let(:mock_app) do
+    lambda do |env|
+      @env = env
+      [status, headers, mock_response]
+    end
+  end
+
+  subject { described_class.new(mock_app) }
+
+  before do
+    allow(Rails).to receive(:env) { environment.inquiry }
+    allow(StreamAPIRequestsToBigQueryJob).to receive(:perform_later)
+  end
+
+  context "when running in other environments other than the allowed ones" do
+    let(:environment) { "test" }
+
+    describe "#call on a non-API path" do
+      it "does not fire StreamAPIRequestsToBigQueryJob" do
+        request.get "/"
+
+        expect(StreamAPIRequestsToBigQueryJob).not_to have_received(:perform_later)
+      end
+    end
+
+    describe "#call on an API path" do
+      it "does not fire StreamAPIRequestsToBigQueryJob" do
+        request.get "/api/v1/participants/npq", params: { foo: "bar" }
+
+        expect(StreamAPIRequestsToBigQueryJob).not_to have_received(:perform_later)
+      end
+    end
+  end
+
+  context "when running in allowed environments" do
+    let(:environment) { "separation" }
+
+    describe "#call on a non-API path" do
+      it "does not fire StreamAPIRequestsToBigQueryJob" do
+        request.get "/"
+
+        expect(StreamAPIRequestsToBigQueryJob).not_to have_received(:perform_later)
+      end
+    end
+
+    describe "#call on an API path" do
+      it "fires an StreamAPIRequestsToBigQueryJob" do
+        request.get "/api/v1/participants/npq", params: { foo: "bar" }
+
+        expect(StreamAPIRequestsToBigQueryJob).to have_received(:perform_later).with(
+          hash_including("path" => "/api/v1/participants/npq", "params" => { "foo" => "bar" }, "method" => "GET"), anything, 200, anything
+        )
+      end
+    end
+
+    describe "#call on a different version API path" do
+      it "fires an StreamAPIRequestsToBigQueryJob" do
+        request.get "/api/v3/participants/npq", params: { foo: "bar" }
+
+        expect(StreamAPIRequestsToBigQueryJob).to have_received(:perform_later).with(
+          hash_including("path" => "/api/v3/participants/npq", "params" => { "foo" => "bar" }, "method" => "GET"), anything, 200, anything
+        )
+      end
+    end
+
+    describe "#call on an API path with POST data" do
+      it "fires an StreamAPIRequestsToBigQueryJob including post data" do
+        request.post "/api/v1/participant-declarations", as: :json, params: { foo: "bar" }.to_json
+
+        expect(StreamAPIRequestsToBigQueryJob).to have_received(:perform_later).with(
+          hash_including("path" => "/api/v1/participant-declarations", "body" => '{"foo":"bar"}', "method" => "POST"), anything, 200, anything
+        )
+      end
+    end
+
+    describe "#call on an API path when an exception happens in the job" do
+      it "logs the exception and returns" do
+        allow(Rails.logger).to receive(:warn)
+        allow(StreamAPIRequestsToBigQueryJob).to receive(:perform_later).and_raise(StandardError)
+
+        request.get "/api/v1/participants/npq"
+
+        expect(Rails.logger).to have_received(:warn)
+      end
+    end
+  end
+
+  context "when `api_enabled` flag is false" do
+    before { allow(Rails.application.config).to receive(:npq_separation).and_return({ api_enabled: false }) }
+
+    it "does not fire StreamAPIRequestsToBigQueryJob" do
+      request.get "/api/v1/participants/npq", params: { foo: "bar" }
+
+      expect(StreamAPIRequestsToBigQueryJob).not_to have_received(:perform_later)
+    end
+  end
+end
