b0bc22a99e42d7fe562fb42bc119b532888f3234
Toby Benedyk <tobybenedyk@gmail.com>
2024-08-27 10:20:39 +0100

    CPDNPQ-2028 migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_120004) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_23_120904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "citext"
