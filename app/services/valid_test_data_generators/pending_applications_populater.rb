6724d42025c56245b30f2c0ee7bc62c0bc14ea25
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-28 16:09:07 +0100

    [CPDLP-3410] Creates addtional pending applications for providers

---
+++ app/services/valid_test_data_generators/pending_applications_populater.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "active_support/testing/time_helpers"
+
+module ValidTestDataGenerators
+  class PendingApplicationsPopulater < ApplicationsPopulater
+    include ActiveSupport::Testing::TimeHelpers
+
+    def populate
+      return unless Rails.env.in?(%w[development review separation])
+
+      logger.info "PendingApplicationsPopulater: Started!"
+
+      ActiveRecord::Base.transaction do
+        create_participants!
+      end
+
+      logger.info "PendingApplicationsPopulater: Finished!"
+    end
+
+  private
+
+    def create_participant(school:)
+      course = courses.sample
+      schedule = Schedule.where(cohort:, course_group: course.course_group).sample
+      create_application(create_user, school, course, schedule)
+    end
+  end
+end
