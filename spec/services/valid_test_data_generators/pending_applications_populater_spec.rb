4bae2ada51b3ee2df3c169351b8adb2b8d65b73c
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-30 15:41:54 +0100

    [CPDLP-3410] Creates addtional pending applications for providers (#1646)

---
+++ spec/services/valid_test_data_generators/pending_applications_populater_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe ValidTestDataGenerators::PendingApplicationsPopulater, :with_default_schedules do
+  let(:lead_provider) { create(:lead_provider) }
+  let(:cohort) { create(:cohort, :current) }
+
+  before do
+    allow(Rails).to receive(:env) { environment.inquiry }
+  end
+
+  subject { described_class.new(lead_provider:, cohort:, number_of_participants: 22) }
+
+  describe "#populate" do
+    context "when running in other environment other than separation or development" do
+      let(:environment) { "test" }
+
+      it "returns nil" do
+        expect(subject.populate).to be_nil
+        expect(Application.count).to eq(0)
+      end
+    end
+
+    context "when running in development or separation environments" do
+      let(:environment) { "separation" }
+
+      it "creates participants" do
+        subject.populate
+
+        expect(User.count).to eq(22)
+        expect(Application.count).to eq(22)
+        expect(Application.where(lead_provider_approval_status: "pending").count).to eq(22)
+        expect(Application.all.map(&:cohort).uniq.first).to eq(cohort)
+        expect(Application.all.map(&:lead_provider).uniq.first).to eq(lead_provider)
+      end
+    end
+  end
+end
