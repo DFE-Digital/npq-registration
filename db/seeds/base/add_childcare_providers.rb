e4f4555dc5edba9ab7ed0bf356f0fab79ff0f4ff
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-08-30 15:34:09 +0100

    Move seed CSV files to 'db/seeds/data' folder

--- db/seeds/base/add_childcare_providers.rb
+++ db/seeds/base/add_childcare_providers.rb
@@ -1,4 +1,4 @@
-CSV.read(Rails.root.join("db/seeds/private_childcare_providers.csv"), headers: true).tap do |data|
+CSV.read(Rails.root.join("db/seeds/data/private_childcare_providers.csv"), headers: true).tap do |data|
   Rails.logger.info("Importing 1000 private childcare providers")
 
   PrivateChildcareProvider.insert_all(data.first(1000).map(&:to_h))
