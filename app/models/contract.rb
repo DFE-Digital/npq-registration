595400cfadfec766b8badf86a8b6dda24b1f6a3c
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-09-02 16:04:24 +0100

    [CPDLP-3448] Move error message to translation file

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -3,5 +3,5 @@ class Contract < ApplicationRecord
   belongs_to :course
   belongs_to :contract_template
 
-  validates :course_id, uniqueness: { scope: :statement_id, message: "Can only have one contract for statement and course" }
+  validates :course_id, uniqueness: { scope: :statement_id }
 end
--- app/models/contract_template.rb
+++ app/models/contract_template.rb
@@ -8,17 +8,14 @@ class ContractTemplate < ApplicationRecord
             numericality: {
               only_integer: true,
               greater_than_or_equal_to: 0,
-              message: "Must be an integer greater than or equal to zero",
             }
   validates :per_participant,
             numericality: {
               greater_than: 0.0,
-              message: "Must be greater than zero",
             }
   validates :recruitment_target,
             numericality: {
               only_integer: true,
               greater_than: 0,
-              message: "Must be an integer greater than zero",
             }
 end
