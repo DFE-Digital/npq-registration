a12b51aae848c1fbb09174a98099a627a2277dbb
Leandro Alemão <leandro.costantini@gmail.com>
2024-09-03 13:14:38 +0100

    Temporarily toggle api on in sandbox for testing

--- config/environments/sandbox.rb
+++ config/environments/sandbox.rb
@@ -1,14 +1,14 @@
 require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
-  config.log_level = :info
+  config.log_level = :debug
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
 
   # Enable/disable aspects of the NPQ separation
   config.npq_separation = {
-    admin_portal_enabled: false,
-    api_enabled: false,
+    admin_portal_enabled: true,
+    api_enabled: true,
     migration_enabled: false,
-    ecf_api_disabled: false,
+    ecf_api_disabled: true,
   }
 end
