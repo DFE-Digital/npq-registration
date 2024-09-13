c36ebfdaea898a2737534584444ead5eb0617e09
Peter Yates <peter.yates@graphia.co.uk>
2024-09-13 17:01:50 +0100

    Ensure the user registrations page isn't public (#1706)
    This page doesn't really show any personal data but should be behind a
    login regardless. No data can be changed from here without signing in.

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -3,9 +3,21 @@ require "rails_helper"
 RSpec.feature "Account", type: :feature do
   include_context "Stub Get An Identity Omniauth Responses"
 
-  scenario "when not logged in, it redirects to sign in" do
-    visit "/account"
-    expect(page).to be_accessible
-    expect(page).to have_current_path("/sign-in")
+  describe "accounts page" do
+    scenario "when not logged in, it redirects to sign in" do
+      visit "/account"
+      expect(page).to be_accessible
+      expect(page).to have_current_path("/sign-in")
+    end
+  end
+
+  describe "accounts user registration page" do
+    let!(:application) { FactoryBot.create(:application) }
+
+    scenario "when not logged in, it redirects to sign in" do
+      visit(accounts_user_registration_path(application.id))
+
+      expect(page).to have_current_path("/sign-in")
+    end
   end
 end
