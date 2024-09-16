469734f982701535bd34d9d68d775a55668ce8ac
Abeer Salameh <cwrw@users.noreply.github.com>
2024-09-16 18:36:12 +0100

    Added 'How-to guides' anchor link to primary navigation on API guidance pages (#1709)
    Moved 'How-to guides' link to right of primary navigation
    
    Edit prefix
    
    Co-authored-by: mark-avery-dfe <80474565+mark-avery-dfe@users.noreply.github.com>

--- app/components/npq_separation/navigation_structures/guidance_navigation_structure.rb
+++ app/components/npq_separation/navigation_structures/guidance_navigation_structure.rb
@@ -22,6 +22,11 @@ module NpqSeparation
             href: api_guidance_page_path(page: "release-notes"),
             prefix: "/api/guidance/release-notes",
           ) => [],
+          Node.new(
+            name: "How-to guides",
+            href: api_guidance_page_path(page: "/", anchor: "how-to-guides"),
+            prefix: "/api/guidance/",
+          ) => [],
         }
       end
     end
