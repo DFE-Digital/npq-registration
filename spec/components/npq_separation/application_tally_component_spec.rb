b06fde5e184efb8832a998a30b5d6ba3de1a8054
Richard Worrall <richard@soupish.net>
2024-09-19 17:04:24 +0100

    Make ApplicationTallyComponent spec deterministic

--- spec/components/npq_separation/application_tally_component_spec.rb
+++ spec/components/npq_separation/application_tally_component_spec.rb
@@ -4,18 +4,19 @@ RSpec.describe NpqSeparation::ApplicationTallyComponent, type: :component do
   subject { described_class.new(Application.where(cohort:), :course) }
 
   let(:cohort) { create :cohort, :current }
-  let(:courses) { 2.times.map { create :course } }
+  let(:course_1) { create :course, :early_headship_coaching_offer }
+  let(:course_2) { create :course, :additional_support_offer }
 
   before do
-    create(:application, cohort:, course: courses[0])
-    create(:application, cohort:, course: courses[0])
-    create(:application, cohort:, course: courses[1])
+    create(:application, cohort:, course: course_1)
+    create(:application, cohort:, course: course_1)
+    create(:application, cohort:, course: course_2)
   end
 
   it "returns the correct rows" do
     expect(subject.rows).to eq([
-      [courses[0].name, 2],
-      [courses[1].name, 1],
+      [course_2.name, 1],
+      [course_1.name, 2],
     ])
   end
 end
