405b5b95762d83e84bbe377172fb48a3273dc670
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-09-19 15:21:25 +0100

    [CPDLP-3577] Migration::UserMerger to merge users

---
+++ spec/services/migration/user_merger_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Migration::UserMerger do
+  let(:from_user) { create(:user, uid: SecureRandom.uuid) }
+  let(:to_user) { create(:user, uid: SecureRandom.uuid) }
+
+  subject { described_class.new(from_user:, to_user:) }
+
+  describe ".merge" do
+    context "when from_user has applications" do
+      let!(:application) { create(:application, user: from_user) }
+
+      it "moves applications over to to_user" do
+        expect(application.user).to eq(from_user)
+
+        subject.merge!
+
+        expect(application.reload.user).to eq(to_user)
+        expect(from_user.uid).to be_nil
+      end
+    end
+
+    context "when from_user has participant_id_changes" do
+      let!(:participant_id_change) { create(:participant_id_change, user: from_user) }
+
+      it "moves participant_id_changes over to to_user" do
+        expect(participant_id_change.user).to eq(from_user)
+
+        subject.merge!
+
+        expect(participant_id_change.reload.user).to eq(to_user)
+        expect(from_user.uid).to be_nil
+      end
+    end
+  end
+end
