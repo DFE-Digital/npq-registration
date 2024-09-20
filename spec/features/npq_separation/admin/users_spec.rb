07cdc03dc1ab24302be6e30d0f47692692c74c7c
Richard Worrall <richard@soupish.net>
2024-09-20 14:32:13 +0100

    Make separation users feature spec more reliable

--- spec/features/npq_separation/admin/users_spec.rb
+++ spec/features/npq_separation/admin/users_spec.rb
@@ -7,7 +7,7 @@ RSpec.feature "Listing and viewing users", type: :feature do
   let(:user) { User.first }
 
   before do
-    create_list(:user, users_per_page + 1, :with_get_an_identity_id)
+    create_list(:user, users_per_page, :with_get_an_identity_id)
     sign_in_as(create(:admin))
   end
 
@@ -16,16 +16,16 @@ RSpec.feature "Listing and viewing users", type: :feature do
 
     expect(page).to have_css("h1", text: "All participants")
 
-    User.limit(users_per_page).each do |user|
+    User.all.find_each do |user|
       expect(page).to have_link(user.full_name, href: npq_separation_admin_user_path(user))
       expect(page).to have_css("td", text: user.trn)
       expect(page).to have_css("td", text: user.created_at.to_date.to_formatted_s(:govuk))
     end
-
-    expect(page).to have_css(".govuk-pagination__item--current", text: 1)
   end
 
   scenario "navigating to the second page of users" do
+    create :user, :with_get_an_identity_id # exceed pagination threshold
+
     visit(npq_separation_admin_users_path)
 
     click_on("Next")
