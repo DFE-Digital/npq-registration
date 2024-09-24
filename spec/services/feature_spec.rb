ba6ac228e8d22b40545ef62d87aa6163382c5cde
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-09-24 14:50:24 +0100

    [CPDLP-3493] merge second feature spec into spec/services/feature_spec.rb

--- spec/services/feature_spec.rb
+++ spec/services/feature_spec.rb
@@ -1,6 +1,28 @@
 require "rails_helper"
 
 RSpec.describe Feature do
+  describe "#registration_closed?" do
+    context 'with Flipper flag "Registration open" turned on' do
+      before do
+        Flipper.enable(Feature::REGISTRATION_OPEN)
+      end
+
+      it "returns true" do
+        expect(described_class.registration_closed?(nil)).to be false
+      end
+    end
+
+    context 'with Flipper flag "Registration open" turned off' do
+      before do
+        Flipper.disable(Feature::REGISTRATION_OPEN)
+      end
+
+      it "returns false" do
+        expect(described_class.registration_closed?(nil)).to be true
+      end
+    end
+  end
+
   describe "#ecf_api_disabled?" do
     context "when enabled" do
       before do
