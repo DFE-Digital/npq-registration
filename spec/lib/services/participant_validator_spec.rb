ffed4bd8cc686b207f649e080c5c09c379478a6e
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-09-30 11:13:01 +0100

    [CPDLP-3586] NPQ Internal API - Implement DQT API calls in NPQ

--- spec/lib/services/participant_validator_spec.rb
+++ spec/lib/services/participant_validator_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ParticipantValidator do
       full_name:,
       date_of_birth:,
       national_insurance_number:,
-    )
+    ).call
   end
 
   let(:trn) { rand(1_000_000..9_999_999).to_s }
@@ -16,107 +16,160 @@ RSpec.describe ParticipantValidator do
   let(:national_insurance_number) { "AB123456C" }
 
   describe "#call" do
-    context "when matching trn found" do
-      let(:body) do
-        {
-          data: {
-            attributes: {
+    context "when ecf_api_disabled is false" do
+      before { allow(Feature).to receive(:ecf_api_disabled?).and_return(false) }
+
+      context "when matching trn found" do
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                trn:,
+                qts: true,
+                active_alert: false,
+              },
+            },
+          }
+        end
+
+        before do
+          stub_request(:post,
+                       "https://ecf-app.gov.uk/api/v1/participant-validation")
+          .with(
+            headers: {
+              "Authorization" => "Bearer ECFAPPBEARERTOKEN",
+            },
+            body: {
               trn:,
-              qts: true,
-              active_alert: false,
+              date_of_birth: date_of_birth.iso8601,
+              full_name:,
+              nino: national_insurance_number,
             },
-          },
-        }
+          )
+          .to_return(status: 200, body: body.to_json, headers: {})
+        end
+
+        it "returns record with matching trn" do
+          expect(subject.trn).to eql(trn)
+          expect(subject.active_alert).to be_falsey
+        end
       end
 
-      before do
-        stub_request(:post,
-                     "https://ecf-app.gov.uk/api/v1/participant-validation")
-        .with(
-          headers: {
-            "Authorization" => "Bearer ECFAPPBEARERTOKEN",
-          },
-          body: {
-            trn:,
-            date_of_birth: date_of_birth.iso8601,
-            full_name:,
-            nino: national_insurance_number,
-          },
-        )
-        .to_return(status: 200, body: body.to_json, headers: {})
-      end
-
-      it "returns record with matching trn" do
-        expect(subject.call.trn).to eql(trn)
-        expect(subject.call.active_alert).to be_falsey
-      end
-    end
-
-    context "when different trn found by fuzzy matching" do
-      let(:body) do
-        {
-          data: {
-            attributes: {
-              trn: (trn.to_i + 1).to_s,
-              qts: true,
-              active_alert: false,
+      context "when different trn found by fuzzy matching" do
+        let(:body) do
+          {
+            data: {
+              attributes: {
+                trn: (trn.to_i + 1).to_s,
+                qts: true,
+                active_alert: false,
+              },
             },
-          },
-        }
+          }
+        end
+
+        before do
+          stub_request(:post,
+                       "https://ecf-app.gov.uk/api/v1/participant-validation")
+          .with(
+            headers: {
+              "Authorization" => "Bearer ECFAPPBEARERTOKEN",
+            },
+            body: {
+              trn:,
+              date_of_birth: date_of_birth.iso8601,
+              full_name:,
+              nino: national_insurance_number,
+            },
+          )
+          .to_return(status: 200, body: body.to_json, headers: {})
+        end
+
+        it "returns record with diffrent trn" do
+          expect(subject.trn).to be_present
+          expect(subject.trn).not_to eql(trn)
+          expect(subject.active_alert).to be_falsey
+        end
       end
 
-      before do
-        stub_request(:post,
-                     "https://ecf-app.gov.uk/api/v1/participant-validation")
-        .with(
-          headers: {
-            "Authorization" => "Bearer ECFAPPBEARERTOKEN",
-          },
-          body: {
-            trn:,
-            date_of_birth: date_of_birth.iso8601,
-            full_name:,
-            nino: national_insurance_number,
-          },
-        )
-        .to_return(status: 200, body: body.to_json, headers: {})
-      end
+      context "when no record could be found" do
+        before do
+          stub_request(:post,
+                       "https://ecf-app.gov.uk/api/v1/participant-validation")
+          .with(
+            headers: {
+              "Authorization" => "Bearer ECFAPPBEARERTOKEN",
+            },
+            body: {
+              trn:,
+              date_of_birth: date_of_birth.iso8601,
+              full_name:,
+              nino: national_insurance_number,
+            },
+          )
+          .to_return(status: 404, body: "", headers: {})
+        end
 
-      it "returns record with diffrent trn" do
-        expect(subject.call.trn).to be_present
-        expect(subject.call.trn).not_to eql(trn)
-        expect(subject.call.active_alert).to be_falsey
+        it "returns nil" do
+          expect(subject).to be_nil
+        end
       end
     end
 
-    context "when no record could be found" do
-      before do
-        stub_request(:post,
-                     "https://ecf-app.gov.uk/api/v1/participant-validation")
-        .with(
-          headers: {
-            "Authorization" => "Bearer ECFAPPBEARERTOKEN",
-          },
-          body: {
-            trn:,
-            date_of_birth: date_of_birth.iso8601,
-            full_name:,
-            nino: national_insurance_number,
-          },
-        )
-        .to_return(status: 404, body: "", headers: {})
+    context "when ecf_api_disabled is true" do
+      before { allow(Feature).to receive(:ecf_api_disabled?).and_return(true) }
+
+      context "when matching trn found" do
+        let(:body) do
+          {
+            "trn" => trn,
+            "active_alert" => false,
+          }
+        end
+
+        before do
+          allow(Dqt::V1::Teacher).to receive(:validate_trn)
+            .with(trn:, birthdate: date_of_birth.iso8601, nino: national_insurance_number)
+            .and_return(body)
+        end
+
+        it "returns record with matching trn" do
+          expect(subject.trn).to eq(trn)
+          expect(subject.active_alert).to be(false)
+        end
       end
 
-      it "returns nil" do
-        expect(subject.call).to be_nil
+      context "when different trn found by fuzzy matching" do
+        let(:body) do
+          {
+            "trn" => (trn.to_i + 1).to_s,
+            "active_alert" => false,
+          }
+        end
+
+        before do
+          allow(Dqt::V1::Teacher).to receive(:validate_trn)
+            .with(trn:, birthdate: date_of_birth.iso8601, nino: national_insurance_number)
+            .and_return(body)
+        end
+
+        it "returns record with diffrent trn" do
+          expect(subject.trn).to be_present
+          expect(subject.trn).not_to eql(trn)
+          expect(subject.active_alert).to be_falsey
+        end
       end
-    end
 
-    context "when ecf_api_disabled flag is toggled on" do
-      before { Flipper.enable(Feature::ECF_API_DISABLED) }
+      context "when no record could be found" do
+        before do
+          allow(Dqt::V1::Teacher).to receive(:validate_trn)
+            .with(trn:, birthdate: date_of_birth.iso8601, nino: national_insurance_number)
+            .and_return(nil)
+        end
 
-      it "returns nil" do
-        expect(subject.call).to be_nil
+        it "returns nil" do
+          expect(subject).to be_nil
+        end
       end
     end
   end
