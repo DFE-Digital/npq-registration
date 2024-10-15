1900c3ef437aa12ddb539fe4cb0a29e0d5b76c16
Ross Oliver <ross.oliver36@gmail.com>
2024-10-15 11:22:06 +0100

    Add ECF models for ApiToken generation
    Bring across the models from ECF that we need to be able to generate API tokens
    for ECF in NPQ registration.
    
    I haven't improved the test coverage here, just a quick lift and shift from ECF.

---
+++ app/models/migration/ecf/lead_provider_api_token.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Migration::Ecf
+  class LeadProviderAPIToken < APIToken
+    belongs_to :lead_provider, optional: true
+    belongs_to :cpd_lead_provider, optional: true
+
+    def owner
+      cpd_lead_provider
+    end
+
+    def owner_description
+      "CPD lead provider: #{cpd_lead_provider.name}"
+    end
+  end
+end
---
+++ spec/models/migration/ecf/lead_provider_api_token_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe Migration::Ecf::LeadProviderAPIToken, type: :model do
+  it "generates a hashed token that can be used" do
+    unhashed_token = described_class.create_with_random_token!(lead_provider: create(:lead_provider))
+
+    expect(
+      described_class.find_by_unhashed_token(unhashed_token),
+    ).to eql(described_class.order(:created_at).last)
+  end
+end
