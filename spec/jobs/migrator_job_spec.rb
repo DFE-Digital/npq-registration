362ff44b2e37966bd0ade838917b9910b807dae3
Leandro Alemão <leandro.costantini@gmail.com>
2024-10-28 21:58:16 +0000

    [CPDLP-3640] Add retry limit into migration jobs

--- spec/jobs/migrator_job_spec.rb
+++ spec/jobs/migrator_job_spec.rb
@@ -2,11 +2,10 @@ require "rails_helper"
 
 RSpec.describe MigratorJob do
   let(:instance) { described_class.new }
+  let(:migrator) { Migration::Migrators::Cohort }
+  let(:worker) { 0 }
 
   describe "#perform" do
-    let(:migrator) { Migration::Migrators::Cohort }
-    let(:worker) { 0 }
-
     subject(:perform_migration) { instance.perform(migrator:, worker:) }
 
     before { Migration::Migrators::Cohort.prepare! }
@@ -15,6 +14,12 @@ RSpec.describe MigratorJob do
       expect(migrator).to receive(:call).with(worker:).once
 
       perform_migration
+    end
+  end
+
+  describe "#perform_later" do
+    it "enqueues the job exactly once" do
+      expect { described_class.perform_later(migrator:, worker:) }.to have_enqueued_job(described_class).exactly(:once).on_queue("high_priority")
     end
   end
 end
