cb0d1eba3901065a9654566beea60ff6218d0507
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-10-31 11:05:59 +0000

    [CPDLP-3693] Keep the old tests

--- spec/services/migration/migrators/declaration_spec.rb
+++ spec/services/migration/migrators/declaration_spec.rb
@@ -32,6 +32,8 @@ RSpec.describe Migration::Migrators::Declaration do
         expect(declaration.state_reason).to eq(ecf_resource1.declaration_states.last.state_reason)
         expect(declaration.cohort.start_year).to eq(ecf_resource1.cohort.start_year)
         expect(declaration.lead_provider.ecf_id).to eq(ecf_resource1.cpd_lead_provider.npq_lead_provider.id)
+        expect(declaration.application.course.identifier.downcase).to eq(ecf_resource1.course_identifier.downcase)
+        expect(declaration.application.user.ecf_id).to eq(ecf_resource1.user.id)
         expect(declaration.application.ecf_id).to eq(ecf_resource1.participant_profile.npq_application.id)
         expect(declaration.created_at.to_s).to eq(ecf_resource1.created_at.to_s)
         expect(declaration.updated_at.to_s).to eq(ecf_resource1.updated_at.to_s)
