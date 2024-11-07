7eb591dd8c524575ea55949ae02f46b32fca48dd
jebw <jeb@jdwilkins.co.uk>
2024-11-07 16:27:45 +0000

    [CPDNPQ-2142] Hooked up calculator on Payment Auth form

--- app/components/npq_separation/admin/statement_details_component.rb
+++ app/components/npq_separation/admin/statement_details_component.rb
@@ -7,27 +7,7 @@ module NpqSeparation
 
       def initialize(statement:)
         @statement = statement
-        # FIXME: Replace with real calculator
-        @calculator = stub_calculator
-      end
-
-    private
-
-      def stub_calculator
-        OpenStruct.new(
-          total_output_payment: 1.0,
-          total_targeted_delivery_funding: 1.0,
-          total_service_fees: 1.0,
-          clawback_payments: 1.0,
-          total_targeted_delivery_funding_refundable: 1.0,
-          total_clawbacks: 1.0,
-          total_payment: 1.0,
-          total_starts: 1,
-          total_retained: 1,
-          total_completed: 1,
-          total_voided: 1,
-          show_targeted_delivery_funding?: false,
-        )
+        @calculator = ::Statements::SummaryCalculator.new(statement: @statement)
       end
     end
   end
