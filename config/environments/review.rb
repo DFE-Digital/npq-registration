1dff17ad7a31abf2d62360ae72051081d59d4715
Leandro Alemão <leandro.costantini@gmail.com>
2024-11-12 12:35:49 +0000

    Fix cache_store for non prod envs

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -2,6 +2,7 @@ require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
+  config.cache_store = :null_store
 
   # Enable/disable aspects of the NPQ separation
   config.npq_separation = {
