e801e24131b8251a1ed4605cdea4075d78e95ad1
jebw <jeb@jdwilkins.co.uk>
2024-11-12 16:45:58 +0000

    [CPDNPQ-2153] Implemented form to change status

--- spec/views/npq_separation/admin/applications/revert_to_pending/new.html.erb_spec.rb
+++ spec/views/npq_separation/admin/applications/revert_to_pending/new.html.erb_spec.rb
@@ -3,5 +3,26 @@ require "rails_helper"
 RSpec.describe "npq_separation/admin/applications/revert_to_pending/new", type: :view do
   subject { render }
 
-  it "needs testing", pending: "view implementation"
+  before do
+    assign(:application, application)
+    assign(:revert_to_pending_form, revert_form)
+  end
+
+  let(:application) { build_stubbed(:application, :accepted) }
+  let(:revert_form) { Applications::RevertToPendingForm.new(application, {}) }
+  let(:form_path) { npq_separation_admin_applications_revert_to_pending_path(application) }
+
+  it { is_expected.to have_css("h1", text: /Are you sure/) }
+  it { is_expected.to have_css(%(form[action="#{form_path}"]), count: 1) }
+  it { is_expected.to have_field("applications-revert-to-pending-form-change-status-to-pending-yes-field") }
+  it { is_expected.to have_css("legend", text: "Change the status to pending?") }
+  it { is_expected.to have_css("label", text: "Yes") }
+  it { is_expected.to have_css("label", text: "No") }
+
+  context "when there are form errors" do
+    before { revert_form.valid? }
+
+    it { is_expected.to have_css(".govuk-error-summary") }
+    it { is_expected.to have_css(".govuk-error-message") }
+  end
 end
