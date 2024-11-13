5eb8f28de0a5637050b6cdb640b6f4511fd5eda6
Emma Frith <emma.frith@education.gov.uk>
2024-11-13 15:34:09 +0000

    add update method to change feature state

--- app/controllers/admin/features_controller.rb
+++ app/controllers/admin/features_controller.rb
@@ -7,5 +7,17 @@ class Admin::FeaturesController < AdminController
     def show
       @feature = Flipper[params[:id]]
     end
+
+    def update
+      @feature = Flipper[params[:id]]
+
+      if @feature.enabled?
+        @feature.disable
+      else
+        @feature.enable
+      end
+
+      redirect_back fallback_location: admin_features_path, notice: "Feature '#{@feature.name}' has been updated."
+    end
   
 end
