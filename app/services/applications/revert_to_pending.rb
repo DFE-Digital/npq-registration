70d5e8be8b6c3bbf1afc33fa18920cf11f141050
jebw <jeb@jdwilkins.co.uk>
2024-11-13 12:22:49 +0000

    [CPDNPQ-2153] Remove declarations
    when reverting application to pending

--- app/services/applications/revert_to_pending.rb
+++ app/services/applications/revert_to_pending.rb
@@ -1,5 +1,7 @@
 module Applications
   class RevertToPending
+    BLOCKING_DECLARATION_STATES = %w[submitted voided ineligible].freeze
+
     class << self
       def call(application)
         new(application).call
@@ -11,7 +13,27 @@ module Applications
     end
 
     def call
-      @application.update!(lead_provider_approval_status: :pending)
+      Application.transaction do
+        return true if @application.pending_lead_provider_approval_status?
+
+        if application_has_declarations?
+          raise RevertToPendingError, "Cannot revert to pending, Application has Declarations"
+        end
+
+        @application.declarations.destroy_all
+        @application.funded_place = nil
+        @application.pending_lead_provider_approval_status!
+
+        true
+      end
+    end
+
+    class RevertToPendingError < StandardError; end
+
+  private
+
+    def application_has_declarations?
+      @application.declarations.where(state: BLOCKING_DECLARATION_STATES).any?
     end
   end
 end
--- spec/services/applications/revert_to_pending_spec.rb
+++ spec/services/applications/revert_to_pending_spec.rb
@@ -10,14 +10,63 @@ RSpec.describe Applications::RevertToPending do
 
     it "updates lead provider approval status" do
       expect { call_service }
-        .to change(application, :lead_provider_approval_status).from("accepted").to("pending")
+        .to change { application.reload.lead_provider_approval_status }
+                   .from("accepted")
+                   .to("pending")
+    end
+
+    it "empties the funded_place attribute" do
+      expect { call_service }
+        .to change { application.reload.funded_place }
+                   .from(false)
+                   .to(nil)
+    end
+
+    context "when already pending" do
+      let(:application) { create(:application, :pending) }
+
+      it "succeeds but does not change the approval status" do
+        expect { call_service }
+          .to not_change(application, :lead_provider_approval_status)
+      end
+    end
+
+    context "when Application already has declarations" do
+      %i[submitted voided ineligible].each do |declaration_state|
+        context "with #{declaration_state} state" do
+          let(:application) { create(:declaration, declaration_state).application }
+
+          it "raises an exception" do
+            expect { call_service }
+              .to raise_exception(Applications::RevertToPending::RevertToPendingError)
+              .and(not_change { application.reload.lead_provider_approval_status })
+              .and(not_change { application.declarations.count })
+          end
+        end
+      end
+
+      %i[eligible payable paid awaiting_clawback clawed_back].each do |declaration_state|
+        context "with #{declaration_state} state" do
+          let(:application) { create(:declaration, declaration_state).application }
+
+          it "updates the state" do
+            expect { call_service }
+              .to change { application.reload.lead_provider_approval_status }
+                  .from("accepted")
+                  .to("pending")
+              .and change { application.declarations.count }.to(0)
+          end
+        end
+      end
     end
   end
 
   describe "#call" do
     it "updates lead provider approval status" do
-      expect { instance.call && application.reload }
-        .to change(application, :lead_provider_approval_status).from("accepted").to("pending")
+      expect { instance.call }
+        .to change { application.reload.lead_provider_approval_status }
+                   .from("accepted")
+                   .to("pending")
     end
   end
 end
