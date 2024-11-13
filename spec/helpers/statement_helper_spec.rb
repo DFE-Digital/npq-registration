1eab26ddc676a6be379ebcb92ea00f8c1ce9ad31
Richard Worrall <richard@soupish.net>
2024-11-13 10:39:18 +0000

    Cover statement_period in statement_helper_spec

--- spec/helpers/statement_helper_spec.rb
+++ spec/helpers/statement_helper_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe StatementHelper, type: :helper do
 
     it { is_expected.to eq("March 2024") }
   end
+
+  describe "#statement_period" do
+    subject { statement_period(statement) }
+
+    let(:statement) { build(:statement, month: 3, year: 2024) }
+
+    it { is_expected.to eq("2024-3") }
+  end
 end
