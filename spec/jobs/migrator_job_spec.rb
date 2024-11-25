ba099d260d5443787e9ec8cb8cff76159381060f
Leandro C. <leandro.costantini@gmail.com>
2024-11-25 10:31:34 +0000

    [CPDLP-3730] Add `discard_on` method instead of `max_attempts` to jobs (#1999)

--- spec/jobs/migrator_job_spec.rb
+++ spec/jobs/migrator_job_spec.rb
@@ -1,25 +1,52 @@
 require "rails_helper"
 
-RSpec.describe MigratorJob do
-  let(:instance) { described_class.new }
+RSpec.describe MigratorJob, type: :job do
   let(:migrator) { Migration::Migrators::Cohort }
   let(:worker) { 0 }
 
   describe "#perform" do
-    subject(:perform_migration) { instance.perform(migrator:, worker:) }
+    subject(:job) { described_class.new.perform(migrator:, worker:) }
 
     before { Migration::Migrators::Cohort.prepare! }
 
     it "runs a migrator" do
       expect(migrator).to receive(:call).with(worker:).once
 
-      perform_migration
+      job
     end
   end
 
   describe "#perform_later" do
-    it "enqueues the job exactly once on migration queue" do
-      expect { described_class.perform_later(migrator:, worker:) }.to have_enqueued_job(described_class).exactly(:once).on_queue("migration")
+    subject(:job) { described_class.perform_later(migrator:, worker:) }
+
+    before do
+      allow(migrator).to receive(:call).and_return(true)
+      allow(Rails.logger).to receive(:warn)
+      allow(Sentry).to receive(:capture_exception).and_return(true)
+    end
+
+    it "enqueues the job exactly once" do
+      expect { job }.to have_enqueued_job(described_class).exactly(:once).on_queue("migration")
+    end
+
+    context "with valid job" do
+      before do
+        perform_enqueued_jobs { job }
+      end
+
+      it { expect(Sentry).not_to have_received(:capture_exception) }
+      it { expect(Delayed::Job.count).to be_zero }
+    end
+
+    context "with invalid job" do
+      before do
+        allow(migrator).to receive(:call).and_raise(ActiveRecord::RecordInvalid)
+
+        perform_enqueued_jobs { job }
+      end
+
+      it { expect(Sentry).to have_received(:capture_exception) }
+      it { expect(Delayed::Job.count).to be_zero }
     end
   end
 end
