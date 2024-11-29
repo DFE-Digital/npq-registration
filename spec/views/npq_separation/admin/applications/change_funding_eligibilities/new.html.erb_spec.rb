689d5fd65ddd01f4ebb962138ff9646257391a9f
jebw <jeb@jdwilkins.co.uk>
2024-11-29 09:52:04 +0000

    [CPDNPQ-2155] Implemented form for changing funding

--- spec/views/npq_separation/admin/applications/change_funding_eligibilities/new.html.erb_spec.rb
+++ spec/views/npq_separation/admin/applications/change_funding_eligibilities/new.html.erb_spec.rb
@@ -5,19 +5,27 @@ require "rails_helper"
 RSpec.describe "npq_separation/admin/applications/change_funding_eligibilities/new", type: :view do
   subject { render }
 
-  before { assign(:application, application) }
+  before do
+    assign(:application, application)
+    assign(:funding_eligibility, change_funding_eligibility)
+  end
 
   let(:application) { build_stubbed(:application, :accepted) }
+  let(:change_funding_eligibility) { Applications::ChangeFundingEligibility.new(application:) }
 
   let :form_path do
     npq_separation_admin_applications_change_funding_eligibility_path(application)
   end
 
-  it { is_expected.to have_css("h1") }
-  xit { is_expected.to have_css(%(form[action="#{form_path}"]), count: 1) }
+  it { is_expected.to have_css("h1", text: application.user.full_name) }
+  it { is_expected.to have_css(%(form[action="#{form_path}"]), count: 1) }
+  it { is_expected.to have_css(%(.govuk-form-group fieldset label), text: "Yes") }
+  it { is_expected.to have_css(%(.govuk-form-group fieldset label), text: "No") }
+  # it { is_expected.to have_css("optgroup", count: 2) }
+  # it { is_expected.to have_css("optgroup option") }
 
-  xcontext "when there are form errors" do
-    before { update_form.valid? }
+  context "when there are form errors" do
+    before { change_funding_eligibility.valid? }
 
     it { is_expected.to have_css(".govuk-error-summary") }
     it { is_expected.to have_css(".govuk-error-message") }
