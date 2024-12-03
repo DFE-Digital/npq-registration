7b87e204d317ef19d67f914aac545a0f13455bd0
jebw <jeb@jdwilkins.co.uk>
2024-12-03 11:30:41 +0000

    [CPDNPQ-2155] Check for billable declartions and for funded place

--- spec/services/applications/change_funding_eligibility_spec.rb
+++ spec/services/applications/change_funding_eligibility_spec.rb
@@ -7,6 +7,36 @@ RSpec.describe Applications::ChangeFundingEligibility, type: :model do
 
   let(:application) { create(:application, :accepted) }
 
+  describe "validations" do
+    it { is_expected.to validate_presence_of :application }
+
+    context "with application with billable declarations" do
+      subject { service.tap(&:valid?).errors.full_messages }
+
+      let(:service) { described_class.new(application:, eligible_for_funding: false) }
+
+      let :application do
+        create(:application, :accepted, eligible_for_funding: false).tap do |application|
+          create(:declaration, :eligible, application:)
+        end
+      end
+
+      it { is_expected.to include(/billable declaration exists/i) }
+    end
+
+    context "with application with funded place" do
+      subject { service.tap(&:valid?).errors.full_messages }
+
+      let(:service) { described_class.new(application:, eligible_for_funding: false) }
+
+      let :application do
+        create(:application, :accepted, funded_place: true, eligible_for_funding: true)
+      end
+
+      it { is_expected.to include(/application is funded/i) }
+    end
+  end
+
   describe "#change_funding_eligibility" do
     subject(:make_change) { service.change_funding_eligibility }
 
