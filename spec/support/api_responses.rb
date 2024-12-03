84b355606d3488c1998242bd4e2ce7d23b8d7636
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-12-03 16:14:46 +0000

    [CPDLP-3775] qualified teacher check with DQT directly

--- spec/support/api_responses.rb
+++ spec/support/api_responses.rb
@@ -1,25 +1,17 @@
 def dqt_response_body(trn: "1234567", date_of_birth: "1960-12-13", active_alert: false)
   {
-    data: {
-      attributes: {
-        teacher_reference_number: trn,
-        full_name: "John Doe",
-        date_of_birth:,
-        national_insurance_number: "AB123456C",
-        qts_date: "1990-12-13",
-        active_alert:,
-      },
-    },
+    "trn": trn,
+    "ni_number": "AB123456C",
+    "name": "Jane Doe",
+    "dob": date_of_birth,
+    "active_alert": active_alert,
+    "state_name": "Active",
   }.to_json
 end
 
-def participant_validator_response(state_name: "Active", trn: "1234567", name: "Jane Smith", ni_number: "AB123456C", dob: "1960-12-13", active_alert: false)
+def ecf_funding_lookup_response(previously_funded:, previously_received_targeted_funding_support: false)
   {
-    state_name:,
-    trn:,
-    name:,
-    ni_number:,
-    dob:,
-    active_alert:,
+    previously_funded:,
+    previously_received_targeted_funding_support:,
   }.to_json
 end
