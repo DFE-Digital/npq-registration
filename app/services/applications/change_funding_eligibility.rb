6db7120962cf80f7e2786346bf4ec8e4cb735f8d
jebw <jeb@jdwilkins.co.uk>
2024-12-03 11:30:41 +0000

    [CPDNPQ-2155] Check for billable declartions and for funded place

--- app/services/applications/change_funding_eligibility.rb
+++ app/services/applications/change_funding_eligibility.rb
@@ -16,6 +16,9 @@ module Applications
     validates :application, presence: true
     validates :eligible_for_funding, inclusion: OPTIONS.keys
 
+    validate  :validate_funding_eligiblity_status_code_change, if: :application
+    validate  :validate_funding_eligiblity_status_with_funded_place, if: :application
+
     def eligible_for_funding_options
       OPTIONS
     end
@@ -27,6 +30,24 @@ module Applications
         (eligible_for_funding ? :marked_funded_by_policy : :marked_ineligible_by_policy)
 
       application.update!(eligible_for_funding:, funding_eligiblity_status_code:)
+    end
+
+  private
+
+    def validate_funding_eligiblity_status_code_change
+      if declared_as_billable? && eligible_for_funding == false
+        errors.add(:base, :billable_declaration_exists)
+      end
+    end
+
+    def validate_funding_eligiblity_status_with_funded_place
+      if !eligible_for_funding && application.funded_place
+        errors.add(:base, :funded_application)
+      end
+    end
+
+    def declared_as_billable?
+      application.declarations.billable.count.positive?
     end
   end
 end
