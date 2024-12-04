9c9da524450c2a98142e7bdc4d58d7087f1d1e9f
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2024-12-04 14:54:13 +0000

    perform bulk revert

--- app/controllers/npq_separation/admin/bulk_operations/applications_uploads_controller.rb
+++ app/controllers/npq_separation/admin/bulk_operations/applications_uploads_controller.rb
@@ -1,7 +1,7 @@
 module NpqSeparation::Admin::BulkOperations
   class ApplicationsUploadsController < AdminController
     def create
-      current_admin.bulk_operations.not_run.destroy_all
+      current_admin.bulk_operations.where(type: "BulkOperations::RevertApplicationsToPending").not_ran.destroy_all
       bulk_operation = BulkOperations::RevertApplicationsToPending.create! admin: current_admin
       bulk_operation.file.attach(params[:file])
       bulk_operation.update!(rows: bulk_operation.file.download.lines.count)
