5522c173fb1bb24f59f5b2bbbea4d230a7b7383a
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-12-04 15:38:48 +0000

    [CPDLP-3775] replaced mock_previous_funding_api_request helper

--- spec/features/journeys/happy_paths/when_previously_funded_spec.rb
+++ spec/features/journeys/happy_paths/when_previously_funded_spec.rb
@@ -14,13 +14,10 @@ RSpec.feature "Happy journeys", :with_default_schedules, type: :feature do
   end
 
   include_context "retrieve latest application data"
-  include_context "Stub previously funding check for all courses" do
-    let(:trn) { user_trn }
-  end
   include_context "Stub Get An Identity Omniauth Responses"
 
   def run_scenario(js:)
-    stub_participant_validation_request
+    stub_participant_validation_request(trn: user_trn, response: { trn: user_trn, date_of_birth: "1980-12-13" })
 
     navigate_to_page(path: "/", submit_form: false, axe_check: false) do
       expect(page).to have_text("Before you start")
@@ -59,6 +56,8 @@ RSpec.feature "Happy journeys", :with_default_schedules, type: :feature do
       expect(page).to have_text("Do you or your employer have an Ofsted unique reference number (URN)?")
       page.choose("Yes", visible: :all)
     end
+
+    mock_previous_funding(previously_funded: true)
 
     choose_a_private_childcare_provider(js:, urn: "EY123456", name: "searchable childcare provider")
 
@@ -141,7 +140,7 @@ RSpec.feature "Happy journeys", :with_default_schedules, type: :feature do
       "archived_email" => nil,
       "archived_at" => nil,
       "date_of_birth" => "1980-12-13",
-      "ecf_id" => User.last.ecf_id,
+      "ecf_id" => latest_application_user.ecf_id,
       "email" => "user@example.com",
       "full_name" => "John Doe",
       "get_an_identity_id_synced_to_ecf" => false,
@@ -158,10 +157,10 @@ RSpec.feature "Happy journeys", :with_default_schedules, type: :feature do
 
     deep_compare_application_data(
       "accepted_at" => nil,
-      "cohort_id" => Cohort.current.id,
+      "cohort_id" => latest_application.cohort_id,
       "course_id" => Course.find_by(identifier: "npq-early-headship-coaching-offer").id,
       "schedule_id" => nil,
-      "ecf_id" => nil,
+      "ecf_id" => latest_application.ecf_id,
       "eligible_for_funding" => false,
       "employer_name" => nil,
       "employment_type" => nil,
