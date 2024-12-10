03757d7cb0a9074d94dcb2734b2aeac1862756f3
Mooktakim Ahmed <Mooktakim@fluxter.co.uk>
2024-12-10 16:12:48 +0000

    [CPDLP-3775] generate ecf_id if nil for User and Application

--- db/migrate/20241203191058_auto_gen_uuid_ecf_id_for_applications.rb
+++ db/migrate/20241203191058_auto_gen_uuid_ecf_id_for_applications.rb
@@ -1,6 +1,12 @@
 class AutoGenUuidEcfIdForApplications < ActiveRecord::Migration[7.1]
-  def change
+  def up
     change_column_default :applications, :ecf_id, from: nil, to: "gen_random_uuid()"
+    execute "UPDATE applications SET ecf_id = gen_random_uuid() WHERE ecf_id IS NULL;"
     change_column_null :applications, :ecf_id, false
+  end
+
+  def down
+    change_column_null :applications, :ecf_id, true
+    change_column_default :applications, :ecf_id, from: "gen_random_uuid()", to: nil
   end
 end
