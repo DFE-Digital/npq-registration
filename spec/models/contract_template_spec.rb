b19ea730849c2a2b549b4be263ffe60cd60ef5cb
Sławosz Sławiński <slawosz@gmail.com>
2024-12-11 09:05:38 +0000

    Updating contract data, fixes 2350 data request (#2066)

--- spec/models/contract_template_spec.rb
+++ spec/models/contract_template_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe ContractTemplate, type: :model do
     it { is_expected.to validate_numericality_of(:recruitment_target).only_integer.is_greater_than(0).with_message("Must be an integer greater than zero") }
     it { is_expected.to validate_uniqueness_of(:ecf_id).case_insensitive.with_message("ECF ID must be unique").allow_nil }
   end
+
+  describe "#new_from_existing" do
+    let(:contract_template) { create(:contract_template, per_participant: 123.0) }
+
+    let(:new_contract_template) { contract_template.new_from_existing(per_participant: 321.0) }
+
+    it "is not persisted" do
+      expect(new_contract_template).not_to be_persisted
+    end
+
+    it "overrides attributes" do
+      expect(new_contract_template.per_participant).to eq(321.0)
+    end
+
+    it "is copying attributes if not specified" do
+      new_attributes = contract_template.new_from_existing({})
+      expect(new_attributes.attributes.except("created_at", "updated_at", "id", "ecf_id")).to eq(contract_template.attributes.except("created_at", "updated_at", "id", "ecf_id"))
+    end
+  end
 end
