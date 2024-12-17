82b166a41af6d894ab4f6a8e07e822ab28667a16
Jeb Wilkins <jeb@jdwilkins.co.uk>
2024-12-17 09:33:04 +0000

    [CPDNPQ-2155] Manage funding eligibility (#2029)
    * [CPDNPQ-2155] Stubbed ChangeFundingEligibiliesController
    
    * [CPDNPQ-2155] Updating eligible for funding field
    
    * [CPDNPQ-2155] Implemented form for changing funding
    
    * [CPDNPQ-2155] Added feature spec and improved validation error
    
    * [CPDNPQ-2155] Assign funding_eligibility_status_code
    
    * [CPDNPQ-2155] Check for billable declartions and for funded place
    
    * [CPDNPQ-2155] Address review comments
    
    * [CPDNPQ-2155] Prevent updating funding eligibility
    
    on applications with submitted states

---
+++ app/services/applications/change_funding_eligibility.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Applications
+  class ChangeFundingEligibility
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+
+    OPTIONS = {
+      true => I18n.t("shared.yes"),
+      false => I18n.t("shared.no"),
+    }.freeze
+
+    attribute :application
+    attribute :eligible_for_funding, :boolean
+
+    validates :application, presence: true
+    validates :eligible_for_funding, inclusion: OPTIONS.keys
+
+    validate  :validate_funding_eligiblity_status_code_change, if: :application
+    validate  :validate_funding_eligiblity_status_with_funded_place, if: :application
+
+    def eligible_for_funding_options
+      OPTIONS
+    end
+
+    def change_funding_eligibility
+      return false if invalid?
+
+      funding_eligiblity_status_code =
+        (eligible_for_funding ? :marked_funded_by_policy : :marked_ineligible_by_policy)
+
+      application.update!(eligible_for_funding:, funding_eligiblity_status_code:)
+    end
+
+  private
+
+    def validate_funding_eligiblity_status_code_change
+      if declared_as_billable_or_changeable? && eligible_for_funding == false
+        errors.add(:base, :declaration_exists)
+      end
+    end
+
+    def validate_funding_eligiblity_status_with_funded_place
+      if !eligible_for_funding && application.funded_place
+        errors.add(:base, :funded_application)
+      end
+    end
+
+    def declared_as_billable_or_changeable?
+      application.declarations.billable_or_changeable.count.positive?
+    end
+  end
+end
