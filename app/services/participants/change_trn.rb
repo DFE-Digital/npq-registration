d25d5c40be9d1c45b173f74c5c256f3f473f1bda
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2024-12-18 10:12:27 +0000

    [CPDNPQ-2178] admin change TRN (#2063)
    * allow user TRN to be changed from admin console
    
    * add missing spec for calling TeacherReferenceNumber to pad and remove non-digits from TRN
    
    * add missing spec for TRNs with less than 7 characters
    
    * remove unused code
    
    * fix wording in spec
    
    * make spec check for error messages
    
    * move TRN validation into a single validator
    
    * admin change TRN error case
    
    * change length validation to check exact length
    
    * make width full so ID does not wrap
    
    * do not use html_safe
    
    * better action method name instead of call
    
    * better default error messages
    
    * update trn_verified to true

---
+++ app/services/participants/change_trn.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Participants
+  class ChangeTrn
+    include ActiveModel::Model
+    include ActiveModel::Attributes
+    include ActiveModel::Validations::Callbacks
+
+    attribute :user
+    attribute :trn
+
+    before_validation :strip_trn_whitespace
+
+    validates :trn, valid_trn: true
+
+    def change_trn
+      return false if invalid?
+
+      user.update(trn:, trn_verified: true) # rubocop:disable Rails/SaveBang - return value is used by caller
+    end
+
+  private
+
+    def strip_trn_whitespace
+      self.trn = trn&.gsub(" ", "")
+    end
+  end
+end
