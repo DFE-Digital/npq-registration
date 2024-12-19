8186886c4a112e2ae7d31b3955e6e84d41ff2d8c
Sławosz Sławiński <slawosz@gmail.com>
2024-12-19 12:49:05 +0000

    2326 update statements (#2090)
    * Introduce FinancialChangeLog to store the financial record changes
    
    * Rake task to update statements from ticket 2326
    
    * Improve description
    
    * Proper end line
    
    * Add missing test for the log on update
    
    * Code cosmetics suggestions from PR
    
    * Imporve FinancialChangeLog - specs, validation, schema
    
    * Minor code improvements
    
    * Fix rubocop
    
    * Suggestion from Alkesh how to save changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_12_170751) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_18_101840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "citext"
@@ -238,6 +238,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_12_170751) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["syncable_id", "syncable_type"], name: "index_ecf_sync_request_logs_on_syncable_id_and_syncable_type"
+  end
+
+  create_table "financial_change_logs", force: :cascade do |t|
+    t.string "operation_description", null: false
+    t.json "data_changes", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "flipper_features", force: :cascade do |t|
