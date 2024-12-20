ef0e6c35977e13b794f024e5fe03e662bc6b0b23
Slawosz Slawinski <slawosz@gmail.com>
2024-12-20 14:03:05 +0000

    Refactor spec - add context and before blocks

--- spec/requests/admin/features_controller_spec.rb
+++ spec/requests/admin/features_controller_spec.rb
@@ -2,30 +2,27 @@ require "rails_helper"
 
 RSpec.describe NpqSeparation::Admin::Finance::Statements::PaymentAuthorisationsController, type: :request do
   include Helpers::NPQSeparationAdminLogin
-  it "changes feature flag" do
-    sign_in_as_admin
-    # create an enabled feature flag
-    Flipper.enable("test")
-    # run controller action
-    patch "/admin/features/test", params: { feature_flag_name: "test" }
-    # expect the feature flag to be disabled
-    expect(Flipper.enabled?("test")).to be false
+
+  before { Flipper.enable("test") }
+
+  context "when user is logged in" do
+    before { sign_in_as_admin }
+
+    it "changes feature flag" do
+      patch "/admin/features/test", params: { feature_flag_name: "test" }
+      expect(Flipper.enabled?("test")).to be false
+    end
+
+    it "redirects to the show page" do
+      patch "/admin/features/test", params: { feature_flag_name: "test" }
+      expect(response).to redirect_to(admin_feature_path("test"))
+    end
   end
-  it "redirects to the show page" do
-    sign_in_as_admin
-    # create an enabled feature flag
-    Flipper.enable("test")
-    # run controller action
-    patch "/admin/features/test", params: { feature_flag_name: "test" }
-    # expect the feature flag to be disabled
-    expect(response).to redirect_to(admin_feature_path("test"))
-  end
-  it "redirects to the admin sign in page if not signed in" do
-    # create an enabled feature flag
-    Flipper.enable("test")
-    # run controller action
-    patch "/admin/features/test", params: { feature_flag_name: "test" }
-    # expect the feature flag to be disabled
-    expect(response).to redirect_to(sign_in_path)
+
+  context "when user is logged in" do
+    it "redirects to the admin sign in page if not signed in" do
+      patch "/admin/features/test", params: { feature_flag_name: "test" }
+      expect(response).to redirect_to(sign_in_path)
+    end
   end
 end
