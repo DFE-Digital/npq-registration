7bd3feab89c46c383d292bf36130f71d3ea3f518
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-01-10 11:46:46 +0000

    rake spec to only test what the rake task is doing. move out of one_off namespace

--- spec/support/shared_examples/rake_tasks_support.rb
+++ spec/support/shared_examples/rake_tasks_support.rb
@@ -5,3 +5,32 @@ RSpec.shared_examples "exiting with error code 1" do
     expect { run_task }.to raise_error(an_instance_of(SystemExit).and(having_attributes(status: 1)))
   end
 end
+
+RSpec.shared_examples "passing dry_run to the service" do
+  context "when dry run false" do
+    let(:dry_run) { "false" }
+
+    it "calls the service with dry_run: false" do
+      expect(service_double).to receive(:call).with(dry_run: false)
+      run_task
+    end
+  end
+
+  context "when dry run true" do
+    let(:dry_run) { "true" }
+
+    it "calls the service with dry_run: true" do
+      expect(service_double).to receive(:call).with(dry_run: true)
+      run_task
+    end
+  end
+
+  context "when dry run not specified" do
+    let(:dry_run) { nil }
+
+    it "calls the service with dry_run: true" do
+      expect(service_double).to receive(:call).with(dry_run: true)
+      run_task
+    end
+  end
+end
