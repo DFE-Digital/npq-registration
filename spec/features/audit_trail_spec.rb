aead6d652ce0f606c0b7fbdb42f9d4dce353567e
jebw <jeb@jdwilkins.co.uk>
2025-01-13 10:57:37 +0000

    [CPDNPQ-2407] Track user when lead provider

--- spec/features/audit_trail_spec.rb
+++ spec/features/audit_trail_spec.rb
@@ -35,7 +35,35 @@ RSpec.feature "Recording audit trail via papertrail", :versioning, type: :reques
   end
 
   describe "a lead provider making changes" do
-    it "assigns lead provider to whodunnit"
+    subject(:change_author) { application.reload.versions.last.whodunnit }
+
+    before do
+      travel_to Date.parse("2024-12-13") # ensure schedule identifier is predicable
+
+      APIToken.create_with_known_token!(raw_token, lead_provider:)
+      create(:schedule, :npq_leadership_autumn,
+             course_group: application.course.course_group,
+             cohort: application.cohort)
+
+      post accept_api_v3_application_path(application.ecf_id), headers:
+    end
+
+    let(:raw_token) { "a-token" }
+    let(:course) { create :"npq-senior-leadership" }
+    let(:lead_provider) { create :lead_provider }
+    let(:application) { create :application, lead_provider:, course: }
+
+    let :headers do
+      {
+        "Content-Type" => "application/json",
+        "Authorization" =>
+          ActionController::HttpAuthentication::Token.encode_credentials(raw_token),
+      }
+    end
+
+    it "records the lead providers details" do
+      expect(change_author).to eq "Lead provider #{lead_provider.id}"
+    end
   end
 
   describe "a public user making changes" do
