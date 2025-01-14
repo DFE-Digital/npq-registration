661b34c2eb8b2e3b9b2966696e215804faee4754
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-01-14 09:51:11 +0000

    use shared helper in rake task spec

--- spec/lib/tasks/one_off/load_legacy_participant_outcomes_spec.rb
+++ spec/lib/tasks/one_off/load_legacy_participant_outcomes_spec.rb
@@ -50,8 +50,6 @@ RSpec.describe "one_off:legacy_participant_outcomes:import" do
   context "when the CSV file is not found" do
     subject(:run_task) { Rake::Task["one_off:legacy_participant_outcomes:import"].execute(file_path: "nonexistent_file") }
 
-    it "exits with error code 1" do
-      expect { run_task }.to raise_error(an_instance_of(SystemExit).and(having_attributes(status: 1)))
-    end
+    it_behaves_like "exiting with error code 1"
   end
 end
