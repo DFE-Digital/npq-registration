3b9c7df2886809f8afd2e48d54be3813ac1d9aa8
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-02-03 15:19:20 +0000

    make content security policy more secure

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# need to disable Lint/PercentStringArray here, because CSP keywords need to be single-quoted
 # rubocop:disable Lint/PercentStringArray
 SecureHeaders::Configuration.default do |config|
   config.x_frame_options = "DENY"
@@ -9,8 +10,14 @@ SecureHeaders::Configuration.default do |config|
   config.x_permitted_cross_domain_policies = "none"
   config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
 
-  google_analytics = %w[www.google-analytics.com ssl.google-analytics.com *.googletagmanager.com tagmanager.google.com *.googleusercontent.com *.gstatic.com]
+  google_analytics = %w[*.google-analytics.com *.analytics.google.com *.googletagmanager.com tagmanager.google.com *.googleusercontent.com *.gstatic.com]
   tracking_pixels = %w[www.facebook.com px.ads.linkedin.com]
+  sentry = []
+
+  if ENV["SENTRY_REPORT_URI"]
+    sentry_report_uri = [ENV["SENTRY_REPORT_URI"]]
+    sentry = [sentry_report_uri.host]
+  end
 
   config.csp = SecureHeaders::OPT_OUT
 
@@ -19,19 +26,18 @@ SecureHeaders::Configuration.default do |config|
     base_uri: %w['self'],
     upgrade_insecure_requests: true,
     child_src: %w['self'],
-    connect_src: %W['self'] + google_analytics,
+    connect_src: %w['self'] + google_analytics + sentry,
     font_src: %w['self' *.gov.uk fonts.gstatic.com],
     form_action: %w['self'],
     frame_ancestors: %w['self'],
     frame_src: %w['self'] + google_analytics,
-    img_src: %W['self' data: *.gov.uk] + google_analytics + tracking_pixels,
+    img_src: %w['self' data: *.gov.uk] + google_analytics + tracking_pixels,
     manifest_src: %w['self'],
     media_src: %w['self'],
-    script_src: %W['self' 'unsafe-inline' 'unsafe-eval' *.gov.uk https://cdn.jsdelivr.net/npm/chart.js] + google_analytics,
-    style_src: %w['self' 'unsafe-inline' *.gov.uk fonts.googleapis.com] + google_analytics,
+    script_src: %w['self' *.gov.uk https://cdn.jsdelivr.net/npm/chart.js] + google_analytics,
+    style_src: %w['self' *.gov.uk fonts.googleapis.com] + google_analytics,
     worker_src: %w['self'],
-    # upgrade_insecure_requests: !Rails.env.development?, # see https://www.w3.org/TR/upgrade-insecure-requests/
-    report_uri: %w[/csp_reports],
+    report_uri: sentry_report_uri,
   }
 end
 # rubocop:enable Lint/PercentStringArray
