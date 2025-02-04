e2c5bf66fc93f85e8b00299d1a35c73b4ef5fb57
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-02-04 11:09:16 +0000

    make task work for Feb-25 statement

--- spec/lib/tasks/one_off/create_contract_spec.rb
+++ spec/lib/tasks/one_off/create_contract_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "one_off:create_contract" do
   let(:lead_provider_name) { "LLSE" }
 
   before do
-    create(:statement, year: 2025, month: 1, cohort: create(:cohort, start_year: 2023), lead_provider: LeadProvider.find_by(name: lead_provider_name))
+    create(:statement, year: 2025, month: 2, cohort: create(:cohort, start_year: 2023), lead_provider: LeadProvider.find_by(name: lead_provider_name))
     csv_file.write("provider_name,cohort_year,course_identifier,recruitment_target,service_fee_installments,service_fee_percentage,per_participant,number_of_payment_periods,output_payment_percentage,monthly_service_fee,targeted_delivery_funding_per_participant,special_course\n")
     csv_file.write("#{lead_provider_name},2023,npq-early-headship-coaching-offer,7,0,1,800,4,100,10,90,TRUE\n")
     csv_file.rewind
@@ -43,7 +43,7 @@ RSpec.describe "one_off:create_contract" do
       expect { run_task }.to change(Contract, :all).from([]).to a_collection_containing_exactly(
         an_object_having_attributes(
           course: Course.find_by(identifier: "npq-early-headship-coaching-offer"),
-          statement: Statement.find_by(year: 2025, month: 1),
+          statement: Statement.find_by(year: 2025, month: 2),
         ),
       )
     end
@@ -66,7 +66,7 @@ RSpec.describe "one_off:create_contract" do
 
     context "when the contract has already been created" do
       before do
-        create(:contract, course: Course.find_by(identifier: "npq-early-headship-coaching-offer"), statement: Statement.find_by(year: 2025, month: 1))
+        create(:contract, course: Course.find_by(identifier: "npq-early-headship-coaching-offer"), statement: Statement.find_by(year: 2025, month: 2))
       end
 
       it "exits with error code 1" do
