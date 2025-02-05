499977a3a6b9c1143d92db7d2f3dd48a0aa6de7d
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-02-05 16:26:27 +0000

    Enable bullet in test+review and fix N+1 (#2184)

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -3,4 +3,11 @@ require Rails.root.join("config/environments/production")
 Rails.application.configure do
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
   config.cache_store = :memory_store
+
+  config.after_initialize do
+    Bullet.enable                       = true
+    Bullet.bullet_logger                = true
+    Bullet.raise                        = true # Raise an error if n+1 query occurs
+    Bullet.unused_eager_loading_enable  = false # Disabled due to the way our queries are structured
+  end
 end
