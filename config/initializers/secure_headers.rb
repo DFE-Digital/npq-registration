e18061916a05542567c660c69ee6cdc884249749
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-02-06 14:55:13 +0000

    [CPDNPQ-2485] content security policy updates (#2181)
    * make content security policy more secure
    remove csp_reports route and controller
    use nonced_javascript_tag instead of script tags
    
    * add flippercloud to CSP
    
    * add identiy domain to form-action
    
    * change env var to SENTRY_CSP_REPORT_URI

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# need to disable Lint/PercentStringArray here, because CSP keywords need to be single-quoted
 # rubocop:disable Lint/PercentStringArray
 SecureHeaders::Configuration.default do |config|
   config.x_frame_options = "DENY"
@@ -9,8 +10,21 @@ SecureHeaders::Configuration.default do |config|
   config.x_permitted_cross_domain_policies = "none"
   config.referrer_policy = %w[origin-when-cross-origin strict-origin-when-cross-origin]
 
-  google_analytics = %w[www.google-analytics.com ssl.google-analytics.com *.googletagmanager.com tagmanager.google.com *.googleusercontent.com *.gstatic.com]
+  google_analytics = %w[*.google-analytics.com *.analytics.google.com *.googletagmanager.com tagmanager.google.com *.googleusercontent.com *.gstatic.com]
   tracking_pixels = %w[www.facebook.com px.ads.linkedin.com]
+  flippercloud = %w[www.flippercloud.io]
+  identity_domain = [ENV["TRA_OIDC_DOMAIN"]]
+  sentry = []
+
+  if ENV["SENTRY_CSP_REPORT_URI"]
+    sentry_report_uri = ENV["SENTRY_CSP_REPORT_URI"]
+    sentry = [URI(sentry_report_uri).host]
+  end
+
+  flipper_ui_hashes = %w[
+    'sha256-NNzAJMHPK9KuPslppuoTz2azqZcpUO0IJZosehbmhHA='
+    'sha256-zuOhDbTpAZjaeemuptCNLaf/7IaV06c8De4EMGOhtzM='
+  ]
 
   config.csp = SecureHeaders::OPT_OUT
 
@@ -19,19 +33,18 @@ SecureHeaders::Configuration.default do |config|
     base_uri: %w['self'],
     upgrade_insecure_requests: true,
     child_src: %w['self'],
-    connect_src: %W['self'] + google_analytics,
+    connect_src: %w['self'] + google_analytics + flippercloud + sentry,
     font_src: %w['self' *.gov.uk fonts.gstatic.com],
-    form_action: %w['self'],
+    form_action: %w['self'] + identity_domain, # needed because the POST to /users/auth/tra_openid_connect' redirects to the identity domain
     frame_ancestors: %w['self'],
     frame_src: %w['self'] + google_analytics,
-    img_src: %W['self' data: *.gov.uk] + google_analytics + tracking_pixels,
+    img_src: %w['self' data: *.gov.uk] + google_analytics + tracking_pixels,
     manifest_src: %w['self'],
     media_src: %w['self'],
-    script_src: %W['self' 'unsafe-inline' 'unsafe-eval' *.gov.uk https://cdn.jsdelivr.net/npm/chart.js] + google_analytics,
-    style_src: %w['self' 'unsafe-inline' *.gov.uk fonts.googleapis.com] + google_analytics,
+    script_src: %w['self' *.gov.uk https://cdn.jsdelivr.net/npm/chart.js] + google_analytics,
+    style_src: %w['self' *.gov.uk fonts.googleapis.com] + google_analytics + %w['unsafe-hashes'] + flipper_ui_hashes,
     worker_src: %w['self'],
-    # upgrade_insecure_requests: !Rails.env.development?, # see https://www.w3.org/TR/upgrade-insecure-requests/
-    report_uri: %w[/csp_reports],
+    report_uri: [sentry_report_uri],
   }
 end
 # rubocop:enable Lint/PercentStringArray
