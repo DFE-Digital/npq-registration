a24965e9527e51f2ffe99153373654c179d71773
jebw <jeb@jdwilkins.co.uk>
2025-02-21 11:08:27 +0000

    [CPDNPQ-2650] Convert to omniauth_openid_connect

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "omniauth/strategies/tra_openid_connect"
-
 Devise.setup do |config|
   require "devise/orm/active_record"
 
@@ -9,16 +7,24 @@ Devise.setup do |config|
     config.secret_key = "devisebequiet1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
   end
 
-  # ==> OmniAuth
-  tra_oidc_secret  = ENV.fetch("TRA_OIDC_CLIENT_SECRET", nil)
-  tra_oidc_id      = ENV.fetch("TRA_OIDC_CLIENT_ID", nil)
-
-  config.omniauth :tra_openid_connect,
-                  tra_oidc_id,
-                  tra_oidc_secret,
+  config.omniauth :openid_connect,
                   name: :tra_openid_connect,
+                  allow_authorize_params: %i[prompt],
+                  callback_path: "/users/auth/tra_openid_connect/callback",
                   client_options: {
-                    identifier: tra_oidc_id,
-                    secret: tra_oidc_secret,
-                  }
+                    host: URI(ENV["TRA_OIDC_DOMAIN"]).host,
+                    identifier: ENV.fetch("TRA_OIDC_CLIENT_ID"),
+                    port: 443,
+                    redirect_uri: ENV.fetch("TRA_OIDC_REDIRECT_URI"),
+                    scheme: "https",
+                    secret: ENV.fetch("TRA_OIDC_CLIENT_SECRET"),
+                  },
+                  discovery: true,
+                  issuer: ENV.fetch("TRA_OIDC_DOMAIN"),
+                  path_prefix: "/users/auth",
+                  pkce: true,
+                  # post_logout_redirect_uri: # TODO
+                  #   "#{ENV["HOSTING_DOMAIN"]}/qualifications/sign-out",
+                  response_type: :code,
+                  scope: %w[email openid profile trn]
 end
