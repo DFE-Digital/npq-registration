10aa12ebbf3e3fc97a1ade34f3764596da75fac4
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-02-25 16:08:52 +0000

    [CPDNPQ-2546] Separate out the wizard session steps (#2180)

--- spec/forms/session_wizard_steps/sign_in_spec.rb
+++ spec/forms/session_wizard_steps/sign_in_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Questionnaires::SignIn, type: :model do
+RSpec.describe SessionWizardSteps::SignIn, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:email) }
   end
