20eaf7ab8034e762258c6cf68f1d122f6deaeb99
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-02-26 11:46:46 +0000

    Revert "CPDNPQ-2650 Fix nil trn issue (#2245)"
    This reverts commit 3026a25b35bf3576538ccd66e87f39c491acaaf1.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,20 +9,16 @@ Devise.setup do |config|
     config.secret_key = "devisebequiet1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
   end
 
-  oidc_domain = ENV["TRA_OIDC_DOMAIN"].presence
+  # ==> OmniAuth
+  tra_oidc_secret  = ENV.fetch("TRA_OIDC_CLIENT_SECRET", nil)
+  tra_oidc_id      = ENV.fetch("TRA_OIDC_CLIENT_ID", nil)
 
   config.omniauth :tra_openid_connect,
-                  allow_authorize_params: %i[request_email_updates],
-                  callback_path: "/users/auth/tra_openid_connect/callback",
+                  tra_oidc_id,
+                  tra_oidc_secret,
+                  name: :tra_openid_connect,
                   client_options: {
-                    host: oidc_domain ? URI(oidc_domain).host : nil,
-                    identifier: ENV.fetch("TRA_OIDC_CLIENT_ID", nil),
-                    redirect_uri:
-                      "#{ENV.fetch("HOSTING_DOMAIN", nil)}/users/auth/tra_openid_connect/callback",
-                    secret: ENV.fetch("TRA_OIDC_CLIENT_SECRET", nil),
-                  },
-                  issuer: oidc_domain,
-                  post_logout_redirect_uri:
-                    "#{ENV.fetch("HOSTING_DOMAIN", nil)}/sign-out",
-                  strategy_class: Omniauth::Strategies::TraOpenidConnect
+                    identifier: tra_oidc_id,
+                    secret: tra_oidc_secret,
+                  }
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe SessionsController do
   include Helpers::JourneyHelper
 
   it "redirects to the external OIDC provider's signout endpoint" do
+    stub_env_variables_for_gai
+
     expect(controller).to receive(:sign_out_all_scopes)
 
     root_path = "https://test.host/"
