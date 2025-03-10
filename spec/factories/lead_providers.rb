d5137c74bd2dd476ee08c2a26b706a8e307355ba
jebw <jeb@jdwilkins.co.uk>
2025-03-10 12:35:06 +0000

    [CPDNPQ-2448] Query delivery_partners for cohort

--- spec/factories/lead_providers.rb
+++ spec/factories/lead_providers.rb
@@ -1,7 +1,28 @@
 FactoryBot.define do
   factory :lead_provider do
+    transient do
+      delivery_partner { nil }
+      delivery_partners { Array.wrap(delivery_partner) }
+    end
+
     name { Faker::Company.unique.name }
     ecf_id { SecureRandom.uuid }
     hint { Faker::Lorem.sentence }
+
+    after :create do |lead_provider, evaluator|
+      partnerships = if evaluator.delivery_partners.is_a?(Hash)
+                       evaluator.delivery_partners
+                     elsif evaluator.delivery_partners&.any?
+                       { Cohort.current => evaluator.delivery_partners }
+                     else
+                       {}
+                     end
+
+      partnerships.each do |cohort, delivery_partners|
+        Array.wrap(delivery_partners).each do |delivery_partner|
+          lead_provider.delivery_partnerships.create! delivery_partner:, cohort:
+        end
+      end
+    end
   end
 end
