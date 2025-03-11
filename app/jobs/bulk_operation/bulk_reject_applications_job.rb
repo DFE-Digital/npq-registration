a8de6a9b3a006988d8c7eaa7592a5d730fc887ae
Richard Worrall <richard@soupish.net>
2025-03-11 14:19:49 +0000

    Update bulk operations to handle BOM CSVs

--- app/jobs/bulk_operation/bulk_reject_applications_job.rb
+++ app/jobs/bulk_operation/bulk_reject_applications_job.rb
@@ -3,7 +3,7 @@
 class BulkOperation::BulkRejectApplicationsJob < ApplicationJob
   def perform(bulk_operation_id:)
     bulk_operation = BulkOperation::RejectApplications.find(bulk_operation_id)
-    application_ecf_ids = CSV.parse(bulk_operation.file.download, headers: false).flatten
+    application_ecf_ids = bulk_operation.file.open { CSV.read(_1, headers: false).flatten }
     Rails.logger.info("Bulk Operation started - bulk_operation_id: #{bulk_operation_id}")
     BulkOperation::BulkRejectApplications.new(application_ecf_ids:, bulk_operation:).run!
     Rails.logger.info("Bulk Operation finished - bulk_operation_id: #{bulk_operation_id}")
