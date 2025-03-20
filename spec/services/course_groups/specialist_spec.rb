61d3a43ff598a048eb0fca086e9e12b6c34f7f0c
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-03-20 16:46:12 +0000

    choose correct schedule for 2024 and 2025 cohorts

--- spec/services/course_groups/specialist_spec.rb
+++ spec/services/course_groups/specialist_spec.rb
@@ -3,66 +3,14 @@ require "rails_helper"
 RSpec.describe CourseGroups::Specialist do
   let(:cohort) { create(:cohort, :current) }
   let(:schedule_date) { Date.current }
-  let(:course_group) { CourseGroup.find_by(name: "specialist") || create(:course_group, name: "specialist") }
+  let(:course_group) { CourseGroup.find_by(name: "specialist") }
+  let(:course_group_name) { "specialist" }
+  let(:spring_schedule_identifier) { "npq_specialist_spring" }
+  let(:autumn_schedule_identifier) { "npq_specialist_autumn" }
 
   subject { described_class.new(course_group:, cohort:, schedule_date:) }
 
-  describe "#schedule" do
-    let!(:autumn_schedule) { create(:schedule, :npq_specialist_autumn, course_group:, cohort:) }
-    let!(:spring_schedule) { create(:schedule, :npq_specialist_spring, course_group:, cohort:) }
-
-    subject(:schedule) { described_class.new(course_group:, cohort:, schedule_date:).schedule }
-
-    context "when date is between June and December of cohort start year" do
-      before { travel_to Date.new(cohort.start_year, 6, 1) }
-
-      it { is_expected.to eq(autumn_schedule) }
-    end
-
-    context "when date is between December of cohort start year and April of the following year" do
-      let(:cohort) { create(:cohort, start_year: 2025) }
-
-      before { travel_to Date.new(cohort.start_year, 12, 26) }
-
-      it { is_expected.to eq(spring_schedule) }
-    end
-
-    context "when date is between April and December of the next year" do
-      before { travel_to Date.new(cohort.start_year + 1, 4, 3) }
-
-      it { is_expected.to eq(autumn_schedule) }
-    end
-
-    context "when date is between December of next year and April in 2 years" do
-      before { travel_to Date.new(cohort.start_year + 1, 12, 26) }
-
-      it { is_expected.to eq(spring_schedule) }
-    end
-
-    context "when date is spring 2025" do
-      let(:cohort) { create(:cohort, start_year: 2024) }
-
-      before { travel_to Date.new(2025, 1, 13) }
-
-      it { is_expected.to eq(autumn_schedule) }
-    end
-  end
-
-  describe "#autumn_schedule_2022?" do
-    it "returns true when date between 1st Jun 2022 and 25th Dec 2022" do
-      (("2022-06-1".to_date)..("2022-12-25".to_date)).each do |date|
-        expect(subject.autumn_schedule_2022?(date)).to be(true)
-      end
-    end
-
-    it "returns false when date between 26th Dec and 31st May" do
-      (2022..Date.current.year).each do |year|
-        (("#{year}-12-26".to_date)..("#{year + 1}-05-31".to_date)).each do |date|
-          expect(subject.autumn_schedule_2022?(date)).to be(false)
-        end
-      end
-    end
-  end
+  it_behaves_like "leadership and specialist #schedule"
 
   describe "#autumn_schedule_2024?" do
     it "returns true when date between 28th Jun 2024 and 6th Jun 2025" do
