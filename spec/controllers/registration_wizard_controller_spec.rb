cd15892a307302facd50ff669945c0d266949e5c
jebw <jeb@jdwilkins.co.uk>
2025-03-26 17:35:25 +0000

    [CPDNPQ-2725] Bounce the user to the start if they are on the wrong page

--- spec/controllers/registration_wizard_controller_spec.rb
+++ spec/controllers/registration_wizard_controller_spec.rb
@@ -1,11 +1,21 @@
 require "rails_helper"
 
 RSpec.describe RegistrationWizardController do
+  subject { response }
+
+  before { session[:user_id] = create(:user).id }
+
+  describe "#show" do
+    before { get :show, params: { step: "course-start-date" } }
+
+    it { is_expected.to have_http_status :success }
+  end
+
   describe "#update" do
     context "when agreeing to terms" do
       it "persists data to session" do
-        patch :update, params: { step: "share-provider", registration_wizard: { can_share_choices: "1" } }
-        expect(session["registration_store"]["can_share_choices"]).to eql("1")
+        patch :update, params: { step: "course-start-date", registration_wizard: { course_start_date: "yes" } }
+        expect(session["registration_store"]["course_start_date"]).to eql("yes")
       end
     end
   end
