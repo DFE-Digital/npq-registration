792c6c90e86b0ae7778a3e1e35833a7bef5d5249
Richard Worrall <richard@soupish.net>
2025-03-27 11:30:39 +0000

    [CPDNPQ-2450] Copy delivery partners

---
+++ app/services/cohorts/copy_delivery_partners.rb
@@ -0,0 +1,32 @@
+module Cohorts
+  class CopyDeliveryPartners
+    def initialize(cohort)
+      @cohort = cohort
+    end
+
+    def copy
+      return if previous_cohort.nil?
+
+      previous_partnerships.find_each do |partnership|
+        @cohort.delivery_partnerships.create!(
+          lead_provider_id: partnership.lead_provider_id,
+          delivery_partner_id: partnership.delivery_partner_id,
+        )
+      end
+    end
+
+  private
+
+    def previous_cohort
+      @previous_cohort ||= Cohort.where("start_year < ?", @cohort.start_year)
+                                 .order(start_year: :desc)
+                                 .first
+    end
+
+    def previous_partnerships
+      return DeliveryPartnership.none if previous_cohort.nil?
+
+      DeliveryPartnership.where(cohort_id: previous_cohort.id)
+    end
+  end
+end
