5828524544225647b33e307e1846036a02f3132a
jebw <jeb@jdwilkins.co.uk>
2025-04-01 11:26:29 +0100

    [BAU] Fix broken reg wizard visualiser + add specs

---
+++ spec/services/registration_wizard_visualiser_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe RegistrationWizardVisualiser do
+  before { FileUtils.rm_rf Rails.root.join("tmp/visualisations_test") }
+
+  let :dot_file do
+    Rails.root.join("tmp/visualisations_test/registration_wizard_visualisation.dot")
+  end
+
+  let :png_file do
+    Rails.root.join("tmp/visualisations_test/registration_wizard_visualisation.dot")
+  end
+
+  it "generates an dot graph of the wizard" do
+    expect { described_class.call }
+      .to change(dot_file, :exist?).from(false).to(true)
+  end
+
+  it "generates a png graph of the wizard" do
+    expect { described_class.call }
+      .to change(png_file, :exist?).from(false).to(true)
+  end
+end
