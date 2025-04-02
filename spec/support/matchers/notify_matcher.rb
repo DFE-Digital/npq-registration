416cb67e895cd54cea331a8db8975b3d984af688
jebw <jeb@jdwilkins.co.uk>
2025-04-02 12:20:05 +0100

    [BAU] Convert to new mail-notify api

--- spec/support/matchers/notify_matcher.rb
+++ spec/support/matchers/notify_matcher.rb
@@ -1,11 +1,11 @@
 RSpec::Matchers.define :use_template do |expected_template|
   match do |mail|
-    mail["template-id"].unparsed_value == expected_template
+    mail.template_id == expected_template
   end
 end
 
 RSpec::Matchers.define :have_personalisation do |expected_personalisation|
   match do |mail|
-    mail["personalisation"].unparsed_value == expected_personalisation
+    mail.personalisation == expected_personalisation
   end
 end
