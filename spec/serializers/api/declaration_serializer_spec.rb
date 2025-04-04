ac27640895f09f877a3e022bf7584c406e9f9c1f
Emma Frith <56349171+EmmaFrith@users.noreply.github.com>
2025-04-04 10:31:06 +0100

    Update spec/serializers/api/declaration_serializer_spec.rb

--- spec/serializers/api/declaration_serializer_spec.rb
+++ spec/serializers/api/declaration_serializer_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe API::DeclarationSerializer, type: :serializer do
-  let(:declaration) do
-    create(:declaration, application:,
+    let(:declaration) do
+    create(:declaration,
+           application:,
            delivery_partner: primary_partner,
            secondary_delivery_partner: secondary_partner)
   end
