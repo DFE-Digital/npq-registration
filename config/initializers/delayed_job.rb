77fe74339497cdf228b7fd10f56b94969f974158
jebw <jeb@jdwilkins.co.uk>
2025-04-15 09:54:21 +0100

    wip

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -18,7 +18,7 @@ Delayed::Worker.max_run_time = 30.minutes
 # lower frequency
 Delayed::Worker.max_attempts = 8
 
-Delayed::Worker.sleep_delay = 30
+# Delayed::Worker.sleep_delay = 30
 
 # Override backoff rate for retries
 ActiveJob::QueueAdapters::DelayedJobAdapter::JobWrapper.class_eval do
