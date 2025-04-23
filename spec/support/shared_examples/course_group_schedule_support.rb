d503cf7c3cfdff51e921c1c06d8501b259f2c23e
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-04-23 09:43:54 +0100

    [CPDNPQ-2800] cope with late acceptance from 2023 and earlier cohorts (#2400)
    * [CPDNPQ-2800] cope with late acceptance from 2023 and earlier cohorts
    
    * add validation to ensure schedule cannot be nil

--- spec/support/shared_examples/course_group_schedule_support.rb
+++ spec/support/shared_examples/course_group_schedule_support.rb
@@ -5,30 +5,24 @@ require "rails_helper"
 RSpec.shared_examples "leadership and specialist #schedule" do
   let(:course_group) { CourseGroup.find_by(name: course_group_name) }
 
-  subject { described_class.new(course_group:, cohort:, schedule_date: date).schedule }
+  subject { described_class.new(course_group:, cohort:, schedule_date: Date.current).schedule }
 
   before { travel_to(date) }
+
+  let(:date) { Date.new(2025, 6, 1) }
 
   context "when the course is only in the spring schedule (like the 2025 cohort)" do
     let!(:spring_schedule) { create(:schedule, spring_schedule_identifier, course_group:, cohort:) }
     let(:cohort) { create(:cohort, start_year: 2025) }
 
-    context "when date is between 28th June 2024 and 6th June 2025" do
-      let(:date) { Date.new(2025, 6, 1) }
-
-      it { is_expected.to eq(spring_schedule) }
-    end
+    it { is_expected.to eq(spring_schedule) }
   end
 
   context "when the course is only in the autumn schedule (like the 2024 cohort)" do
     let!(:autumn_schedule) { create(:schedule, autumn_schedule_identifier, course_group:, cohort:) }
     let(:cohort) { create(:cohort, start_year: 2024) }
 
-    context "when date is between 28th June 2024 and 6th June 2025" do
-      let(:date) { Date.new(2025, 6, 1) }
-
-      it { is_expected.to eq(autumn_schedule) }
-    end
+    it { is_expected.to eq(autumn_schedule) }
   end
 
   context "when course is in both autumn and spring schedules (like 2021-2023 cohorts)" do
@@ -37,19 +31,19 @@ RSpec.shared_examples "leadership and specialist #schedule" do
     let(:cohort) { create(:cohort, start_year: 2023) }
 
     context "when date is between 1st January and 2nd April" do
-      let(:date) { Date.new(2023, 4, 2) }
+      before { travel_to(Date.new(2025, 4, 2)) }
 
       it { is_expected.to eq(spring_schedule) }
     end
 
     context "when date is between 26th December and 31st December" do
-      let(:date) { Date.new(2023, 12, 31) }
+      before { travel_to(Date.new(2025, 12, 31)) }
 
       it { is_expected.to eq(spring_schedule) }
     end
 
     context "when date is between 3rd April and 25th December" do
-      let(:date) { Date.new(2023, 12, 25) }
+      before { travel_to(Date.new(2025, 12, 25)) }
 
       it { is_expected.to eq(autumn_schedule) }
     end
