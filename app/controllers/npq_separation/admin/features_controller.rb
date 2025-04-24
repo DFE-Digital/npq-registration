6b7c2071ff7c1b6f9440a0bda2c9c55e90417c5a
Emma Frith <emma.frith@education.gov.uk>
2025-04-24 12:40:47 +0100

    Update the features controller to reflect new namespace and new paths

---
+++ app/controllers/npq_separation/admin/features_controller.rb
@@ -0,0 +1,28 @@
+class NpqSeparation::Admin::FeaturesController < SuperAdminController
+  def index
+    @features = Feature::FEATURE_FLAG_KEYS
+  end
+
+  def show
+    @feature = params[:id]
+    unless Feature::FEATURE_FLAG_KEYS.include?(@feature)
+      redirect_back fallback_location: npq_separation_admin_features_path
+    end
+  end
+
+  def update
+    @feature = params[:id]
+    if params[:feature_flag_name] == @feature
+      if Flipper.enabled?(@feature)
+        Flipper.disable(@feature)
+        flash[:success] = "You have turned the #{@feature} feature flag off."
+      else
+        Flipper.enable(@feature)
+        flash[:success] = "You have turned the #{@feature} feature flag on."
+      end
+    else
+      flash[:error] = "There was an error updating the feature flag."
+    end
+    redirect_to npq_separation_admin_feature_path(@feature)
+  end
+end
