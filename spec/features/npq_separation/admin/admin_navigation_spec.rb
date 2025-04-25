7057d15c7d2b2f310414c5141febfb2bc3541812
Emma Frith <emma.frith@education.gov.uk>
2025-04-25 13:46:39 +0100

    start writing test for regular admins to check thaat they can't see the feature flags link - it will fail as the functionality isn't there yet

--- spec/features/npq_separation/admin/admin_navigation_spec.rb
+++ spec/features/npq_separation/admin/admin_navigation_spec.rb
@@ -3,9 +3,7 @@ require "rails_helper"
 RSpec.feature "admin", :rack_test_driver, type: :feature do
   include Helpers::AdminLogin
   include_context "Stub Get An Identity Omniauth Responses"
-
   let(:admin) { create(:admin) }
-
   scenario "regular admins cannot see links that super admins can" do
     create(:cohort, :current)
     sign_in_as_admin
