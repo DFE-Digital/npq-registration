a90196eab9a3b4f5028bcf02a330110dd6b523ca
Emma Frith <emma.frith@education.gov.uk>
2025-05-06 16:48:55 +0100

    Only show feature flags link when user is a super admin

--- app/components/npq_separation/navigation_structures/admin_navigation_structure.rb
+++ app/components/npq_separation/navigation_structures/admin_navigation_structure.rb
@@ -4,42 +4,46 @@ module NpqSeparation
       include Rails.application.routes.url_helpers
       include AdminHelper
 
+      def initialize(current_admin)
+        @current_admin = current_admin
+      end
+
     private
 
       # Returns a hash where the keys are primary nodes and the values are
       # sub nodes nested with the 'nodes: key'
       def structure
-        {
+        nav = {
           Node.new(
             name: "Dashboard",
             href: npq_separation_admin_path,
             prefix: "/npq-separation/admin/dashboard",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Applications",
             href: npq_separation_admin_applications_path,
             prefix: "/npq-separation/admin/applications",
-          ) => application_nodes,
+            ) => application_nodes,
           Node.new(
             name: "Cohorts",
             href: npq_separation_admin_cohorts_path,
             prefix: "/npq-separation/admin/cohorts",
-          ) => cohort_nodes,
+            ) => cohort_nodes,
           Node.new(
             name: "Courses",
             href: npq_separation_admin_courses_path,
             prefix: "/npq-separation/admin/courses",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Participants",
             href: npq_separation_admin_users_path,
             prefix: "/npq-separation/admin/users",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Finance",
             href: npq_separation_admin_finance_statements_path,
             prefix: "/npq-separation/admin/finance",
-          ) => [
+            ) => [
             Node.new(
               name: "Statements",
               href: npq_separation_admin_finance_statements_path,
@@ -48,7 +52,7 @@ module NpqSeparation
                 Node.new(name: "Unpaid statements", href: npq_separation_admin_finance_unpaid_index_path, prefix: "/npq-separation/admin/finance/statements/unpaid"),
                 Node.new(name: "Paid statements", href: npq_separation_admin_finance_paid_index_path, prefix: "/npq-separation/admin/finance/statements/paid"),
               ],
-            ),
+              ),
             Node.new(name: "Declarations", href: "#", prefix: "/npq-separation/admin/finance/declarations"),
             Node.new(name: "Contracts", href: "#", prefix: "/npq-separation/admin/finance/contracts"),
           ],
@@ -56,34 +60,40 @@ module NpqSeparation
             name: "Schools",
             href: npq_separation_admin_schools_path,
             prefix: "/npq-separation/admin/schools",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Lead providers",
             href: npq_separation_admin_lead_providers_path,
             prefix: "/npq-separation/admin/lead_providers",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Bulk operations",
             href: npq_separation_admin_bulk_operations_path,
             prefix: "/npq-separation/admin/bulk_operations",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Delivery partners",
             href: npq_separation_admin_delivery_partners_path,
             prefix: "/npq-separation/admin/delivery-partners",
-          ) => [],
-          Node.new(
-            name: "Feature flags",
-            href: npq_separation_admin_features_path,
-            prefix: "/npq-separation/admin/features",
-          ) => [],
+            ) => [],
           Node.new(
             name: "Settings",
             href: "#",
             prefix: "/npq-separation/admin/settings",
-          ) => [],
+            ) => [],
         }
+
+        if @current_admin&.super_admin?
+          nav[Node.new(
+            name: "Feature flags",
+            href: npq_separation_admin_features_path,
+            prefix: "/npq-separation/admin/features",
+            )] = []
+        end
+
+        nav
       end
+
 
       def application_nodes
         [
