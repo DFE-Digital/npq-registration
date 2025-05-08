7a03b36f23a877992ae7c13173eee97cdcdf448a
Emma Frith <emma.frith@education.gov.uk>
2025-05-08 16:49:34 +0100

    Tidy up specs: consolidate admin_navigation_spec and super_admin_navigation_spec into one file; create a separate spec file for feature flags functionality; remove 'visit' action as we already have click_lick action

--- spec/features/npq_separation/admin/admin_navigation_spec.rb
+++ spec/features/npq_separation/admin/admin_navigation_spec.rb
@@ -3,13 +3,23 @@ require "rails_helper"
 RSpec.feature "admin", :rack_test_driver, type: :feature do
   include Helpers::AdminLogin
   include_context "Stub Get An Identity Omniauth Responses"
+
   let(:admin) { create(:admin) }
+  let(:super_admin) { create(:super_admin) }
+
+  before do
+    create(:cohort, :current)
+  end
 
   scenario "regular admins cannot see links that super admins can" do
-    create(:cohort, :current)
     sign_in_as_admin
-    visit "/npq-separation/admin"
     click_link("Separation Admin")
-    expect(page).not_to have_link("Feature flags", href: "/npq-separation/admin/features")
+    expect(page).not_to have_link("Feature flags")
+  end
+
+  scenario "super admins can see links that regular admins can't" do
+    sign_in_as_super_admin
+    click_link("Separation Admin")
+    expect(page).to have_link("Feature flags", href: "/npq-separation/admin/features")
   end
 end
