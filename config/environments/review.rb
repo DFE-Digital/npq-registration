ff5dab72f985fa52a743b6fe1731d8cf5119e847
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-05-13 08:23:26 +0100

    [CPDNPQ-2713] Swap back to memory store for now for non-prod environments (#2476)
    When the new redis pops up its non functional leaving the app unable to
    boot

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -2,6 +2,7 @@ require Rails.root.join("config/environments/production")
 
 Rails.application.configure do
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
+  config.cache_store = :memory_store
 
   config.after_initialize do
     Bullet.enable                       = true
