8f2c413cde9d5d7cc03769d61d400ed36df3c57a
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-05-13 11:55:54 +0100

    [CPDNPQ-2713] Use redis cache store for review and staging environments (#2479)
    This reverts commit d5ed182eaa60b2ca1a013920639f72f10cfda321.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,5 +3,4 @@ require Rails.root.join("config/environments/production")
 Rails.application.configure do
   config.log_level = :debug
   config.ssl_options = { redirect: { exclude: ->(request) { request.path.include?("/healthcheck") } } }
-  config.cache_store = :memory_store
 end
