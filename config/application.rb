1e6bb4d7838805f279663a70712d2bcd934c749f
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-05-14 09:48:02 +0100

    Revert "[BAU] Use rails 7.1 defaults (#2481)"
    This reverts commit 22cd67f4dd4e077480bd824ea0f711af631b36c4.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ Bundler.require(*Rails.groups)
 module NpqRegistration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
     config.exceptions_app = routes
 
