64346b2fe076dd337396a038388a35084937bbee
Emma Frith <emma.frith@education.gov.uk>
2025-05-16 11:31:06 +0100

    Move the super admins controller to the separation admin folder

--- app/controllers/npq_separation/admin/super_admins_controller.rb
+++ app/controllers/npq_separation/admin/super_admins_controller.rb
@@ -1,5 +1,5 @@
-class Admin::SuperAdminsController < SuperAdminController
-  def update
+class NpqSeparation::Admin::SuperAdminsController < SuperAdminController
+def update
     @admin = Admin.find(params[:id])
 
     if @admin.update(super_admin: true)
@@ -8,6 +8,6 @@ class Admin::SuperAdminsController < SuperAdminController
       flash[:error] = t(".failure", email: @admin.email)
     end
 
-    redirect_back(fallback_location: admin_admins_path)
+    redirect_back(fallback_location: npq_separation_admin_admins_path)
   end
 end
