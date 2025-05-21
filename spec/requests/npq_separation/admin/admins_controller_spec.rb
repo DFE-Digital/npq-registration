4164ffdab5a31150ec2e2d366b6605d0562a73a8
Emma Frith <emma.frith@education.gov.uk>
2025-05-21 16:23:35 +0100

    Add request spec to check that only super admins can access the admins index page

--- spec/requests/npq_separation/admin/admins_controller_spec.rb
+++ spec/requests/npq_separation/admin/admins_controller_spec.rb
@@ -1,9 +1,32 @@
-# require "rails_helper"
-#
-# RSpec.describe NpqSeparation::Admin::AdminsController, type: :request do
-#   describe("index") do
-#     before { get(npq_separation_admin_admins_path) }
-#
-#     specify { expect(response).to(be_method_not_allowed) }
-#   end
-# end
+require "rails_helper"
+
+RSpec.describe NpqSeparation::Admin::AdminsController, type: :request do
+  include Helpers::NPQSeparationAdminLogin
+
+  describe("GET /npq_separation/admin/admins") do
+    subject { get(npq_separation_admin_admins_path) && response }
+
+    context "when signed in as a super admin" do
+      before { sign_in_as_admin(super_admin: true) }
+
+      it "returns a 200 OK response" do
+        expect(subject).to(have_http_status(:ok))
+      end
+    end
+
+    context "when signed in as an admin" do
+      before { sign_in_as_admin }
+
+      it "returns a 302 unauthorised response" do
+        expect(subject).to(have_http_status(:redirect))
+      end
+    end
+
+    context "when not signed in" do
+      it "returns a 302 unauthorised response" do
+        expect(subject).to(have_http_status(:redirect))
+      end
+    end
+
+  end
+end
