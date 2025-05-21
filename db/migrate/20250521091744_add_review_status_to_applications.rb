5148a4633053153ad04b3dd06fb9156503415809
jebw <jeb@jdwilkins.co.uk>
2025-05-21 11:04:52 +0100

    [CPDNPQ-2566] Set review_status for existing 'in review' applications

--- db/migrate/20250521091744_add_review_status_to_applications.rb
+++ db/migrate/20250521091744_add_review_status_to_applications.rb
@@ -5,6 +5,21 @@ class AddReviewStatusToApplications < ActiveRecord::Migration[7.1]
 
     add_column :applications, :review_status, :enum, enum_type: "review_statuses",
                                                      null: true
+
+    safety_assured do
+      execute <<~SQL
+        UPDATE applications
+        SET review_status='decision_made'
+        WHERE
+          referred_by_return_to_teaching_adviser='yes'
+          OR
+          "employment_type" IN (
+            'hospital_school', 'lead_mentor_for_accredited_itt_provider',
+            'local_authority_supply_teacher', 'local_authority_virtual_school',
+            'young_offender_institution', 'other'
+          )
+      SQL
+    end
   end
 
   def down
