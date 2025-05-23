0ef63b5e0450a2052bbf9229b66997fa42d5262c
Emma Frith <56349171+EmmaFrith@users.noreply.github.com>
2025-05-23 14:44:08 +0100

    Move 'Admins' to new admin console (#2500)
    * Move routes to separation admin namespace, move views to separation admin folders, update paths, delete admins from old navbar and move it to new navbar
    
    * Move the admins controller to the separation admin folder, commenting out existing controller code which blocks access to the admins controller
    
    * Move the super admins controller to the separation admin folder
    
    * Update layout to match new layouts in the new admin console
    
    * Update path in admin_mamagement_helper
    
    * Move the super admin route to the NPQ separation namespace, update the controller so it inherits from NPQSeparation::AdminController, andn include the before_action so that it can only be accessed by super admins
    
    * Update paths in admin_management_helper.rb
    
    * Move content in yml file so that it's under npq_separation
    
    * Add previously missing summary text back in
    
    * Fixing linting issues
    
    * Add/adjust tests to verify that the navbar includes 'Admins' and 'Feature flags' for super admins
    
    * Update the admin_management_spec so it's using the correct links.
    
    * Delete commented out controller, and comment out the associated spec for now (will look into replacing this with some new tests)
    
    * Fix linting issue - empty space
    
    * Remove tests from admin_spec.rb which are already covered in admin_management_spec.rb
    
    * Add request spec to check that only super admins can access the admins index page
    
    * Update it statements in admins_controller_spec.rb
    
    * Specify where the redirects should go in the admins_controller_spec.rb.
    
    * Remove white space
    
    * Fix spacing and remove unneeded Pagy line

--- spec/requests/npq_separation/admin/admins_controller_spec.rb
+++ spec/requests/npq_separation/admin/admins_controller_spec.rb
@@ -1,9 +1,31 @@
 require "rails_helper"
 
 RSpec.describe NpqSeparation::Admin::AdminsController, type: :request do
-  describe("index") do
-    before { get(npq_separation_admin_admins_path) }
+  include Helpers::NPQSeparationAdminLogin
 
-    specify { expect(response).to(be_method_not_allowed) }
+  describe("GET /npq_separation/admin/admins") do
+    subject { get(npq_separation_admin_admins_path) && response }
+
+    context "when signed in as a super admin" do
+      before { sign_in_as_admin(super_admin: true) }
+
+      it "returns a 200 OK response" do
+        expect(subject).to(have_http_status(:ok))
+      end
+    end
+
+    context "when signed in as an admin" do
+      before { sign_in_as_admin }
+
+      it "redirects to the sign in page" do
+        expect(subject).to redirect_to(sign_in_path)
+      end
+    end
+
+    context "when not signed in" do
+      it "redirects to the sign in page" do
+        expect(subject).to redirect_to(sign_in_path)
+      end
+    end
   end
 end
