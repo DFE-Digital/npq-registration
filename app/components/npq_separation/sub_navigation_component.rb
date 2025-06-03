063440956ec3300109b8fc9f5e559fe76b4928e1
Richard Worrall <richard@soupish.net>
2025-06-03 10:17:23 +0100

    [CPDNPQ-2818] Admin side navigation (#2526)
    * Add heading support to SubNavigationComponent
    
    * Support inserting extra side navigation content
    
    * Add sub navigation heading specs
    
    * Support headings in static side navigation
    
    * Add side navigation to admin application views
    
    * Avoid multiple sub navigation ID conflict
    
    * Remove Applications heading after all
    
    * Revert to explicit heading kwarg after all

--- app/components/npq_separation/sub_navigation_component.rb
+++ app/components/npq_separation/sub_navigation_component.rb
@@ -1,10 +1,11 @@
 module NpqSeparation
   class SubNavigationComponent < ViewComponent::Base
-    attr_accessor :current_path, :current_section, :structure
+    attr_accessor :current_path, :current_section, :structure, :heading
 
-    def initialize(current_path, structure:)
+    def initialize(current_path, structure:, heading: {})
       @current_path = current_path
       @structure = structure
+      @heading = heading
     end
 
     def render?
@@ -25,6 +26,20 @@ module NpqSeparation
         "x-govuk-sub-navigation__section-item",
         "x-govuk-sub-navigation__section-item--current" => current?(section.prefix),
       )
+    end
+
+    def render_heading
+      heading_text = heading[:text].presence || "Navigation"
+      heading_class = class_names(
+        "x-govuk-sub-navigation__heading",
+        "govuk-visually-hidden" => !heading[:visible],
+      )
+
+      tag.h2(heading_text, class: heading_class, id: aria_key)
+    end
+
+    def aria_key
+      @aria_key ||= "sub-navigation-heading-#{SecureRandom.base58(4)}"
     end
 
   private
