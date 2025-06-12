926e0ffb27af02e4d8689c9516d742fb26175518
Richard Worrall <richard@soupish.net>
2025-06-12 11:15:16 +0100

    [CPDNPQ-2567] Change review status (#2584)

---
+++ app/components/npq_separation/admin/application_notes_input_component.rb
@@ -0,0 +1,28 @@
+module NpqSeparation
+  module Admin
+    class ApplicationNotesInputComponent < ViewComponent::Base
+      attr_reader :form
+
+      def initialize(form:)
+        @form = form
+      end
+
+      def call
+        form.govuk_text_area :notes,
+                             label: { text: label_text, size: "m" },
+                             hint: { text: "Begin your note with the date and your initials. (Notes are for internal use only.)" },
+                             max_chars: 1_000
+      end
+
+    private
+
+      def label_text
+        if form.object.notes.present?
+          "Edit the note about the changes to this registration"
+        else
+          "Add a note about the changes to this registration"
+        end
+      end
+    end
+  end
+end
