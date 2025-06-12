04e8416cb478cbc171dba06caeb9b033c43e1b80
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-06-12 09:32:21 +0100

    [CPDNPQ-2868] admin console consistent cohort date format

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,11 +1,4 @@
 module AdminHelper
-  def format_cohort(cohort)
-    start_year = cohort.start_year
-    end_year = start_year.next - 2000
-
-    "#{start_year}/#{end_year}"
-  end
-
   def format_cohort_full(cohort)
     "#{cohort.start_year} to #{cohort.start_year.next}"
   end
