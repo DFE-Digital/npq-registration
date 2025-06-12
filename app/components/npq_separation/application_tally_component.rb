1302a75724e4350eb6738a9128d863179d2a60b0
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-06-12 14:55:26 +0100

    [CPDNPQ-2903] course provider page and rename all instances of "Lead provider" (#2586)
    * [CPDNPQ-2903] course provider page and rename all instances of Lead provider
    
    * fix flaky spec

--- app/components/npq_separation/application_tally_component.rb
+++ app/components/npq_separation/application_tally_component.rb
@@ -1,10 +1,11 @@
 module NpqSeparation
   class ApplicationTallyComponent < ViewComponent::Base
-    attr_reader :applications, :dimension
+    attr_reader :applications, :dimension, :dimension_header
 
-    def initialize(applications, dimension)
+    def initialize(applications, dimension, dimension_header: nil)
       @applications = applications
       @dimension = dimension
+      @dimension_header = dimension_header || dimension.to_s.titleize
     end
 
     def rows
