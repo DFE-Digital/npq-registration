1a3b1bbfdec3319d28faa2c0ecce38f4db1e56e2
Richard Worrall <richard@soupish.net>
2025-06-17 10:35:19 +0100

    [CPDNPQ-2891] Link applications to workplaces (#2619)

--- spec/views/npq_separation/admin/applications/show.html.erb_spec.rb
+++ spec/views/npq_separation/admin/applications/show.html.erb_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe "npq_separation/admin/applications/show.html.erb", type: :view do
   subject { Capybara.string(render) }
 
+  let(:declarations) { [] }
+
   before do
     assign(:application, application)
     assign(:declarations, declarations)
@@ -20,6 +22,7 @@ RSpec.describe "npq_separation/admin/applications/show.html.erb", type: :view do
 
     it { is_expected.to have_css "h1", text: application.user.full_name }
     it { is_expected.to have_text "TRN: #{application.user.trn}", normalize_ws: true }
+    it { is_expected.to have_link(application.employer_name_to_display, href: npq_separation_admin_schools_path(q: application.school.urn)) }
     it { is_expected.to have_summary_item "Unique reference number (URN)", application.school.urn }
     it { is_expected.to have_summary_item "UK Provider Reference Number (UKPRN)", application.school.ukprn }
 
@@ -32,12 +35,19 @@ RSpec.describe "npq_separation/admin/applications/show.html.erb", type: :view do
     end
   end
 
+  describe "a row for an employer with no URN" do
+    let :application do
+      build_stubbed :application, school: nil, employer_name: "No URN"
+    end
+
+    it { is_expected.to have_text(application.employer_name_to_display) }
+    it { is_expected.not_to have_link(application.employer_name_to_display) }
+  end
+
   describe "a row for a minimal application" do
     let :application do
       build_stubbed :application, cohort: nil, itt_provider: nil, school: nil
     end
-
-    let(:declarations) { [] }
 
     it { is_expected.to have_css "h1", text: application.user.full_name }
     it { is_expected.to have_text "TRN: #{application.user.trn}", normalize_ws: true }
