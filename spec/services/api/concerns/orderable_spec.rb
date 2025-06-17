81544cdd8aa66bfc18be3ec4d15ff0ed9edcca47
jebw <jeb@jdwilkins.co.uk>
2025-06-17 12:01:06 +0100

    [CPDNPQ-2383] Fix orderable spec checking for table existance

--- spec/services/api/concerns/orderable_spec.rb
+++ spec/services/api/concerns/orderable_spec.rb
@@ -2,33 +2,36 @@
 
 require "rails_helper"
 
-class TestQuery
-  include API::Concerns::Orderable
-end
-
-class Test < ApplicationRecord
-  include ActiveModel::Attributes
-
-  attribute :foo
-  attribute :bar
-end
-
 RSpec.describe API::Concerns::Orderable do
-  let(:query) { TestQuery.new }
+  let :model do
+    Class.new(ApplicationRecord) do
+      include ActiveModel::Attributes
+
+      # As of Rails 7.2 the table is queried so this needs backing by a real table
+      self.table_name = "schema_migrations"
+
+      attribute :foo
+      attribute :bar
+    end
+  end
+
+  let :query do
+    Class.new { include API::Concerns::Orderable }.new
+  end
 
   describe "#sort_order" do
     it "returns a formatted sort order relative to the model" do
-      sort_order = query.sort_order(sort: "-foo,bar,invalid", model: Test, default: { id: :asc })
-      expect(sort_order).to eq("tests.foo DESC, tests.bar ASC")
+      sort_order = query.sort_order(sort: "-foo,bar,invalid", model:, default: { id: :asc })
+      expect(sort_order).to eq("#{model.table_name}.foo DESC, #{model.table_name}.bar ASC")
     end
 
     it "returns nil when there is no sort" do
-      expect(query.sort_order(sort: " ", model: Test)).to be_nil
+      expect(query.sort_order(sort: " ", model:)).to be_nil
     end
 
     it "returns the default sort order when there is no sort" do
       default = { created_at: :asc }
-      sort_order = query.sort_order(sort: nil, default:, model: Test)
+      sort_order = query.sort_order(sort: nil, default:, model:)
       expect(sort_order).to eq(default)
     end
   end
