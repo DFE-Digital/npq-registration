36fbf66d81d4d6a05aedfa5897311f913abe8941
shaheislamdfe <shahe.islam@education.gov.uk>
2025-07-18 09:16:36 +0100

    Configure containers to run as non-root user (#2722)
    ### Context
    
    
    https://trello.com/c/ylAGk14G/2375-security-aks-containers-run-as-non-root-user-npq
    
    To restrict user to non root and only allow non read permission on
    directories which require write access
    
    ### Changes proposed in this pull request
    
    <img width="1127" height="441" alt="Screenshot 2025-07-17 at 12 59 37"
    src="https://github.com/user-attachments/assets/2002e76e-e9c3-4578-ab36-5d8ea33afbd1"
    />
    
    Update: Edited to disable bullet.log and remove /app/log chown
    
    <img width="1040" height="207" alt="Screenshot 2025-07-17 at 14 18 14"
    src="https://github.com/user-attachments/assets/ee69c2e1-edee-4de1-8c9d-39a24d1fb0f4"
    />
    
    ---------
    
    Co-authored-by: Mohammed Islam <mohammed.islam2@bp.com>

--- config/environments/review.rb
+++ config/environments/review.rb
@@ -3,7 +3,7 @@ require Rails.root.join("config/environments/production")
 Rails.application.configure do
   config.after_initialize do
     Bullet.enable                       = true
-    Bullet.bullet_logger                = true
+    Bullet.bullet_logger                = false
     Bullet.raise                        = true # Raise an error if n+1 query occurs
     Bullet.unused_eager_loading_enable  = false # Disabled due to the way our queries are structured
   end
