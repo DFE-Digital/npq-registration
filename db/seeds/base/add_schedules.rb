1073f10d58c70a15565747d10b6db6114d9225dc
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-07-18 10:47:46 +0100

    [CPDNPQ-2811] application accept error messages (#2708)
    ### Context
    
    Ticket: https://dfedigital.atlassian.net/browse/CPDNPQ-2811
    
    currently, 3 scenarios are covered by the "Schedule cannot be
    determined" error:
    1. A schedule identifier has been specified when accepting the
    application, but that schedule does not exist in the application's
    cohort - this is something under the user's control - they can specify a
    different identifier, or not specify one, and we'll determine the
    correct schedule.
    2. No schedule identifier has been specified, and we have not been able
    to determine what schedule to use - this is a problem on our end.
    3. When the selected schedule is not valid for the application's course,
    we currently show 2 errors: "The selected schedule is not valid for the
    course" and "Schedule cannot be determined" - we shouldn't be showing
    the second error really
    
    ### Changes proposed in this pull request
    
    - For scenario (1.) above - I'm suggesting we should have a better
    error: "The selected schedule cannot be found in the application cohort"
    - For scenario (2.) we still have the same error message, and we still
    send an error to Sentry, as this is something we need to look into.
    - For scenario (3.) we will only show the one error: "The selected
    schedule is not valid for the course"
    - Make our seeded schedules look more like production

--- db/seeds/base/add_schedules.rb
+++ db/seeds/base/add_schedules.rb
@@ -1,4 +1,20 @@
-Cohort.find_each do |cohort|
+# cohorts up to 2025 reflect production
+{
+  2021 => %i[npq_leadership_spring npq_leadership_autumn npq_specialist_spring npq_specialist_autumn npq_aso_december npq_aso_june npq_aso_march npq_aso_november npq_ehco_june],
+  2022 => %i[npq_leadership_spring npq_leadership_autumn npq_specialist_spring npq_specialist_autumn npq_ehco_june npq_ehco_march npq_ehco_november npq_ehco_december],
+  2023 => %i[npq_leadership_spring npq_leadership_autumn npq_specialist_spring npq_specialist_autumn npq_ehco_june npq_ehco_march npq_ehco_november npq_ehco_december],
+  2024 => %i[npq_leadership_autumn npq_specialist_autumn npq_ehco_june npq_ehco_november npq_ehco_december],
+  2025 => %i[npq_leadership_spring npq_specialist_spring npq_ehco_march npq_ehco_june],
+}.each do |start_year, schedules|
+  funding_cap = start_year >= 2024
+  cohort = FactoryBot.create(:cohort, start_year:, funding_cap:)
+  schedules.each do |schedule_identifier|
+    FactoryBot.create(:schedule, schedule_identifier, cohort:, change_applies_dates: false)
+  end
+end
+
+# cohorts from 2026 onwards have all schedules
+Cohort.where(start_year: 2026..).find_each do |cohort|
   %i[
     npq_aso_december
     npq_aso_june
