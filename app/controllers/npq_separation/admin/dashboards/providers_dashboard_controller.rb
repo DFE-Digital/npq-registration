9fa318556712ffe6e638be163c31e089b6b89463
Emma Frith <emma.frith@education.gov.uk>
2025-07-24 18:24:21 +0100

    Make the all option work

--- app/controllers/npq_separation/admin/dashboards/providers_dashboard_controller.rb
+++ app/controllers/npq_separation/admin/dashboards/providers_dashboard_controller.rb
@@ -1,6 +1,10 @@
 class NpqSeparation::Admin::Dashboards::ProvidersDashboardController < NpqSeparation::AdminController
   def show
-    @cohort = Cohort.find_by(id: params[:cohort_id]) || Cohort.current
-    @applications = Application.where(cohort: @cohort)
+    if params[:cohort_id].present?
+      @cohort = Cohort.find_by(id: params[:cohort_id])
+      @applications = Application.where(cohort: @cohort)
+    else
+      @applications = Application
+    end
   end
 end
