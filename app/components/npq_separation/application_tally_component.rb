3c4f04cb82028a27ad35ba196f92bbe0df7e5bf0
Slawosz Slawinski <slawosz@gmail.com>
2025-07-24 17:32:42 +0100

    Example of removing abstraction for easier undrestanding

--- app/components/npq_separation/application_tally_component.rb
+++ app/components/npq_separation/application_tally_component.rb
@@ -2,13 +2,23 @@ module NpqSeparation
   class ApplicationTallyComponent < ViewComponent::Base
     attr_reader :applications, :dimension, :dimension_header
 
+    # Dimension is a relation name that we group by our records eg :course or :lead_provider
     def initialize(applications, dimension, dimension_header: nil)
       @applications = applications
       @dimension = dimension
       @dimension_header = dimension_header || dimension.to_s.titleize
     end
 
+    # Eg. `[1,2,3,3,2,4,5].tally`
     def rows
+      # if params[:cohort_id].present?
+      #   cohort = Cohort.find_by(id: params[:cohort_id])
+      #   # we add condition ie. only one cohort
+      #   applications = Application.where(cohort: @cohort)
+      # else
+      #   # we add condition ie. only one cohort
+      #   applications = Application
+      # end
       applications.joins(dimension).pluck(column).tally.sort
     end
 
--- app/controllers/npq_separation/admin/dashboards/courses_dashboard_controller.rb
+++ app/controllers/npq_separation/admin/dashboards/courses_dashboard_controller.rb
@@ -1,6 +1,17 @@
 class NpqSeparation::Admin::Dashboards::CoursesDashboardController < NpqSeparation::AdminController
   def show
-    @cohort = Cohort.find_by(id: params[:cohort_id]) || Cohort.current
-    @applications = Application.where(cohort: @cohort)
+    if params[:cohort_id].present?
+      @cohort = Cohort.find_by(id: params[:cohort_id])
+      # we add condition ie. only one cohort
+      @applications = Application.where(cohort: @cohort)
+    else
+      # we add condition ie. only one cohort
+      @applications = Application
+    end
+
+    # this splits application to course and counts how many of application is per course
+    #
+    # Eg. `[1,2,3,3,2,4,5].tally`
+    @rows = @applications.joins(:course).pluck("courses.name").tally.sort
   end
 end
