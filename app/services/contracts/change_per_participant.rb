04666cea39febd9930bd71bc6c483e984af046d7
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-07-24 11:56:00 +0100

    update all contracts for current and future statements for that cohort and lead provider

--- app/services/contracts/change_per_participant.rb
+++ app/services/contracts/change_per_participant.rb
@@ -15,15 +15,46 @@ module Contracts
     validates :per_participant, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :statement, presence: true
     validates :contract, presence: true
+    validate :contract_belongs_to_statement, if: -> { contract.present? }
+    validate :statement_not_paid, if: -> { statement.present? }
 
     def change
       return false if invalid?
 
-      contract_template = contract.contract_template.find_from_existing(per_participant:) || contract.contract_template.new_from_existing(per_participant:)
-      contract.update(contract_template:) # rubocop:disable Rails/SaveBang - return value is used by caller
+      ActiveRecord::Base.transaction do
+        current_and_future_statements.each do |statement_for_month|
+          contract_for_month = statement_for_month.contracts.find_by(course: contract.course)
+          contract_template = contract_for_month.contract_template.find_from_existing(per_participant:) || contract_for_month.contract_template.new_from_existing(per_participant:)
+          contract_for_month.update!(contract_template:)
+        end
+      end
+
+      true
     end
 
   private
+
+    def contract_belongs_to_statement
+      return if contract.statement == statement
+
+      errors.add(:contract, :does_not_belong_to_statement)
+    end
+
+    def statement_not_paid
+      return unless current_and_future_statements.any?(&:paid?)
+
+      errors.add(:statement, :paid)
+    end
+
+    def current_and_future_statements
+      (Time.zone.today..Date.new(last_statement.year, last_statement.month)).map { |d| [d.year, d.month] }.uniq.map { |year, month|
+        Statement.find_by(year:, month:, cohort: statement.cohort, lead_provider: statement.lead_provider)
+      }.compact
+    end
+
+    def last_statement
+      Statement.where(cohort: statement.cohort, lead_provider: statement.lead_provider).order(:year, :month).last
+    end
 
     def round_per_participant
       self.per_participant = per_participant.to_f.round(2)
--- spec/services/contracts/change_per_participant_spec.rb
+++ spec/services/contracts/change_per_participant_spec.rb
@@ -3,11 +3,24 @@
 require "rails_helper"
 
 RSpec.describe Contracts::ChangePerParticipant, type: :model do
-  subject { described_class.new(statement:, contract:, per_participant:) }
+  subject(:service) { described_class.new(statement: future_statement, contract: future_contract, per_participant:) }
 
-  let(:statement) { create(:statement) }
-  let!(:contract) { create(:contract, statement:) }
-
+  let(:lead_provider) { create(:lead_provider) }
+  let(:statement) { create(:statement, lead_provider:) }
+  let(:course) { create(:course, :senior_leadership) }
+  let(:last_months_statement) { create(:statement, lead_provider:, month: Time.zone.today.month - 1, year: Time.zone.today.year) }
+  let!(:last_months_contract) { create(:contract, statement: last_months_statement, course:) }
+  let(:this_months_statement) { create(:statement, lead_provider:, month: Time.zone.today.month, year: Time.zone.today.year) }
+  let!(:this_months_contract) { create(:contract, statement: this_months_statement, course:) }
+  let(:next_months_statement) { create(:statement, lead_provider:, month: Time.zone.today.month + 1, year: Time.zone.today.year) }
+  let!(:next_months_contract) { create(:contract, statement: next_months_statement, course:) }
+  let(:future_statement) { create(:statement, lead_provider:, month: Time.zone.today.month + 2, year: Time.zone.today.year) }
+  let!(:future_contract) { create(:contract, statement: future_statement, course:) }
+  let!(:contract_for_other_course) { create(:contract, statement: next_months_statement, course: create(:course, :early_headship_coaching_offer)) }
+  let(:statement_in_other_cohort) { create(:statement, lead_provider:, cohort: create(:cohort, :next)) }
+  let!(:contract_in_other_cohort) { create(:contract, statement: statement_in_other_cohort, course:) }
+  let(:statement_for_other_provider) { create(:statement, lead_provider: create(:lead_provider), month: Time.zone.today.month + 1, year: Time.zone.today.year) }
+  let!(:contract_for_other_provider) { create(:contract, statement: statement_for_other_provider, course:) }
   let(:per_participant) { "1234.56" }
 
   describe "validations" do
@@ -30,25 +43,76 @@ RSpec.describe Contracts::ChangePerParticipant, type: :model do
     it "does not allow a nil contract" do
       expect(subject).not_to allow_value(nil).for(:contract)
     end
+
+    context "when a statement is paid" do
+      before do
+        create(:statement, :paid, lead_provider:, month: Time.zone.today.month + 3, year: Time.zone.today.year)
+      end
+
+      it "does not allow changes" do
+        expect(subject).not_to be_valid
+        expect(subject).to have_error(:statement, :paid, "The statement is already paid and cannot be changed")
+      end
+    end
+
+    context "when a contract is not associated with the statement" do
+      subject { described_class.new(statement: future_statement, contract: contract_for_other_course, per_participant:) }
+
+      it "does not allow changes" do
+        expect(subject).not_to be_valid
+        expect(subject).to have_error(:contract, :does_not_belong_to_statement, "The contract does not belong to the specified statement")
+      end
+    end
   end
 
   describe "#change" do
+    subject { service.change }
+
     context "when there is a matching contract template" do
       let!(:contract_template) { create(:contract_template, per_participant:) }
 
       it "does not create a new contract template" do
-        expect { subject.change }.not_to change(ContractTemplate, :count)
+        expect { subject }.not_to change(ContractTemplate, :count)
       end
 
-      it "updates the contract template to the matching one" do
-        expect { subject.change }.to change { contract.reload.contract_template }.to(contract_template)
+      it "updates the contract templates for contracts from the current on onwards" do
+        subject
+        expect(this_months_contract.reload.contract_template).to eq(contract_template)
+        expect(next_months_contract.reload.contract_template).to eq(contract_template)
+        expect(future_contract.reload.contract_template).to eq(contract_template)
+      end
+
+      it "does not update past contracts" do
+        expect { subject }.not_to(change { last_months_contract.reload.contract_template })
+      end
+
+      it "does not update contracts for statements in other cohorts" do
+        expect { subject }.not_to(change { contract_in_other_cohort.reload.contract_template })
+      end
+
+      it "does not update contracts for statements for other providers" do
+        expect { subject }.not_to(change { contract_for_other_provider.reload.contract_template })
       end
     end
 
     context "when there isn't a matching contract template" do
       it "creates a new contract template with the per_participant value" do
-        expect { subject.change }.to change(ContractTemplate, :count).by(1)
-        expect(contract.contract_template.per_participant).to eq(per_participant.to_f)
+        expect { subject }.to change(ContractTemplate, :count).by(1)
+      end
+
+      it "updates the contract templates for contracts from the current on onwards" do
+        subject
+        expect(this_months_contract.reload.contract_template.per_participant).to eq(per_participant.to_f)
+        expect(next_months_contract.reload.contract_template.per_participant).to eq(per_participant.to_f)
+        expect(future_contract.reload.contract_template.per_participant).to eq(per_participant.to_f)
+      end
+
+      it "does not update past contracts" do
+        expect { subject }.not_to(change { last_months_contract.reload.contract_template })
+      end
+
+      it "does not update contracts for statements in other cohorts" do
+        expect { subject }.not_to(change { contract_in_other_cohort.reload.contract_template })
       end
     end
 
@@ -56,7 +120,7 @@ RSpec.describe Contracts::ChangePerParticipant, type: :model do
       let(:per_participant) { "1234.5678" }
 
       it "rounds the per_participant value to 2 decimal places" do
-        expect { subject.change }.to change(contract, :per_participant).to(1234.57)
+        expect { subject }.to change { this_months_contract.reload.contract_template.per_participant }.to(1234.57)
       end
     end
   end
