e8f738f956d613c1424d05477c048cb5e5a7d186
Emma Frith <emma.frith@education.gov.uk>
2025-07-28 14:56:12 +0100

    Add scenarios to test that the cohort filter displays relevant results

--- spec/features/npq_separation/admin/dashboards/courses_dashboard_spec.rb
+++ spec/features/npq_separation/admin/dashboards/courses_dashboard_spec.rb
@@ -15,4 +15,16 @@ RSpec.feature "Viewing summary dashboard", type: :feature do
     expect(page).to have_css("th", text: "Course")
     expect(page).to have_css("th", text: "Applications")
   end
+
+  scenario "filtering by a single cohort" do
+    test_course = create(:course, name: "Filtered Course")
+    create(:application, course: test_course, cohort: Cohort.current)
+
+    visit npq_separation_admin_dashboards_courses_dashboard_path
+
+    select Cohort.current.start_year.to_s, from: "Search by cohort"
+    click_button "Search"
+
+    expect(page).to have_content("Filtered Course")
+  end
 end
--- spec/features/npq_separation/admin/dashboards/providers_dashboard_spec.rb
+++ spec/features/npq_separation/admin/dashboards/providers_dashboard_spec.rb
@@ -15,4 +15,16 @@ RSpec.feature "Viewing summary dashboard", type: :feature do
     expect(page).to have_css("th", text: "Provider")
     expect(page).to have_css("th", text: "Applications")
   end
+
+  scenario "filtering providers dashboard by a single cohort" do
+    test_provider = create(:lead_provider, name: "Filtered Provider")
+    create(:application, cohort: Cohort.current, lead_provider: test_provider)
+
+    visit npq_separation_admin_dashboards_providers_dashboard_path
+
+    select Cohort.current.start_year.to_s, from: "Search by cohort"
+    click_button "Search"
+
+    expect(page).to have_content("Filtered Provider")
+  end
 end
