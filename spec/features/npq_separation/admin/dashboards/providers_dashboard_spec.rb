bd3c9a4e8d8e22b2d684aa94d4b47da3c049039b
Emma Frith <emma.frith@education.gov.uk>
2025-08-01 13:51:17 +0100

    Ensure test courses and providers show in the unfiltered tables

--- spec/features/npq_separation/admin/dashboards/providers_dashboard_spec.rb
+++ spec/features/npq_separation/admin/dashboards/providers_dashboard_spec.rb
@@ -3,9 +3,15 @@ require "rails_helper"
 RSpec.feature "Viewing the providers dashboard", type: :feature do
   include Helpers::AdminLogin
 
+  let(:test_provider) { create(:lead_provider, name: "Test Provider") }
+  let(:current_cohort) { create(:cohort, :current) }
+  let(:previous_cohort) { create(:cohort, :previous) }
+
   before do
-    create :cohort, :current
     sign_in_as(create(:admin))
+
+    create_list(:application, 2, cohort: current_cohort, lead_provider: test_provider)
+    create(:application, cohort: previous_cohort, lead_provider: test_provider)
   end
 
   scenario "viewing the providers dashboard table" do
@@ -14,16 +20,10 @@ RSpec.feature "Viewing the providers dashboard", type: :feature do
     expect(page).to have_css("h1", text: "Providers dashboard")
     expect(page).to have_css("th", text: "Provider")
     expect(page).to have_css("th", text: "Applications")
+    expect(page).to have_content("3")
   end
 
   scenario "filtering providers dashboard by a single cohort updates application counts" do
-    test_provider = create(:lead_provider, name: "Test Provider")
-    current_cohort = create(:cohort, :current)
-    previous_cohort = create(:cohort, :previous)
-    create_list(:application, 2, cohort: current_cohort, lead_provider: test_provider)
-
-    create(:application, cohort: previous_cohort, lead_provider: test_provider)
-
     visit npq_separation_admin_dashboard_path("providers-dashboard")
 
     select previous_cohort.start_year.to_s, from: "Search by cohort"
