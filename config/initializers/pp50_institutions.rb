4a5e34fbb3bab4123eb767d6da7e220d4fac9589
Slawosz Slawinski <slawosz@gmail.com>
2025-08-06 12:45:35 +0100

    Add new PP50 CSVs

--- config/initializers/pp50_institutions.rb
+++ config/initializers/pp50_institutions.rb
@@ -1,6 +1,6 @@
 require "csv"
-PP50_SCHOOLS_CSV    = "config/data/January2025/NPQ_Schools_PP50_2025_cohort.csv".freeze
-PP50_FE_CSV         = "config/data/January2025/NPQ_FE_PP50_2025_cohort.csv".freeze # FE is further education
+PP50_SCHOOLS_CSV    = "config/data/Autumn2025/Schools_PP50_Autumn_2025.csv".freeze
+PP50_FE_CSV         = "config/data/Autumn2025/FE_PP50_Autumn_2025.csv".freeze # FE is further education
 EY_CHILDMINDERS_CSV = "config/data/January2025/NPQ_EY_Childminders_2025_cohort.csv".freeze # EY is early years
 EY_SCHOOLS_CSV      = "config/data/January2025/NPQ_Disadvantaged_EY_2025_cohort.csv".freeze
 EY_LA_NURSERIES_CSV = "config/data/January2025/NPQ_LA_Nursery_Schools_2025_cohort.csv".freeze # LA is local authority
