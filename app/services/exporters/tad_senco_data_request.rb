0a0da9960bb8930af5a5c3e506ff79a1933e49ee
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-08-12 13:52:24 +0100

    improvements

--- app/services/exporters/tad_senco_data_request.rb
+++ app/services/exporters/tad_senco_data_request.rb
@@ -3,13 +3,10 @@ class Exporters::TadSencoDataRequest
     @cohort = cohort
     @schedules = schedules
     @file = file
-    @csv = []
-    @csv << csv_headers
   end
 
   def call
-    generate_data
-    save_data_to_csv
+    generate_csv
   end
 
   def applications
@@ -21,45 +18,44 @@ class Exporters::TadSencoDataRequest
 
 private
 
-  def generate_data
-    applications.find_each(batch_size: 500) do |application|
-      @csv << [
-        application.training_status ? application.user.full_name : nil,
-        application.training_status ? application.user.email : nil,
-        application.id,
-        application.ecf_id,
-        application.user.id,
-        application.user.ecf_id,
-        application.user.trn_verified,
-        application.lead_provider.id,
-        application.lead_provider.ecf_id,
-        application.employment_role,
-        application.employment_type,
-        application.work_setting,
-        application.works_in_school,
-        application&.school&.urn,
-        application&.private_childcare_provider&.urn,
-        application&.school&.number_of_pupils,
-        application.eligible_for_funding,
-        application.funding_choice,
-        application.funded_place,
-        application.senco_in_role,
-        application.senco_start_date,
-        started?(application),
-        application.lead_provider_approval_status,
-        application.training_status,
-        application.headteacher_status,
-        application.cohort.start_year,
-      ]
+  def generate_csv
+    CSV.open(@file, "w") do |csv|
+      csv << csv_headers
+      applications.find_each(batch_size: 500) do |application|
+        csv << [
+          application.training_status ? application.user.full_name : nil,
+          application.training_status ? application.user.email : nil,
+          application.id,
+          application.ecf_id,
+          application.user.id,
+          application.user.ecf_id,
+          application.user.trn_verified,
+          application.lead_provider.id,
+          application.lead_provider.ecf_id,
+          application.employment_role,
+          application.employment_type,
+          application.work_setting,
+          application.works_in_school,
+          application&.school&.urn,
+          application&.private_childcare_provider&.urn,
+          application&.school&.number_of_pupils,
+          application.eligible_for_funding,
+          application.funding_choice,
+          application.funded_place,
+          application.senco_in_role,
+          application.senco_start_date,
+          started?(application),
+          application.lead_provider_approval_status,
+          application.training_status,
+          application.headteacher_status,
+          application.cohort.start_year,
+        ]
+      end
     end
   end
 
   def started?(application)
-    application.declarations.where(declaration_type: "started").any?
-  end
-
-  def save_data_to_csv
-    @file.write(@csv.map(&:to_csv).join)
+    !!application.declarations.find { |d| d.declaration_type == "started" }
   end
 
   def csv_headers
--- spec/services/exporters/tad_senco_data_request_spec.rb
+++ spec/services/exporters/tad_senco_data_request_spec.rb
@@ -51,18 +51,70 @@ RSpec.describe Exporters::TadSencoDataRequest do
 
   describe "#call" do
     let(:headers) do
-      "Full Name,Email,Application ID,Application ECF ID,User ID,User ECF ID,TRN Verified,Lead Provider ID,Lead Provider ECF ID,Employment Role,Employment Type,Work Setting,Works In School,School URN,Private Childcare Provider URN,Number Of Pupils,Eligible For Funding,Funding Choice,Funded Place,Senco In Role,Senco Start Date,Started Course,Lead Provider Approval Status,Training Status,Headteacher Status,Cohort"
+      [
+        "Full Name",
+        "Email",
+        "Application ID",
+        "Application ECF ID",
+        "User ID",
+        "User ECF ID",
+        "TRN Verified",
+        "Lead Provider ID",
+        "Lead Provider ECF ID",
+        "Employment Role",
+        "Employment Type",
+        "Work Setting",
+        "Works In School",
+        "School URN",
+        "Private Childcare Provider URN",
+        "Number Of Pupils",
+        "Eligible For Funding",
+        "Funding Choice",
+        "Funded Place",
+        "Senco In Role",
+        "Senco Start Date",
+        "Started Course",
+        "Lead Provider Approval Status",
+        "Training Status",
+        "Headteacher Status",
+        "Cohort",
+      ]
     end
-    let(:expected_csv) do
-      <<~CSV
-        #{headers}
-        #{expected_name},#{expected_email},#{application.id},#{application.ecf_id},#{user.id},#{user.ecf_id},#{user.trn_verified},#{application.lead_provider.id},#{application.lead_provider.ecf_id},#{application.employment_role},#{application.employment_type},#{application.work_setting},#{application.works_in_school},#{application&.school&.urn},#{application&.private_childcare_provider&.urn},#{application&.school&.number_of_pupils},#{application.eligible_for_funding},#{application.funding_choice},#{application.funded_place},#{application.senco_in_role},#{application.senco_start_date},#{expected_started},#{application.lead_provider_approval_status},#{application.training_status},#{application.headteacher_status},#{cohort.start_year}
-      CSV
+    let(:expected_data) do
+      [
+        expected_name,
+        expected_email,
+        application.id,
+        application.ecf_id,
+        user.id,
+        user.ecf_id,
+        user.trn_verified,
+        application.lead_provider.id,
+        application.lead_provider.ecf_id,
+        application.employment_role,
+        application.employment_type,
+        application.work_setting,
+        application.works_in_school,
+        application&.school&.urn,
+        application&.private_childcare_provider&.urn,
+        application&.school&.number_of_pupils,
+        application.eligible_for_funding,
+        application.funding_choice,
+        application.funded_place,
+        application.senco_in_role,
+        application.senco_start_date,
+        expected_started,
+        application.lead_provider_approval_status,
+        application.training_status,
+        application.headteacher_status,
+        cohort.start_year,
+      ]
     end
 
     let(:expected_name) { user.full_name }
     let(:expected_email) { user.email }
     let(:expected_started) { false }
+    let(:expected_csv) { CSV.generate_lines([headers, expected_data]) }
 
     context "when there are no started declarations" do
       it "produces a CSV with Started Course: false" do
