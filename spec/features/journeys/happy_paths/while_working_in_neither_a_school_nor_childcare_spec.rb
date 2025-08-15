faf6c7f5226f30de0580de931a924e2f27a09f54
Richard Worrall <richard@soupish.net>
2025-08-15 15:48:25 +0100

    Update HandleSubmissionForStore manual_review criteria

--- spec/features/journeys/happy_paths/while_working_in_neither_a_school_nor_childcare_spec.rb
+++ spec/features/journeys/happy_paths/while_working_in_neither_a_school_nor_childcare_spec.rb
@@ -157,7 +157,7 @@ RSpec.feature "Happy journeys", :with_default_schedules, type: :feature do
       "senco_in_role" => nil,
       "senco_start_date" => nil,
       "on_submission_trn" => nil,
-      "review_status" => "Needs review",
+      "review_status" => nil,
       "raw_application_data" => {
         "can_share_choices" => "1",
         "chosen_provider" => "yes",
