c7d7d925188503618eed6a499c2ea247388ed44b
jebw <jeb@jdwilkins.co.uk>
2025-08-18 17:39:50 +0100

    [CPDNPQ-2298] Addressed review comments

---
+++ spec/forms/questionnaires/start_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe Questionnaires::Start do
+  it { is_expected.to be_requirements_met }
+
+  describe "#next_step?" do
+    subject { instance.next_step }
+
+    before { instance.wizard = wizard }
+
+    let(:instance) { described_class.new }
+    let(:wizard) { RegistrationWizard.new(store:, request:, current_step: :start, current_user:) }
+    let(:request) { nil }
+    let(:store) { {} }
+
+    context "when logged in" do
+      context "with TRN" do
+        let(:current_user) { build_stubbed :user }
+
+        it { is_expected.to eq :course_start_date }
+      end
+
+      context "without TRN" do
+        let(:current_user) { build_stubbed :user, trn: nil }
+
+        it { is_expected.to eq :teacher_reference_number }
+      end
+    end
+
+    context "when not logged in" do
+      let(:current_user) { nil }
+
+      it { is_expected.to eq :start }
+    end
+  end
+end
