9a0bf68db60ad1af6b0932e278fd3c7ad0c35554
jebw <jeb@jdwilkins.co.uk>
2025-08-19 17:25:53 +0100

    [NPQ-3401] Added Workplace model

--- app/controllers/npq_separation/admin/schools_controller.rb
+++ app/controllers/npq_separation/admin/schools_controller.rb
@@ -1,11 +1,18 @@
 class NpqSeparation::Admin::SchoolsController < NpqSeparation::AdminController
   def index
     @pagy, @schools = pagy(scope)
+    @schools = @schools.map(&:source) unless @schools.is_a?(AdminService::WorkplaceSearch)
   end
 
 private
 
   def scope
-    AdminService::WorkplaceSearch.new(q: params[:q])
+    if params[:usenew].to_s != "0"
+      Workplace.includes(:source)
+               .order(:name, :source_id)
+               .search(params[:q])
+    else
+      AdminService::WorkplaceSearch.new(q: params[:q])
+    end
   end
 end
