4b1a25ef4e32e7150b0f21ca9bbe4f0f197ce978
Richard Worrall <richard@soupish.net>
2025-09-02 14:25:12 +0100

    Remove jobs for sending outcomes

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -5,6 +5,5 @@ Rails.autoloaders.each do |autoloader|
     "npq" => "NPQ",
     "qualified_teachers_api_sender" => "QualifiedTeachersAPISender",
     "stream_api_requests_to_big_query_job" => "StreamAPIRequestsToBigQueryJob",
-    "send_to_qualified_teachers_api_job" => "SendToQualifiedTeachersAPIJob",
   )
 end
