36b95e8ed16c1a8cdc203106856cbd87f2182569
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-09-03 10:53:22 +0100

    [CPDNPQ-3025] show TRN verified automatically (#2839)
    ### Context
    
    Ticket: https://dfedigital.atlassian.net/browse/CPDNPQ-3025
    
    ### Changes proposed in this pull request

--- spec/features/npq_separation/admin/users_trn_verification_spec.rb
+++ spec/features/npq_separation/admin/users_trn_verification_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "viewing a user's TRN status", type: :feature do
+RSpec.feature "viewing a user's TRN status", :no_js, type: :feature do
   include Helpers::AdminLogin
 
   feature "when TRN verified is true and TRN auto verified is true" do
@@ -9,18 +9,31 @@ RSpec.feature "viewing a user's TRN status", type: :feature do
       sign_in_as(create(:admin))
       visit npq_separation_admin_user_path(user)
       within(".govuk-summary-card", text: "Overview") do |summary_card|
-        expect(summary_card).to have_summary_item("TRN", user.trn, "Verified - automatically")
+        expect(summary_card).to have_summary_item("TRN", "#{user.trn} Verified - automatically")
       end
     end
   end
 
   feature "when TRN verified is true and TRN auto verified is false" do
-    scenario "shows TRN verified manually" do
-      user = create(:user, trn_verified: true, trn_auto_verified: false)
-      sign_in_as(create(:admin))
-      visit npq_separation_admin_user_path(user)
-      within(".govuk-summary-card", text: "Overview") do |summary_card|
-        expect(summary_card).to have_summary_item("TRN", user.trn, "Verified - manually")
+    context "when TRN lookup status is Found" do
+      scenario "shows TRN verified automatically" do
+        user = create(:user, trn_verified: true, trn_auto_verified: false, trn_lookup_status: "Found")
+        sign_in_as(create(:admin))
+        visit npq_separation_admin_user_path(user)
+        within(".govuk-summary-card", text: "Overview") do |summary_card|
+          expect(summary_card).to have_summary_item("TRN", "#{user.trn} Verified - automatically")
+        end
+      end
+    end
+
+    context "when TRN lookup status is not Found" do
+      scenario "shows TRN verified manually" do
+        user = create(:user, trn_verified: true, trn_auto_verified: false, trn_lookup_status: "Pending")
+        sign_in_as(create(:admin))
+        visit npq_separation_admin_user_path(user)
+        within(".govuk-summary-card", text: "Overview") do |summary_card|
+          expect(summary_card).to have_summary_item("TRN", "#{user.trn} Verified - manually")
+        end
       end
     end
   end
@@ -31,19 +44,8 @@ RSpec.feature "viewing a user's TRN status", type: :feature do
       sign_in_as(create(:admin))
       visit npq_separation_admin_user_path(user)
       within(".govuk-summary-card", text: "Overview") do |summary_card|
-        expect(summary_card).to have_summary_item("TRN", user.trn, "Not verified")
+        expect(summary_card).to have_summary_item("TRN", "#{user.trn} Not verified")
       end
-    end
-  end
-
-  feature "when TRN verified is false and TRN auto verified is false" do
-    scenario "shows TRN not verified" do
-      user = create(:user, trn_verified: false, trn_auto_verified: false)
-      sign_in_as(create(:admin))
-      visit npq_separation_admin_user_path(user)
-      expect(page).to have_content("TRN")
-      expect(page).to have_content(user.trn)
-      expect(page).to have_content("Not verified")
     end
   end
 end
