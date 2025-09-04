da47167afa534706a883df8abe2407f8cc097fe6
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-09-04 11:26:40 +0100

    better naming in html matcher

--- spec/support/matchers/html_matcher.rb
+++ spec/support/matchers/html_matcher.rb
@@ -1,11 +1,11 @@
-RSpec::Matchers.define :have_html do |attribute, js: true|
+RSpec::Matchers.define :have_html do |expected_html, js: true|
   match do
-    string = attribute.gsub("<br>", "\n") if js # a quirk of the capybara vs racktest rendering
+    string = expected_html.gsub("<br>", "\n") if js # a quirk of the capybara vs racktest rendering
     expected_string = ActionController::Base.helpers.strip_tags(string)
     expect(page).to have_text(expected_string)
   end
 
   failure_message do |actual|
-    "expected #{actual.body} to include #{attribute}"
+    "expected #{actual.body} to include #{expected_html}"
   end
 end
