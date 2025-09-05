3de99176e38166aa4cc2c6fa8402dd47de29f4b0
Emma Frith <emma.frith@education.gov.uk>
2025-09-05 17:36:12 +0100

    Schema auto-updated to reflect the migration converting employment_type to an enum.
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_08_162110) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_05_101912) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "citext"
@@ -608,7 +608,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_09_08_162110) do
     t.json "object_changes"
     t.string "note"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
-    t.index ["whodunnit"], name: "index_versions_on_whodunnit"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
