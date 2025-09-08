2a141c6aee280d76635b35b9d043600df463bc6f
Richard Worrall <richard@soupish.net>
2025-09-08 11:37:29 +0100

    Implement ExternalLink class

---
+++ app/models/external_link.rb
@@ -0,0 +1,64 @@
+class ExternalLink
+  class VerificationError < StandardError; end
+
+  class << self
+    attr_writer :logger
+
+    def config_path = Rails.root.join("config/external_links.yml")
+
+    def all = mapping.values.map { new(it) }
+
+    def fetch(key) = new(mapping.fetch(key.to_s))
+
+    def verify_all = all.each(&:verify)
+
+    def logger
+      @logger ||= Logger.new($stdout)
+    end
+
+  private
+
+    def mapping
+      @mapping ||= YAML.load_file(config_path)
+    end
+  end
+
+  attr_reader :url
+
+  delegate :logger, to: :class
+
+  def initialize(url)
+    @url = url
+  end
+
+  def verify
+    response = request(url)
+
+    unless response.is_a? Net::HTTPSuccess
+      fail "URL returned status #{response.code}"
+    end
+
+    logger.info("External link #{url} verified successfully")
+    true
+  end
+
+private
+
+  def request(url, limit = 5)
+    fail "Too many redirects" if limit.zero?
+
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+
+    if response.is_a? Net::HTTPRedirection
+      request(response["location"], limit - 1)
+    else
+      response
+    end
+  end
+
+  def fail(message)
+    logger.fatal("External link #{url} failed verification: #{message}")
+    raise VerificationError, message
+  end
+end
