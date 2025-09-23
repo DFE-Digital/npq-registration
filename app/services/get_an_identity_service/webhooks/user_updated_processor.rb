7f84a2b338d28b806548c1e73cd6649011ecd818
Sławosz Sławiński <slawosz@gmail.com>
2025-09-23 09:38:11 +0100

    Update gai callback - remove unnecesarry updates and better tracing (#2873)
    ### Context
    
    Ticket: https://dfedigital.atlassian.net/browse/CPDNPQ-3202
    
    * Make sure user is not updated when there are no attributes updates
    when processin GAI webhook
    * Add better value to `whodunnit` field.

--- app/services/get_an_identity_service/webhooks/user_updated_processor.rb
+++ app/services/get_an_identity_service/webhooks/user_updated_processor.rb
@@ -79,7 +79,15 @@ module GetAnIdentityService
           updated_from_tra_at: decorated_message.sent_at,
         })
         user.set_trn_from_provider_data(trn:, trn_lookup_status:)
-        user.save # rubocop:disable Rails/SaveBang - return value is used by caller
+
+        if user.changed? && user.changes.excluding("updated_from_tra_at").any?
+          PaperTrail.request(whodunnit: "UserUpdatedProcessor") do
+            user.save
+          end
+        else
+          webhook_message.status_comment = "Skipped - no data changes"
+          true
+        end
       end
     end
   end
