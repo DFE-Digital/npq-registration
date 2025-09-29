d3674dce05cfba066c53dd73f8e04d84f041e50d
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-09-29 17:30:23 +0100

    different CSV headers for different list types

--- app/services/eligibility_lists/update.rb
+++ app/services/eligibility_lists/update.rb
@@ -9,28 +9,38 @@ module EligibilityLists
 
     validates :file, presence: true
     validate :headers_valid
+    validate :file_not_empty, if: -> { file.present? }
 
     def call
       return unless valid?
 
-      rows = CSV.table(file)
       ActiveRecord::Base.transaction do
         eligibility_list_type.constantize.delete_all
-        rows.each do |row|
-          eligibility_list_type.constantize.create!(identifier: row[eligibility_list_type.constantize::IDENTIFIER_TYPE])
+        csv_table.each do |row|
+          eligibility_list_type.constantize.create!(identifier: row[eligibility_list_type.constantize::IDENTIFIER_HEADER])
         end
       end
 
-      rows.count
+      csv_table.count
     end
 
   private
 
+    def csv_table
+      @csv_table ||= CSV.table(file, header_converters: [])
+    end
+
     def headers_valid
       return if errors.any?
 
-      actual_headers = CSV.table(file).headers
-      errors.add(:file, :invalid_headers) unless actual_headers.include?(eligibility_list_type.constantize::IDENTIFIER_TYPE)
+      actual_headers = csv_table.headers
+      errors.add(:file, :invalid_headers) unless actual_headers.include?(eligibility_list_type.constantize::IDENTIFIER_HEADER)
+    end
+
+    def file_not_empty
+      return if errors.any?
+
+      errors.add(:file, :empty) if csv_table.count.zero?
     end
   end
 end
