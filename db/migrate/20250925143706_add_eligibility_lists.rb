ff27781f43d6d0971fa769fff286b10caa5cada9
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-10-08 15:14:27 +0100

    DB migration can lock table, as it will be empty

--- db/migrate/20250925143706_add_eligibility_lists.rb
+++ db/migrate/20250925143706_add_eligibility_lists.rb
@@ -1,6 +1,4 @@
 class AddEligibilityLists < ActiveRecord::Migration[7.2]
-  disable_ddl_transaction!
-
   def change
     create_table :eligibility_list_entries do |t|
       t.string :type, null: false, index: true
@@ -9,6 +7,6 @@ class AddEligibilityLists < ActiveRecord::Migration[7.2]
       t.timestamps
     end
 
-    add_index :eligibility_list_entries, %i[type identifier identifier_type], unique: true, algorithm: :concurrently
+    add_index :eligibility_list_entries, %i[type identifier identifier_type], unique: true
   end
 end
