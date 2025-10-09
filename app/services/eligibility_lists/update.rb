56a1b3c5b629d7adf53d4d4e1ab0146fc04b6776
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-10-09 14:02:53 +0100

    cope with duplicates and ISO-8859-1 encoding

--- app/services/eligibility_lists/update.rb
+++ app/services/eligibility_lists/update.rb
@@ -17,7 +17,7 @@ module EligibilityLists
       ActiveRecord::Base.transaction do
         eligibility_list_type_class.delete_all
         csv_table.each do |row|
-          eligibility_list_type_class.create!(identifier: identifier(row))
+          eligibility_list_type_class.find_or_create_by!(identifier: identifier(row))
         end
       end
 
@@ -30,8 +30,12 @@ module EligibilityLists
       @eligibility_list_type_class ||= eligibility_list_type.constantize
     end
 
-    def csv_table
-      @csv_table ||= CSV.table(file, header_converters: ->(header) { header&.strip })
+    def csv_table(iso_8859_1: false)
+      @csv_table ||= if iso_8859_1
+                       CSV.table(file, encoding: "ISO-8859-1", header_converters: ->(header) { header&.strip })
+                     else
+                       CSV.table(file, header_converters: ->(header) { header&.strip })
+                     end
     end
 
     def csv_valid
@@ -39,8 +43,13 @@ module EligibilityLists
 
       all_headers_present = (eligibility_list_type_class::IDENTIFIER_CSV_HEADERS - csv_table.headers).empty?
       errors.add(:file, :invalid_headers) unless all_headers_present
-    rescue CSV::MalformedCSVError
+    rescue CSV::MalformedCSVError => e
+      if e.message.include?("Invalid byte sequence in UTF-8")
+        csv_table(iso_8859_1: true)
+        return if valid?
+      end
       errors.add(:file, :invalid)
+      errors.add(:base, message: e.message)
     end
 
     def file_not_empty
