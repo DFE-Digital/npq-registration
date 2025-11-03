211cf9c13898ea32dded0ec26a0f1ac727ce0898
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-11-03 09:59:02 +0000

    address SonarQube issue

--- spec/support/with_default_school.rb
+++ spec/support/with_default_school.rb
@@ -2,9 +2,15 @@
 
 RSpec.shared_context "with default school", shared_context: :metadata do
   before do
-    return if School.where(urn: 100_000).exists?
-
-    School.create!(urn: 100_000, name: "open manchester school", address_1: "street 1", town: "manchester", establishment_status_code: "1")
+    unless School.where(urn: 100_000).exists?
+      School.create!(
+        urn: 100_000,
+        name: "open manchester school",
+        address_1: "street 1",
+        town: "manchester",
+        establishment_status_code: "1",
+      )
+    end
   end
 end
 
