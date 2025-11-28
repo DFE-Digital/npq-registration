96e8ae018981172b3ad666cfa82e1396ac9384ad
Jerome Riga <jriga@zemis.co.uk>
2025-11-28 12:03:35 +0000

    Update database name prefix (#9)
    ### Context
    
    Ticket: #5
    
    Update database name for TTE

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "npq_registration_production"
+  # config.active_job.queue_name_prefix = "cpd_tte_production"
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
