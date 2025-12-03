cbba13417e7572486f394709cd11733140f8cbcb
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2025-12-03 09:11:24 +0000

    add index to schedules

--- db/migrate/20251128113435_add_policy_descriptor_and_acceptance_dates_to_schedules.rb
+++ db/migrate/20251128113435_add_policy_descriptor_and_acceptance_dates_to_schedules.rb
@@ -1,7 +1,11 @@
 class AddPolicyDescriptorAndAcceptanceDatesToSchedules < ActiveRecord::Migration[7.2]
+  disable_ddl_transaction!
+
   def change
     add_column :schedules, :policy_descriptor, :integer
     add_column :schedules, :acceptance_window_start, :date
     add_column :schedules, :acceptance_window_end, :date
+
+    add_index :schedules, %i[course_group_id acceptance_window_start acceptance_window_end], algorithm: :concurrently
   end
 end
