1f5427db959199ce119424b5b41f5c5b13d0b038
Jeb Wilkins <jeb@jdwilkins.co.uk>
2025-12-12 11:09:04 +0000

    [CPDNPQ-3363] Cohort migration script (#3034)
    ### Context
    
    Ticket: [NPQ-3363](https://dfedigital.atlassian.net/browse/NPQ-3363)
    
    Add migration script to move applications and declarations from spring
    to autumn cohort
    
    ### Changes proposed in this pull request
    
    1. Add service to handle the migration logic
    2. Add rake task to drive that service
    
    [NPQ-3363]:
    https://dfedigital.atlassian.net/browse/NPQ-3363?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ spec/lib/tasks/one_off/move_applications_to_autumn_2025_spec.rb
@@ -0,0 +1,72 @@
+require "rails_helper"
+
+RSpec.describe "one_off:move_applications_to_autumn_2025" do
+  subject :run_task do
+    Rake::Task["one_off:move_applications_to_autumn_2025"].invoke(lead_provider.id, "false")
+  end
+
+  before { autumn_statement }
+  after { Rake::Task["one_off:move_applications_to_autumn_2025"].reenable }
+
+  let(:lead_provider) { create(:lead_provider) }
+  let(:spring) { create(:cohort, start_year: 2025, suffix: "a") }
+  let(:autumn) { create(:cohort, start_year: 2025, suffix: "b") }
+
+  let :autumn_statement do
+    create :statement, :open, :next_output_fee, cohort: autumn,
+                                                lead_provider:
+  end
+
+  let :applications do
+    travel_to Time.zone.parse("2025-10-01") do
+      create_list(:application, 3, cohort: spring, lead_provider:)
+    end
+  end
+
+  context "with applications in spring cohort to be moved" do
+    let(:spring_schedule) { create(:schedule, :npq_leadership_autumn, cohort: spring) }
+    let(:autumn_schedule) { create(:schedule, :npq_leadership_autumn, cohort: autumn) }
+
+    context "and autumn cohort correctly setup" do
+      before { autumn && applications }
+
+      it "moves applications to autumn cohort" do
+        expect { run_task }
+          .to change { applications[0].reload.cohort }.from(spring).to(autumn)
+          .and change { applications[1].reload.cohort }.from(spring).to(autumn)
+          .and change { applications[2].reload.cohort }.from(spring).to(autumn)
+      end
+    end
+  end
+
+  context "with unknown lead provider" do
+    subject :run_task do
+      Rake::Task["one_off:move_applications_to_autumn_2025"].invoke(999, false)
+    end
+
+    before { applications && autumn }
+
+    it "raises an exception" do
+      expect { run_task }
+        .to raise_exception(ActiveRecord::RecordNotFound)
+        .and(not_change { applications[0].reload.cohort })
+        .and(not_change { applications[1].reload.cohort })
+        .and(not_change { applications[2].reload.cohort })
+    end
+  end
+
+  context "when performing a dry run" do
+    subject :run_task do
+      Rake::Task["one_off:move_applications_to_autumn_2025"].invoke(lead_provider.id, nil)
+    end
+
+    before { applications && autumn }
+
+    it "does not change those applications" do
+      expect { run_task }
+        .to not_change { applications[0].reload.cohort }
+        .and(not_change { applications[1].reload.cohort })
+        .and(not_change { applications[2].reload.cohort })
+    end
+  end
+end
