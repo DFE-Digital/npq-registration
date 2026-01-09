23e09a3d0f4fff5efa5be0362bd96f85456a4690
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-01-09 14:40:40 +0000

    improve expected_applications method, create all_expected_applications method, and memoize results

--- app/services/statements/declarations_calculator.rb
+++ app/services/statements/declarations_calculator.rb
@@ -2,56 +2,48 @@
 
 module Statements
   class DeclarationsCalculator
+    class InvalidDeclarationType < StandardError; end
+
     attr_reader :statement
 
     def initialize(statement:)
       @statement = statement
     end
 
-    def expected_applications(declaration_type = nil)
-      scope =
-        Application
-        .joins(:declarations, schedule: :milestones)
-        .where(
-          training_status: "active",
-          cohort: statement.cohort,
-          lead_provider: statement.lead_provider,
-        )
+    def expected_applications(declaration_type)
+      @expected_applications ||=
+        case declaration_type
+        when "started"
+          Application.where(cohort: statement.cohort, lead_provider: statement.lead_provider).accepted
+        when "retained-1"
+          applications_with_declarations_and_milestones(
+            declaration_type: :started,
+            milestone_declaration_type: :"retained-1",
+          )
+        when "retained-2"
+          applications_with_declarations_and_milestones(
+            declaration_type: :"retained-1",
+            milestone_declaration_type: :"retained-2",
+          )
+        when "completed"
+          applications_in_schedules_with_declarations_and_milestones(
+            schedules: Schedule.with_retained_2_milestone,
+            declaration_type: :"retained-2",
+            milestone_declaration_type: :completed,
+          ).or(
+            applications_in_schedules_with_declarations_and_milestones(
+              schedules: Schedule.without_retained_2_milestone,
+              declaration_type: :"retained-1",
+              milestone_declaration_type: :completed,
+            ),
+          ).distinct
+        else
+          raise InvalidDeclarationType, "Invalid declaration type: #{declaration_type}, class: #{declaration_type.class}"
+        end
+    end
 
-      case declaration_type
-      when "started"
-        Application.where(cohort: statement.cohort, lead_provider: statement.lead_provider).accepted
-      when "retained-1"
-        scope.where(
-          declarations: { declaration_type: Declaration.declaration_types[:started] },
-          schedule: { milestones: { declaration_type: Declaration.declaration_types[:"retained-1"] } },
-        ).distinct
-      when "retained-2"
-        scope.where(
-          declarations: { declaration_type: Declaration.declaration_types[:"retained-1"] },
-          schedule: { milestones: { declaration_type: Declaration.declaration_types[:"retained-2"] } },
-        ).distinct
-      when "completed"
-        scope.where(
-          declarations: { declaration_type: Declaration.declaration_types[:"retained-2"] },
-          schedule: {
-            id: Schedule.with_retained_2_milestone,
-            milestones: { declaration_type: Declaration.declaration_types[:completed] },
-          },
-        ).or(
-          scope.where(
-            declarations: { declaration_type: Declaration.declaration_types[:"retained-1"] },
-            schedule: {
-              id: Schedule.without_retained_2_milestone,
-              milestones: { declaration_type: Declaration.declaration_types[:completed] },
-            },
-          ),
-        ).distinct
-      when nil
-        statement.milestone_declaration_types.map { |declaration_type| expected_applications(declaration_type) }.flatten
-      else
-        raise "Invalid declaration type"
-      end
+    def all_expected_applications
+      @all_expected_applications ||= statement.milestone_declaration_types.map { |declaration_type| expected_applications(declaration_type) }.flatten
     end
 
     def received_declarations(declaration_type = nil)
@@ -62,6 +54,35 @@ module Statements
       else
         scope.where(declaration_type: statement.milestone_declaration_types)
       end
+    end
+
+  private
+
+    def active_applications
+      Application
+        .joins(:declarations, schedule: :milestones)
+        .where(
+          training_status: "active",
+          cohort: statement.cohort,
+          lead_provider: statement.lead_provider,
+        )
+    end
+
+    def applications_with_declarations_and_milestones(declaration_type:, milestone_declaration_type:)
+      active_applications.where(
+        declarations: { declaration_type: Declaration.declaration_types[declaration_type] },
+        schedule: { milestones: { declaration_type: Declaration.declaration_types[milestone_declaration_type] } },
+      ).distinct
+    end
+
+    def applications_in_schedules_with_declarations_and_milestones(schedules:, declaration_type:, milestone_declaration_type:)
+      active_applications.where(
+        declarations: { declaration_type: Declaration.declaration_types[declaration_type] },
+        schedule: {
+          id: schedules,
+          milestones: { declaration_type: Declaration.declaration_types[milestone_declaration_type] },
+        },
+      )
     end
   end
 end
