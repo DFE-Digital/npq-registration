5c192dada8dfc2a9c0618d0f5015a664c8b82d63
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-03-24 14:32:13 +0000

    update application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ Bundler.require(*Rails.groups)
 module NpqRegistration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    # config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w[assets tasks middleware])
 
     # Configuration for the application, engines, and railties goes here.
     #
