1d73fb36b5661c5914830f5169436fd14721f5e7
jebw <jeb@jdwilkins.co.uk>
2026-03-25 16:29:12 +0000

    [NPQ-3654] Remove circular route on private childcare wizard step

--- spec/forms/questionnaires/choose_private_childcare_provider_spec.rb
+++ spec/forms/questionnaires/choose_private_childcare_provider_spec.rb
@@ -1,28 +1,39 @@
 require "rails_helper"
 
 RSpec.describe Questionnaires::ChoosePrivateChildcareProvider, type: :model do
+  subject :instance do
+    described_class.new(wizard:,
+                        institution_identifier: identifier,
+                        institution_name: name)
+  end
+
   let(:current_step) { :choose_private_childcare_provider }
   let(:store) { { "works_in_childcare" => "yes" } }
   let(:request) { nil }
+  let(:identifier) { "" }
+  let(:name) { "" }
+  let(:provider) { create :private_childcare_provider, provider_urn: "8329422" }
 
   let(:wizard) do
     RegistrationWizard.new(current_step:, store:, request:, current_user: create(:user))
   end
 
   describe "validations" do
-    subject { described_class.new(wizard:) }
-
-    before { create(:private_childcare_provider, provider_urn: "123456") }
+    let(:errors) { instance.tap(&:valid?).errors.to_hash }
 
     describe "#institution_identifier" do
+      before { create(:private_childcare_provider, provider_urn: "123456") }
+
       it "can have institution_identifier as empty string" do
         subject.institution_identifier = ""
-        expect(subject).to be_valid
+        expect(subject).to be_invalid
+        expect(errors).to eq institution_name: ["Enter a private childcare provider"]
       end
 
       it "can have institution_identifier as 'other'" do
         subject.institution_identifier = "other"
-        expect(subject).to be_valid
+        expect(subject).to be_invalid
+        expect(errors).to eq institution_name: ["Enter a private childcare provider"]
       end
 
       it "can have institution_identifier as 'PrivateChildcareProvider-123456'" do
@@ -49,24 +60,87 @@ RSpec.describe Questionnaires::ChoosePrivateChildcareProvider, type: :model do
         expect(subject).to be_invalid
         expect(subject.errors.messages[:institution_identifier]).to include(expected_message)
       end
+    end
 
-      it { is_expected.to validate_length_of(:institution_name).is_at_most(64) }
+    it { is_expected.to validate_length_of(:institution_name).is_at_most(64) }
+
+    context "when used from javascript autocomplete widget" do
+      context "with institution valid" do
+        let(:identifier) { "PrivateChildcareProvider-#{provider.provider_urn}" }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "with no institution or name" do
+        it { is_expected.to be_invalid }
+        it { is_expected.not_to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_name: ["Enter a private childcare provider"] }
+      end
+
+      context "with invalid institution" do
+        let(:identifier) { "PrivateChildcareProvider-X" }
+
+        it { is_expected.to be_invalid }
+        it { is_expected.not_to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_identifier: ["No private childcare providers with the URN X were found, please try again"] }
+      end
+    end
+
+    context "when used from static input fields without javascript" do
+      context "with institution valid" do
+        let(:identifier) { "PrivateChildcareProvider-#{provider.provider_urn}" }
+        let(:name) { provider.provider_name }
+
+        it { is_expected.to be_valid }
+      end
+
+      context "with no institution or name" do
+        it { is_expected.to be_invalid }
+        it { is_expected.not_to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_name: ["Enter a private childcare provider"] }
+      end
+
+      context "with workplace set to other" do
+        let(:identifier) { "other" }
+        let(:name) { provider.provider_name }
+
+        it { is_expected.to be_invalid }
+        it { is_expected.to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to be_empty }
+      end
+
+      context "with workplace set to invalid value and other identifier" do
+        let(:identifier) { "other" }
+        let(:name) { "Something" }
+
+        it { is_expected.to be_invalid }
+        it { is_expected.to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_name: ["No private childcare providers with the URN Something were found, please try again"] }
+      end
+
+      context "with workplace set to invalid value and blank identifier" do
+        let(:name) { "Something" }
+
+        it { is_expected.to be_invalid }
+        it { is_expected.to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_name: ["No private childcare providers with the URN Something were found, please try again"] }
+      end
+
+      context "with workplace set to other and not name" do
+        let(:identifier) { "other" }
+
+        it { is_expected.to be_invalid }
+        it { is_expected.to be_search_term_entered_in_no_js_fallback_form }
+        it { expect(errors).to eq institution_name: ["Enter a private childcare provider"] }
+      end
     end
   end
 
   describe "#next_step" do
-    context "when institution_identifier is blank" do
-      it "is choose_private_childcare_provider" do
-        expect(subject.next_step).to be(:choose_private_childcare_provider)
-      end
-    end
+    before { allow(subject).to receive(:institution_identifier).and_return("12345") }
 
-    context "when institution_identifier is present" do
-      before { allow(subject).to receive(:institution_identifier).and_return("12345") }
-
-      it "is choose_private_childcare_provider" do
-        expect(subject.next_step).to be(:choose_your_npq)
-      end
+    it "is choose_private_childcare_provider" do
+      expect(subject.next_step).to be(:choose_your_npq)
     end
   end
 
