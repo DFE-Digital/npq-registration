7cc2a3f478559eff89c18b80c350ce404ca45437
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-03-26 14:43:29 +0000

    make rake task void submitted duplicate declarations

--- spec/lib/tasks/one_off/void_or_clawback_duplicate_declarations_spec.rb
+++ spec/lib/tasks/one_off/void_or_clawback_duplicate_declarations_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe "one_off:clawback_duplicate_delcarations" do
+RSpec.describe "one_off:void_or_clawback_duplicate_declarations" do
   subject :run_task do
-    Rake::Task["one_off:clawback_duplicate_delcarations"].invoke(dry_run)
+    Rake::Task["one_off:void_or_clawback_duplicate_declarations"].invoke(dry_run)
   end
 
   let(:lead_provider) { create(:lead_provider) }
@@ -22,6 +22,9 @@ RSpec.describe "one_off:clawback_duplicate_delcarations" do
   let(:non_duplcate_declaration_completed) { create(:declaration, :paid, :completed, application: application_with_no_duplicate_declarations, cohort: cohort, lead_provider:) }
   let(:statement) { create(:statement, :next_output_fee, cohort:, lead_provider:) }
   let(:other_statement) { create(:statement, :next_output_fee, cohort: other_cohort, lead_provider:) }
+  let(:submitted_declaration) { create(:declaration, :submitted, application:, lead_provider:) }
+  let(:duplicate_submitted_declaration) { create(:declaration, :submitted, application:, lead_provider:) }
+  let(:non_duplicate_submitted_declaration) { create(:declaration, :submitted, application: application_with_no_duplicate_declarations, cohort:, lead_provider:) }
 
   let(:paid_declarations) do
     [
@@ -42,25 +45,39 @@ RSpec.describe "one_off:clawback_duplicate_delcarations" do
     ]
   end
 
+  let(:submitted_delcarations) do
+    [
+      submitted_declaration,
+      duplicate_submitted_declaration,
+      non_duplicate_submitted_declaration,
+    ]
+  end
+
   before do
     paid_declarations
     payable_declarations
+    submitted_delcarations
     statement
     other_statement
   end
 
-  after { Rake::Task["one_off:clawback_duplicate_delcarations"].reenable }
+  after { Rake::Task["one_off:void_or_clawback_duplicate_declarations"].reenable }
 
   context "when dry run is false" do
     let(:dry_run) { "false" }
 
-    it "voids the duplicate declarations" do
+    it "claws back the duplicate paid declarations" do
       run_task
       expect(duplicate_declaration.reload.state).to eq "awaiting_clawback"
       expect(other_cohort_duplicate_declaration.reload.state).to eq "awaiting_clawback"
     end
 
-    it "attaches the awaiting clawback declarations to a statement" do
+    it "voids the duplicate submitted declarations" do
+      run_task
+      expect(duplicate_submitted_declaration.reload.state).to eq "voided"
+    end
+
+    it "attaches the duplicate paid declarations to a statement" do
       run_task
       expect(statement.declarations).to contain_exactly(duplicate_declaration)
       expect(other_statement.declarations).to contain_exactly(other_cohort_duplicate_declaration)
@@ -73,6 +90,8 @@ RSpec.describe "one_off:clawback_duplicate_delcarations" do
       expect(non_duplcate_declaration.reload.state).to eq "paid"
       expect(non_duplcate_declaration_retained.reload.state).to eq "paid"
       expect(non_duplcate_declaration_completed.reload.state).to eq "paid"
+      expect(submitted_declaration.reload.state).to eq "submitted"
+      expect(non_duplicate_submitted_declaration.reload.state).to eq "submitted"
     end
 
     context "when there is a validation error on the declaration" do
@@ -85,6 +104,7 @@ RSpec.describe "one_off:clawback_duplicate_delcarations" do
         run_task
         paid_declarations.each { |declaration| expect(declaration.reload.state).to eq "paid" }
         payable_declarations.each { |declaration| expect(declaration.reload.state).to eq "payable" }
+        submitted_delcarations.each { |declaration| expect(declaration.reload.state).to eq "submitted" }
       end
     end
   end
@@ -96,6 +116,7 @@ RSpec.describe "one_off:clawback_duplicate_delcarations" do
       run_task
       paid_declarations.each { |declaration| expect(declaration.reload.state).to eq "paid" }
       payable_declarations.each { |declaration| expect(declaration.reload.state).to eq "payable" }
+      submitted_delcarations.each { |declaration| expect(declaration.reload.state).to eq "submitted" }
     end
 
     it "does not attach any declarations to a statement" do
