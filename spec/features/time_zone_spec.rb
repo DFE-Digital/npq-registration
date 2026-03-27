71bc38d403732655235224a30db6c14cc73a537b
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-03-27 17:59:44 +0000

    fix specs

--- spec/features/time_zone_spec.rb
+++ spec/features/time_zone_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Showing Application timestamps in UK local time", type: :feature do
+RSpec.feature "Showing Application timestamps in UK local time", :no_js, type: :feature do
   include Helpers::AdminLogin
 
   let(:next_year) { 1.year.from_now.year }
@@ -12,29 +12,31 @@ RSpec.feature "Showing Application timestamps in UK local time", type: :feature 
   before do
     travel_to(winter_timestamp) { winter_application }
     travel_to(summer_timestamp) { summer_application }
-
-    sign_in_as create(:admin)
   end
 
-  scenario "Viewing times in the winter" do
-    travel_to(winter_timestamp) do
-      visit(npq_separation_admin_applications_path)
+  shared_examples "displays times in UK local time" do
+    scenario "displays times in UK local time" do
+      travel_to(timestamp) do
+        sign_in_as create(:admin)
 
-      expect(page).to have_css("h1", text: "Applications")
+        visit(npq_separation_admin_applications_path)
 
-      expect(page).to have_css("td", text: "1 Jan #{next_year} 8:15am")
-      expect(page).to have_css("td", text: "1 Jul #{next_year} 12:15pm")
+        expect(page).to have_css("h1", text: "Applications")
+        expect(page).to have_css("td", text: "1 Jan #{next_year} 8:15am")
+        expect(page).to have_css("td", text: "1 Jul #{next_year} 12:15pm")
+      end
     end
   end
 
-  scenario "Viewing times in the summer" do
-    travel_to(summer_timestamp) do
-      visit(npq_separation_admin_applications_path)
+  context "when viewing times in the winter" do
+    let(:timestamp) { winter_timestamp }
 
-      expect(page).to have_css("h1", text: "Applications")
+    it_behaves_like "displays times in UK local time"
+  end
 
-      expect(page).to have_css("td", text: "1 Jan #{next_year} 8:15am")
-      expect(page).to have_css("td", text: "1 Jul #{next_year} 12:15pm")
-    end
+  context "when viewing times in the summer" do
+    let(:timestamp) { summer_timestamp }
+
+    it_behaves_like "displays times in UK local time"
   end
 end
