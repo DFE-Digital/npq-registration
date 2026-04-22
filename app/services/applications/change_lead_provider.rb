9d14e9ac5c8fcd9ff576da3e16ac007f2f36f5eb
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-04-22 16:36:47 +0100

    change Applications::ChangeLeadProvider to specify cohort when looking up lead providers

--- app/services/applications/change_lead_provider.rb
+++ app/services/applications/change_lead_provider.rb
@@ -20,12 +20,10 @@ module Applications
     end
 
     def lead_provider_options
-      # TODO: specify cohort when checking for valid lead provider
-      current_cohort_lead_providers_offering_the_same_course = LeadProvider.for(course: application.course).pluck(:id)
+      selected_cohort_lead_providers_offering_the_same_course = LeadProvider.for(course: application.course, cohort: application.cohort).pluck(:id)
       LeadProvider.where.not(id: application.lead_provider.id).pluck(:id, :name).map do |id, name|
-        unless current_cohort_lead_providers_offering_the_same_course.include?(id)
-          # TODO: change description to include application cohort
-          description = "This provider is not offering #{application.course.identifier} for the latest cohort"
+        unless selected_cohort_lead_providers_offering_the_same_course.include?(id)
+          description = "This provider is not offering #{application.course.identifier} for the chosen cohort"
         end
         OpenStruct.new(id:, name:, description:)
       end
--- spec/services/applications/change_lead_provider_spec.rb
+++ spec/services/applications/change_lead_provider_spec.rb
@@ -5,7 +5,8 @@ require "rails_helper"
 RSpec.describe Applications::ChangeLeadProvider, :with_cohorts, type: :model do
   subject(:service) { described_class.new(application:, lead_provider_id:) }
 
-  let(:application) { create(:application) }
+  let(:application) { create(:application, cohort:) }
+  let(:cohort) { create(:cohort, :current) }
   let(:lead_provider_id) { LeadProvider.for(course: application.course).last.id }
 
   describe "validation" do
@@ -44,10 +45,32 @@ RSpec.describe Applications::ChangeLeadProvider, :with_cohorts, type: :model do
   end
 
   describe "#lead_provider_options" do
+    let(:lead_providers_offering_course) { LeadProvider.where(name: ["Best Practice Network", "LLSE"]).map(&:name) }
+    let(:cohort) { create(:cohort, :next, :unfunded) }
+
+    before do
+      course_cohort = create(:course_cohort, course: application.course, cohort:)
+      lead_providers_offering_course.each do |lead_provider_name|
+        create(:course_cohort_provider, course_cohort:, lead_provider: LeadProvider.find_by(name: lead_provider_name))
+      end
+    end
+
     it "includes all lead providers except the current lead provider" do
       expect(service.lead_provider_options).to match_array(
         LeadProvider.where.not(id: application.lead_provider.id).map { |lp| an_object_having_attributes(id: lp.id, name: lp.name) },
       )
+    end
+
+    it "does not have a description for lead providers that offering the course" do
+      expect(
+        service.lead_provider_options.select { |option| lead_providers_offering_course.include?(option.name) }.map(&:description),
+      ).to all(be_nil)
+    end
+
+    it "includes a description for lead providers that are not offering the course" do
+      expect(
+        service.lead_provider_options.reject { |option| lead_providers_offering_course.include?(option.name) }.map(&:description),
+      ).to all(eq "This provider is not offering npq-senior-leadership for the chosen cohort")
     end
   end
 end
