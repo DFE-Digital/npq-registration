d66b6ac6db4516d2d7e00680df2864df3205e8a3
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-04-23 10:08:27 +0100

    add simple validation to check chosen cohort exists

--- spec/forms/questionnaires/course_start_date_spec.rb
+++ spec/forms/questionnaires/course_start_date_spec.rb
@@ -11,6 +11,19 @@ RSpec.describe Questionnaires::CourseStartDate, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:course_start_cohort) }
     it { is_expected.to validate_inclusion_of(:course_start_cohort).in_array(described_class::OPTIONS.keys) }
+
+    context "when the course_start_cohort does not correspond to an existing cohort" do
+      it { is_expected.to have_error(:course_start_cohort, :invalid) }
+    end
+
+    context "when the course_start_cohort corresponds to an existing cohort" do
+      before do
+        instance.course_start_cohort = described_class::OPTIONS.keys.first
+        create(:cohort, start_year: 2026)
+      end
+
+      it { is_expected.to be_valid }
+    end
   end
 
   describe "#next_step" do
