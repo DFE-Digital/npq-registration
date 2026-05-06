9f5fc7d8c936080d686defde3fb1f62eab5b1262
Jeb Wilkins <jeb@jdwilkins.co.uk>
2026-05-06 15:30:08 +0100

    [NPQ-3715] Follow up PR to address a flaky spec (#3322)
    ### Context
    
    Ticket: [NPQ-3715](https://dfedigital.atlassian.net/browse/NPQ-3715)
    
    ### Changes proposed in this pull request
    
    1. Follow up PR because the original one had a flaky spec
    
    
    [NPQ-3715]:
    https://dfedigital.atlassian.net/browse/NPQ-3715?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- spec/views/accounts/show.html.erb_spec.rb
+++ spec/views/accounts/show.html.erb_spec.rb
@@ -17,14 +17,19 @@ RSpec.describe "accounts/show.html.erb", type: :view do
   it { is_expected.to have_link "View details", href: accounts_user_registration_path(applications[1]) }
 
   context "with expired applications" do
-    before do
-      applications[0].update!(lead_provider_approval_status: :rejected,
-                              created_at: Application.cut_off_date_for_expired_applications - 1.day)
+    let(:applications) { [active, expired] }
+    let(:active) { create :application, user: }
+
+    let :expired do
+      create :application,
+             user:,
+             lead_provider_approval_status: :rejected,
+             created_at: Application.cut_off_date_for_expired_applications - 1.day
     end
 
     it { is_expected.to have_content "Register for another NPQ" }
     it { is_expected.to have_css ".govuk-summary-card", count: 2 }
-    it { is_expected.not_to have_link "View details", href: accounts_user_registration_path(applications[0]) }
-    it { is_expected.to have_link "View details", href: accounts_user_registration_path(applications[1]) }
+    it { is_expected.to have_link "View details", href: accounts_user_registration_path(active) }
+    it { is_expected.not_to have_link "View details", href: accounts_user_registration_path(expired) }
   end
 end
