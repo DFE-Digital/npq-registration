52018ebbcd054d784c6e0c569bbfb9cf2eb33fa7
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-05-12 11:11:28 +0100

    [NPQ-3597] upgrade to rails 8.1.3 (#3324)
    ### Context
    
    Ticket: https://dfedigital.atlassian.net/browse/NPQ-3597
    
    ### Changes proposed in this pull request
    
    * update to rails 8.1.3
    * ran `rails app:update` and merged in any upstream changes
    * `config.load_defaults` remains unchanged at `8.0`, and there is a new
    file `config/initializers/new_framework_defaults_8_1.rb` which hasn't
    had anything uncommented on it
    
    The `canonical-rails` specifies rails <8.1 - and [there is an open
    PR](https://github.com/jumph4x/canonical-rails/pull/96) - I've used the
    version from that PR.

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# NOTE: specifiying a symbol with match parameters that contain that word
+# Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
+# Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
   /\Abcc\z/,
