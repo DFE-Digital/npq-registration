b420f63c9b707743d705b6e80024bbe92b35e10c
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-05-14 11:30:48 +0100

    spec for scholarship funding details on account page

--- spec/features/account_spec.rb
+++ spec/features/account_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
-RSpec.feature "Account", type: :feature do
+RSpec.feature "Account", :no_js, type: :feature do
   include Helpers::JourneyAssertionHelper
+  include ActionView::Helpers::SanitizeHelper
 
   include_context "Stub Get An Identity Omniauth Responses"
 
@@ -26,10 +27,33 @@ RSpec.feature "Account", type: :feature do
         end
       end
 
-      scenario "when logged in, it shows the application details" do
+      scenario "it redirects to the accounts user registration page" do
         visit "/account"
 
         expect(page).to have_current_path("/accounts/user_registrations/#{application.id}")
+      end
+    end
+  end
+
+  describe "accounts user registration page" do
+    scenario "when not logged in, it redirects to sign in" do
+      visit(accounts_user_registration_path(application.id))
+
+      expect(page).to have_current_path("/sign-in")
+    end
+
+    context "when logged in" do
+      before do
+        navigate_to_page(path: "/", submit_form: false, axe_check: false) do
+          page.click_button("Start now")
+        end
+      end
+
+      scenario "it shows the accounts user registration page" do
+        expect(page).to have_current_path("/accounts/user_registrations/#{application.id}")
+      end
+
+      scenario "it shows the course start cohort" do
         expect(page).to have_summary_item("Course start", "2025")
       end
 
@@ -42,14 +66,34 @@ RSpec.feature "Account", type: :feature do
           expect(page).to have_summary_item("Course start", "Spring 2026")
         end
       end
-    end
-  end
 
-  describe "accounts user registration page" do
-    scenario "when not logged in, it redirects to sign in" do
-      visit(accounts_user_registration_path(application.id))
+      FundingEligibility.constants
+        .excluding(:MissingMandatoryInstitution,
+                   :FUNDING_STATUS_CODE_DESCRIPTIONS,
+                   :FUNDED_ELIGIBILITY_RESULT,
+                   :SUBJECT_TO_REVIEW).each do |funding_eligiblity_status|
+        context "when the application funding_eligiblity_status_code is #{funding_eligiblity_status}" do
+          let(:application) { create(:application, :without_funded_place, user:, cohort:, funding_eligiblity_status_code:) }
+          let(:funding_eligiblity_status_code) { FundingEligibility.const_get(funding_eligiblity_status) }
+          let(:funding_details_key) { FundingEligibility::FUNDING_STATUS_CODE_DESCRIPTIONS[funding_eligiblity_status_code] }
 
-      expect(page).to have_current_path("/sign-in")
+          scenario "it shows scholarship funding details" do
+            visit "/accounts/user_registrations/#{application.id}"
+
+            expect(page).to(
+              have_summary_item(
+                "Scholarship funding",
+                strip_tags(
+                  I18n.t(
+                    "funding_details.#{FundingEligibility::FUNDING_STATUS_CODE_DESCRIPTIONS[funding_eligiblity_status_code]}",
+                    course_name: "the Senior leadership NPQ",
+                  ),
+                ),
+              ),
+            )
+          end
+        end
+      end
     end
   end
 end
