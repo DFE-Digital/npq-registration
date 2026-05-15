dac9bf815f71ba07350f6b97e1865b9775917db9
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-05-15 16:00:11 +0100

    set up keys for activerecord encryption

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,11 @@ module NpqRegistration
     config.generators.system_tests = nil
 
     # NPQ specific changes
+
+    config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
+    config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
+    config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+
     config.exceptions_app = routes
 
     config.middleware.use Rack::Deflater
