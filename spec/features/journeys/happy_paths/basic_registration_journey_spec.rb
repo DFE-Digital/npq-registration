d46bb3394f71f80bdf1de345cff6bed08d8b9412
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-05-15 11:46:27 +0100

    remove warning from provider selection step

--- spec/features/journeys/happy_paths/basic_registration_journey_spec.rb
+++ spec/features/journeys/happy_paths/basic_registration_journey_spec.rb
@@ -64,7 +64,6 @@ RSpec.feature "Happy journeys", :with_default_schedules, :with_default_school, t
 
     expect_page_to_have(path: "/registration/choose-your-provider", submit_form: true) do
       expect(page).to have_text("Select your provider")
-      expect(page).to have_text("Before selecting your provider, you must check that they are currently open to accepting NPQ applications.")
       page.choose("Teach First", visible: :all)
     end
 
