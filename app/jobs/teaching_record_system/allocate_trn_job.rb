03aeb9878bb3f6da4c4827007455870b3df285c9
jebw <jeb@jdwilkins.co.uk>
2026-05-16 20:31:07 +0100

    [NPQ-3481] Added job to refresh token and trigger activation API

---
+++ app/jobs/teaching_record_system/allocate_trn_job.rb
@@ -0,0 +1,25 @@
+module TeachingRecordSystem
+  class AllocateTrnJob < ApplicationJob
+    def perform(user_id:)
+      user = User.find(user_id)
+      persisted_token = user.oauth_tokens.refresh.first
+
+      if user.trn.present?
+        # TRN has been allocated already and token no longer needed
+        persisted_token.destroy!
+        return
+      end
+
+      new_access, new_refresh = RefreshAccessToken.refresh!(persisted_token.token)
+      persisted_token.update!(token: new_refresh, last_updated_token_at: Time.zone.now)
+
+      new_trn = ActivateTrnAllocation.activate!(new_access)
+      if new_trn.present?
+        # Received TRN from API response so don't need to wait for webhook to receive TRN
+        user.update!(trn: new_trn, trn_verified: true, trn_auto_verified: true)
+      end
+
+      persisted_token.destroy!
+    end
+  end
+end
---
+++ spec/jobs/teaching_record_system/allocate_trn_job_spec.rb
@@ -0,0 +1,106 @@
+require "rails_helper"
+
+RSpec.describe TeachingRecordSystem::AllocateTrnJob, type: :job do
+  subject(:perform_job) { described_class.perform_now(user_id:) }
+
+  before do
+    allow(TeachingRecordSystem::RefreshAccessToken)
+      .to receive(:refresh!).and_return %w[NEWACCESS NEWREFRESH]
+
+    allow(TeachingRecordSystem::ActivateTrnAllocation)
+      .to receive(:activate!).and_return allocated_trn
+
+    allow(Sentry).to receive(:capture_exception)
+  end
+
+  let(:user_id) { user.id }
+  let(:allocated_trn) { nil }
+  let(:trn) { nil }
+
+  let :user do
+    create(:user, trn:, trn_verified: !!trn, trn_auto_verified: !!trn) do |user|
+      user.oauth_tokens.create(token: "OLDREFRESH", last_updated_token_at: 5.minutes.ago)
+    end
+  end
+
+  describe "#perform" do
+    context "with unknown user" do
+      let(:user_id) { nil }
+
+      it "throws a RecordNotFound exception" do
+        expect { perform_job }
+          .to raise_exception(ActiveRecord::RecordNotFound)
+          .and not_change(user.oauth_tokens.first, :token)
+      end
+    end
+
+    context "with user who already has TRN allocated" do
+      let(:trn) { "4536243" }
+
+      it "removes the refresh token but does not call the APIs" do
+        expect { perform_job }
+          .to change(user.oauth_tokens, :count).from(1).to(0)
+          .and not_change(user.reload, :trn)
+
+        expect(TeachingRecordSystem::RefreshAccessToken).not_to have_received(:refresh!)
+        expect(TeachingRecordSystem::ActivateTrnAllocation).not_to have_received(:activate!)
+      end
+    end
+
+    context "with user without TRN" do
+      context "when activate API does return a TRN" do
+        let(:allocated_trn) { "7349349" }
+
+        it "updates user with trn and removes the refresh token" do
+          expect { perform_job }
+            .to change { user.reload.trn }.from(nil).to(allocated_trn)
+            .and change(user.oauth_tokens, :count).from(1).to(0)
+        end
+      end
+
+      context "when activate API does not return a TRN" do
+        it "does not change trn but removes the refresh token now activation is triggered" do
+          expect { perform_job }
+            .to not_change { user.reload.trn }
+            .and change(user.oauth_tokens, :count).from(1).to(0)
+        end
+      end
+    end
+
+    context "when refresh API errors" do
+      before do
+        allow(TeachingRecordSystem::RefreshAccessToken)
+          .to receive(:refresh!).and_raise(Faraday::ForbiddenError)
+      end
+
+      it "notifies Sentry and does not attempt the Activate API" do
+        expect { perform_job }
+          .to raise_exception(Faraday::ForbiddenError)
+          .and not_change(user.reload, :trn)
+          .and not_change(user.oauth_tokens.first, :token)
+
+        expect(TeachingRecordSystem::ActivateTrnAllocation).not_to have_received(:activate!)
+        expect(Sentry).to have_received(:capture_exception).with(Faraday::ForbiddenError)
+      end
+    end
+
+    context "when activate API errors" do
+      before do
+        allow(TeachingRecordSystem::ActivateTrnAllocation)
+          .to receive(:activate!).and_raise(Faraday::ForbiddenError)
+      end
+
+      it "notifies Sentry and allows the job to retry" do
+        expect { perform_job }
+          .to raise_exception(Faraday::ForbiddenError)
+          .and not_change(user.reload, :trn)
+          .and not_change(user.oauth_tokens, :count)
+          .and change { user.oauth_tokens.first.reload.token }
+                      .from("OLDREFRESH")
+                      .to("NEWREFRESH")
+
+        expect(Sentry).to have_received(:capture_exception).with(Faraday::ForbiddenError)
+      end
+    end
+  end
+end
