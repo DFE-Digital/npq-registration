76742522c0429d3c1d8b3a65efa5369f103defc3
jebw <jeb@jdwilkins.co.uk>
2026-05-27 11:29:10 +0100

    [NPQ-3743] Added header links for TeacherAuth

--- lib/omniauth/strategies/teacher_auth.rb
+++ lib/omniauth/strategies/teacher_auth.rb
@@ -2,14 +2,30 @@ module Omniauth
   module Strategies
     class TeacherAuth < OmniAuth::Strategies::OpenIDConnect
       NAME = :teacher_auth
+      TOKEN_KEY = "id_token_hint".freeze
 
       option :name, NAME
       option :pkce, true
       option :discovery, true
       option :send_scope_to_token_endpoint, false
 
-      # TeacherAuth scopes: openid, email, profile, teaching_record, offline_access
+      # TeacherAuth scopes: openid, email, profile, teaching_record
       option :scope, %i[email openid profile teaching_record offline_access].join(" ")
+
+      def encoded_post_logout_redirect_uri
+        return unless options.post_logout_redirect_uri
+
+        logout_uri_params = {
+          "post_logout_redirect_uri" => options.post_logout_redirect_uri,
+        }
+
+        if query_string.present?
+          query_params = CGI.parse(query_string[1..])
+          logout_uri_params[TOKEN_KEY] = query_params[TOKEN_KEY].first if query_params.key?(TOKEN_KEY)
+        end
+
+        URI.encode_www_form(logout_uri_params)
+      end
     end
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,14 +3,68 @@ require "rails_helper"
 RSpec.describe SessionsController do
   include Helpers::JourneyHelper
 
-  it "redirects to the external OIDC provider's signout endpoint" do
-    expect(controller).to receive(:sign_out_all_scopes)
+  let(:post_logout_uri) { "http://test.host/sign-out" }
 
-    root_path = "https://test.host/"
-    expected_redirect_url = "https://tra-domain.com:443/connect/signout?client_id=register-for-npq&post_logout_redirect_uri=#{CGI.escape(root_path)}"
+  context "with a Get an Identity user is signed in" do
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
 
-    get :destroy
+    let(:user) { create(:user, :with_get_an_identity_id) }
 
-    expect(response).to redirect_to(expected_redirect_url)
+    it "redirects to the external OIDC provider's signout endpoint" do
+      expect(controller).to receive(:sign_out_all_scopes)
+
+      expected_redirect_url = "https://tra-domain.com:443/connect/signout?client_id=register-for-npq&post_logout_redirect_uri=#{CGI.escape(post_logout_uri)}"
+
+      get :destroy
+
+      expect(response).to redirect_to(expected_redirect_url)
+    end
+  end
+
+  context "when a TeacherAuth user is signed in" do
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    let(:user) { create(:user, :with_teacher_auth) }
+    let(:id_token) { "test-id-token" }
+
+    it "redirects to the external OIDC provider's signout endpoint" do
+      expect(controller).to receive(:sign_out_all_scopes)
+
+      session[:id_token] = id_token
+
+      expected_redirect_url = "/users/auth/teacher_auth/logout?id_token_hint=#{id_token}"
+
+      get :destroy
+
+      expect(response).to redirect_to(expected_redirect_url)
+    end
+  end
+
+  context "when an admin is signed in" do
+    before do
+      allow(controller).to receive(:current_admin).and_return(create(:admin))
+    end
+
+    it "redirects to admin path" do
+      expect(controller).to receive(:sign_out_all_scopes)
+
+      get :destroy
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  context "when no user is signed in (callback from provider)" do
+    it "redirects to root" do
+      expect(controller).to receive(:sign_out_all_scopes)
+
+      get :destroy
+
+      expect(response).to redirect_to(root_path)
+    end
   end
 end
