7e5307ebfcf0fac92f066734ec82bc04549bdfac
jebw <jeb@jdwilkins.co.uk>
2026-06-05 17:20:09 +0100

    [NPQ-3780] Review app for testing configuring 2026

--- db/seeds/base/add_cohorts.rb
+++ db/seeds/base/add_cohorts.rb
@@ -8,7 +8,3 @@ current_cohort_start_year = Date.current.month < 9 ? (Date.current.year - 1) : D
 (2024..current_cohort_start_year).each do |start_year|
   FactoryBot.create(:cohort, :with_funding_cap, start_year:)
 end
-
-# create next cohorts
-FactoryBot.create(:cohort, :next, :unfunded, description: "Unfunded cohort")
-FactoryBot.create(:cohort, :next, :with_funding_cap, suffix: "b")
