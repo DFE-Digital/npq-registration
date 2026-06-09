d7f8037083f6aad241281ffe0f8d55382ad4e3d7
Lorenzo Tello <ltello8a@gmail.com>
2026-06-09 15:43:53 +0100

    [NPQ-3795] Remove DfE Identity personal details from account pages
    Now the account and registration pages don't show the message for visit
    the DfE Identity account to check or change personal details. I also
    remove the helper link_to_identity_account that is not used now and its
    specs.

--- spec/helpers/identity_account_helper_spec.rb
+++ spec/helpers/identity_account_helper_spec.rb
@@ -3,34 +3,6 @@ require "rails_helper"
 RSpec.describe IdentityAccountHelper, type: :helper do
   include Helpers::JourneyHelper
 
-  describe "#link_to_identity_account" do
-    subject(:link) { helper.link_to_identity_account(redirect_uri) }
-
-    let(:redirect_uri) { "https://redirect.uri?param=value" }
-
-    it "is built with the TRA domain" do
-      expect(link).to match("https://tra-domain.com/")
-    end
-
-    it "includes the client_id query parameter" do
-      expect(link).to match("client_id=npq")
-    end
-
-    it "includes the URL encoded redirect_id query parameter" do
-      # CGI::escape('https://redirect.uri?param=value')
-      url_encoded = "https%3A%2F%2Fredirect.uri%3Fparam%3Dvalue"
-
-      expect(link).to match("redirect_uri=#{url_encoded}")
-    end
-
-    it "includes the URL encoded for the sign_out parameter" do
-      # CGI::escape('https://redirect.uri/sign-out')
-      url_encoded = "https%3A%2F%2Fredirect.uri%2Fsign-out"
-
-      expect(link).to match("sign_out_uri=#{url_encoded}")
-    end
-  end
-
   describe "#identity_link_uri" do
     let(:fake_account_link) do
       instance_double(IdentityAccountHelper::IdentityAccountLink, build: true)
