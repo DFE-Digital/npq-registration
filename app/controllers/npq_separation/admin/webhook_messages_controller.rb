31a70c98d42e51b0f9826d31399e6314ec8b5b42
Lorenzo Tello <ltello8a@gmail.com>
2026-06-12 16:26:26 +0100

    [NPQ-3805] Add filters by message type and status to webhook messages admin list

--- app/controllers/npq_separation/admin/webhook_messages_controller.rb
+++ app/controllers/npq_separation/admin/webhook_messages_controller.rb
@@ -1,6 +1,7 @@
 class NpqSeparation::Admin::WebhookMessagesController < NpqSeparation::AdminController
   def index
     @pagy, @webhook_messages = pagy(scope)
+    @message_types = GetAnIdentity::WebhookMessage.distinct.pluck(:message_type).compact.sort
   end
 
   def show
@@ -9,7 +10,11 @@ class NpqSeparation::Admin::WebhookMessagesController < NpqSeparation::AdminCont
 
 private
 
+  def filter_params
+    params.permit(%i[message_type status]).compact_blank
+  end
+
   def scope
-    GetAnIdentity::WebhookMessage.all.order(created_at: :desc, id: :desc)
+    GetAnIdentity::WebhookMessage.where(filter_params).order(created_at: :desc, id: :desc)
   end
 end
