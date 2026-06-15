caa71f919405acfe28375c9b226ca8191c9269f0
Benjamin Fowl <b.fowl87@gmail.com>
2026-06-15 15:10:13 +0100

    [NPQ-3787] - Change webhooks to use blocklist

--- app/jobs/get_an_identity/process_webhook_message_job.rb
+++ app/jobs/get_an_identity/process_webhook_message_job.rb
@@ -1,5 +1,7 @@
 module GetAnIdentity
   class ProcessWebhookMessageJob < ApplicationJob
+    class UnknownMessageTypeError < StandardError; end
+
     queue_as :default
 
     def perform(webhook_message:)
@@ -7,16 +9,17 @@ module GetAnIdentity
 
       webhook_processor_klass = webhook_message.processor_klass
 
-      if webhook_processor_klass.blank?
+      if webhook_processor_klass.present?
+        webhook_processor_klass.call(webhook_message:)
+      elsif webhook_message.ignored_message_type?
         webhook_message.update!(
           status: :unhandled_message_type,
           status_comment: "No processor found for webhook message",
           processed_at: Time.zone.now,
         )
-        return
+      else
+        raise UnknownMessageTypeError, "No processor found for webhook message type: #{webhook_message.message_type}"
       end
-
-      webhook_processor_klass.call(webhook_message:)
     end
   end
 end
