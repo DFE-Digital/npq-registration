b28b6fc7336051b22b89010d4c72a41c74200cdd
Alkesh Vaghmaria <alkesh.vaghmaria@education.gov.uk>
2026-06-25 10:37:19 +0100

    [BAU] do not use constantize on a param (#3441)
    ### Context
    
    BAU
    
    avoid using `constantize` on a parameter
    
    ### Changes proposed in this pull request
    
    1. use a mapping hash instead of `constantize`

--- app/models/eligibility_list/childminder.rb
+++ app/models/eligibility_list/childminder.rb
@@ -2,5 +2,4 @@ class EligibilityList::Childminder < EligibilityList::Entry
   IDENTIFIER_CSV_HEADERS = ["Childminder URN"].freeze
   IDENTIFIER_CSV_EXAMPLE = "CA000006\nEY248638\n225540".freeze
   IDENTIFIER_TYPE = :urn
-  BULK_OPERATION_CLASS = BulkOperation::UploadEligibilityList::Childminder
 end
