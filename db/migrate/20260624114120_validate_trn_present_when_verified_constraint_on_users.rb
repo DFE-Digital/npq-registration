088597b5b2b3700e280a2abf8c7d9d9bfadb76a1
Lorenzo Tello <ltello8a@gmail.com>
2026-06-26 16:36:44 +0100

    [NPQ-3786] Move trn_verified cleanup into the constraint migration

--- db/migrate/20260624114120_validate_trn_present_when_verified_constraint_on_users.rb
+++ db/migrate/20260624114120_validate_trn_present_when_verified_constraint_on_users.rb
@@ -1,17 +1,7 @@
 class ValidateTrnPresentWhenVerifiedConstraintOnUsers < ActiveRecord::Migration[8.0]
-  def up
-    # Clean rows that would fail the constraint before validating it.
-    safety_assured do
-      execute(<<~SQL)
-        UPDATE users
-        SET trn_verified = false
-        WHERE trn_verified = true
-          AND (trn IS NULL OR trn = '')
-      SQL
-    end
+  disable_ddl_transaction!
 
+  def change
     validate_check_constraint :users, name: "users_trn_present_when_verified"
   end
-
-  def down; end
 end
