806ebb04024cd2fcd25a1b5dada4078076f702ea
Lorenzo Tello <ltello8a@gmail.com>
2026-07-14 17:41:37 +0100

    [NPQ-3850] Update the Your NPQ registrations page

--- spec/views/accounts/show.html.erb_spec.rb
+++ spec/views/accounts/show.html.erb_spec.rb
@@ -4,20 +4,33 @@ RSpec.describe "accounts/show.html.erb", type: :view do
   subject { render }
 
   before do
-    applications
+    assign(:active_applications, active_applications)
+    assign(:expired_applications, expired_applications)
     allow(view).to receive(:current_user).and_return(user)
   end
 
   let(:user) { create(:user, :with_teacher_auth) }
-  let(:applications) { create_list :application, 2, user: }
+  let(:active_applications) { create_list :application, 2, user: }
+  let(:expired_applications) { [] }
 
+  it { is_expected.to have_css "h1", text: "Your NPQ registrations" }
   it { is_expected.to have_content "Register for another NPQ" }
   it { is_expected.to have_css ".govuk-summary-card", count: 2 }
-  it { is_expected.to have_link "View details", href: accounts_user_registration_path(applications[0]) }
-  it { is_expected.to have_link "View details", href: accounts_user_registration_path(applications[1]) }
+  it { is_expected.to have_link "View details", href: accounts_user_registration_path(active_applications[0]) }
+  it { is_expected.to have_link "View details", href: accounts_user_registration_path(active_applications[1]) }
+
+  it "shows a link to update personal details on GOV.UK One Login" do
+    expect(render).to have_link "GOV.UK One Login (opens in a new tab)", href: Rails.configuration.x.teacher_auth.onelogin_home_uri
+  end
+
+  it "shows the awaiting provider application status" do
+    expect(render).to have_content "Awaiting provider"
+    expect(render).to have_content "Your provider will contact you with instructions on how to apply for the course."
+  end
 
   context "with expired applications" do
-    let(:applications) { [active, expired] }
+    let(:active_applications) { [active] }
+    let(:expired_applications) { [expired] }
     let(:active) { create :application, user: }
 
     let :expired do
@@ -28,6 +41,7 @@ RSpec.describe "accounts/show.html.erb", type: :view do
     end
 
     it { is_expected.to have_content "Register for another NPQ" }
+    it { is_expected.to have_content "Expired registrations" }
     it { is_expected.to have_css ".govuk-summary-card", count: 2 }
     it { is_expected.to have_link "View details", href: accounts_user_registration_path(active) }
     it { is_expected.not_to have_link "View details", href: accounts_user_registration_path(expired) }
