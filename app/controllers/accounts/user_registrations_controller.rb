0313baac1c2d70bd348547a0ff35ec3ef33642a3
Lorenzo Tello <ltello8a@gmail.com>
2026-07-15 14:13:01 +0100

    [NPQ-3849] Keep the show action to stop the details page redirect loop

--- app/controllers/accounts/user_registrations_controller.rb
+++ app/controllers/accounts/user_registrations_controller.rb
@@ -1,6 +1,10 @@
 class Accounts::UserRegistrationsController < AccountsController
   before_action :set_application
 
+  def show
+    # Overrides AccountsController#show, which would redirect back to this page and loop.
+  end
+
 private
 
   def set_application
