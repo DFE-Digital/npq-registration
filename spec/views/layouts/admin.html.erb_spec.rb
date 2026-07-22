94a74838723e3ad1f108df6461b92b863593b03e
jebw <jeb@jdwilkins.co.uk>
2026-07-22 17:16:15 +0100

    [NPQ-3918] Only show admin sidenav if content present

--- spec/views/layouts/admin.html.erb_spec.rb
+++ spec/views/layouts/admin.html.erb_spec.rb
@@ -1,13 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "layouts/admin.html.erb", type: :view do
-  subject { Capybara.string(render) }
+  subject(:rendered) { Capybara.string(render) }
 
   let(:admin) { build_stubbed(:admin) }
-  let(:expected_items) do
-    structure = Admin::NavigationStructures::AdminNavigationStructure.new(admin)
-    structure.primary_structure.map(&:name)
-  end
+  let(:nav) { Admin::NavigationStructures::AdminNavigationStructure.new(admin) }
+  let(:expected_items) { nav.primary_structure.map(&:name) }
 
   before do
     view.define_singleton_method(:current_admin, &method(:admin))
@@ -23,5 +21,27 @@ RSpec.describe "layouts/admin.html.erb", type: :view do
     end
 
     it { is_expected.to have_link("Sign out") }
+  end
+
+  describe "sidebar navigation" do
+    subject do
+      rendered.find(".govuk-width-container > .govuk-main-wrapper > .govuk-grid-row")
+    end
+
+    before { allow(view).to receive(:admin_navigation_structure).and_return(nav) }
+
+    context "when sidebar navigation content present" do
+      it { is_expected.to have_css "> .govuk-grid-column-one-quarter" }
+      it { is_expected.to have_css "> .govuk-grid-column-three-quarters" }
+      it { is_expected.not_to have_css "> .govuk-grid-column-full" }
+    end
+
+    context "when sidebar navigation content is blank" do
+      before { allow(nav).to receive(:sub_structure).and_return([]) }
+
+      it { is_expected.to have_css "> .govuk-grid-column-full" }
+      it { is_expected.not_to have_css "> .govuk-grid-column-one-quarter" }
+      it { is_expected.not_to have_css "> .govuk-grid-column-three-quarters" }
+    end
   end
 end
