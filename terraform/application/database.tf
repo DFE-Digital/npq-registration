30897f4fe551d2e31fe7222199238937acc1dc08
Philip Hope <philip.hope@mobilise.cloud>
2026-08-03 08:36:06 +0100

    2811 - Review app using Postgres v17 container (#3525)
    ### Context
    
    https://trello.com/c/0Ux3mX0O/2811-upgrade-npq-postgres-version
    
    We're looking to upgrade the Postgres version to 17.
    
    ### Changes proposed in this pull request
    
    - This PR will create a review app to check application functionality
    with Postgres v17 containerised.
    - A new variable, postgres_server_version will allow configuration
    between environments. The default is set to the current version 14.
    - postgres_server_version set to 17 in review.tfvars.json
    - Change the Docker container versions of Postgres to v17
    
    ### Guidance to review
    
    - Run `make review terraform-plan PULL_REQUEST_NUMBER=3525
    DOCKER_IMAGE=main` only the Kubernetes image should change.
    - Check the npq-registration-review-3525-postgres deployment to ensure
    the image is
    ghcr.io/dfe-digital/teacher-services-cloud:postgres-17-alpine
    - Run `make staging terraform-plan DOCKER_IMAGE=main` only the
    Kubernetes image should change.
    - Test the [review
    app](https://npq-registration-review-3525-web.test.teacherservices.cloud/)
    crated by this PR.
    
    ### Checklist
    
    - [x] Attach to Trello card
    - [x] Rebased main
    - [x] Cleaned commit history
    - [x] Tested by running locally

--- terraform/application/database.tf
+++ terraform/application/database.tf
@@ -35,7 +35,7 @@ module "postgres" {
   use_azure                      = var.deploy_azure_backing_services
   azure_enable_monitoring        = var.enable_monitoring
   azure_enable_backup_storage    = var.enable_postgres_backup_storage
-  server_version                 = "14"
+  server_version                 = var.postgres_server_version
   azure_extensions               = ["btree_gin", "citext", "fuzzystrmatch", "pg_trgm"]
   azure_enable_high_availability = var.postgres_enable_high_availability
   azure_sku_name                 = var.postgres_flexible_server_sku
