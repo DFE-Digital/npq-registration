6ebdfac8408977e4f43898ce053f9d28744ce76f
jebw <jeb@jdwilkins.co.uk>
2026-09-07 13:51:46 +0100

    [NPQ-3251] Address issues in change date specs

--- spec/features/admin/finance/statement_change_deadline_date_spec.rb
+++ spec/features/admin/finance/statement_change_deadline_date_spec.rb
@@ -64,7 +64,7 @@ RSpec.feature "Statement - change deadline date", :no_js, type: :feature do
     scenario "it shows an error" do
       visit(admin_finance_statements_change_deadline_date_path(statement))
 
-      new_deadline_date = statement.payment_date - 1.month
+      new_deadline_date = statement.deadline_date + 1.day
       fill_in "statements_change_deadline_date[deadline_date(3i)]", with: new_deadline_date.day
       fill_in "statements_change_deadline_date[deadline_date(2i)]", with: new_deadline_date.month
       fill_in "statements_change_deadline_date[deadline_date(1i)]", with: new_deadline_date.year
