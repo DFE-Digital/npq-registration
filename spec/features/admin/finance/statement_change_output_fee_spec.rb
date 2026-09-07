54f3669216493f8469afa7c349295f86fba2f299
jebw <jeb@jdwilkins.co.uk>
2026-09-07 09:30:17 +0100

    [NPQ-3251] Added permissions checks to feature spec

--- spec/features/admin/finance/statement_change_output_fee_spec.rb
+++ spec/features/admin/finance/statement_change_output_fee_spec.rb
@@ -7,12 +7,11 @@ RSpec.feature "Statement - change output_fee", :no_js, type: :feature do
 
   let(:statement) { create(:statement, output_fee: false) }
 
-  before do
-    allow(Statements::ChangeOutputFeeJob).to receive(:perform_later)
-    sign_in_as(create(:super_admin))
-  end
+  before { allow(Statements::ChangeOutputFeeJob).to receive(:perform_later) }
 
   scenario "updating output fee" do
+    sign_in_as create(:super_admin)
+
     visit admin_finance_statement_path(statement)
     within(".govuk-summary-list__row", text: "Output statement") do
       click_link "Change"
@@ -51,6 +50,8 @@ RSpec.feature "Statement - change output_fee", :no_js, type: :feature do
     let(:statement) { create(:statement, :payable, output_fee: false) }
 
     scenario "it shows an error" do
+      sign_in_as create(:super_admin)
+
       visit admin_finance_statement_path(statement)
       within(".govuk-summary-list__row", text: "Output statement") do
         click_link "Change"
@@ -65,5 +66,22 @@ RSpec.feature "Statement - change output_fee", :no_js, type: :feature do
       expect(page).to have_current_path admin_finance_statements_change_output_fee_path(statement)
       expect(page).to have_content I18n.t("activemodel.errors.models.statements/change_output_fee.attributes.allow_payable_statement_changes.accepted")
     end
+  end
+
+  scenario "when attempting to change output as a regular admin" do
+    sign_in_as create(:admin)
+
+    visit admin_finance_statement_path(statement)
+    within(".govuk-summary-list__row", text: "Output statement") do |row|
+      expect(row).not_to have_link("Change")
+    end
+
+    visit admin_finance_statements_change_output_fee_path(statement)
+    expect(page).to have_current_path sign_in_path
+  end
+
+  scenario "when not signed in" do
+    visit admin_finance_statement_path(statement)
+    expect(page).to have_current_path sign_in_path
   end
 end
